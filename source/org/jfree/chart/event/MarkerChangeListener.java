0072f512b2efc26f26718d7ca486356f88a60e36
David Gilbert <jfree@users.noreply.github.com>
2007-09-28 15:52:14 +0000

    Removed CVS $Id tags.

--- source/org/jfree/chart/event/MarkerChangeListener.java
+++ source/org/jfree/chart/event/MarkerChangeListener.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2006, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,12 +27,10 @@
  * -------------------------
  * MarkerChangeListener.java
  * -------------------------
- * (C) Copyright 2006, by Object Refinery Limited.
+ * (C) Copyright 2006, 2007, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
- *
- * $Id: MarkerChangeListener.java,v 1.1.2.3 2006/10/25 14:49:30 mungady Exp $
  *
  * Changes
  * -------
