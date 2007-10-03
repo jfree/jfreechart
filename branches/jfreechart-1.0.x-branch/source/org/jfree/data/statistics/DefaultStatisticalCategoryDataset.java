60a933b23ed5def7836a5a0353f61bac409ad404
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2007-10-03 13:04:39 +0000

    Inserted missing braces.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/data/statistics/DefaultStatisticalCategoryDataset.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/data/statistics/DefaultStatisticalCategoryDataset.java
@@ -550,14 +550,17 @@ public class DefaultStatisticalCategoryDataset extends AbstractDataset
         Range result = null;
         if (includeInterval) {
             if (!Double.isNaN(this.minimumRangeValueIncStdDev) 
-                    && !Double.isNaN(this.maximumRangeValueIncStdDev))
-            result = new Range(this.minimumRangeValueIncStdDev, 
-                    this.maximumRangeValueIncStdDev);
+                    && !Double.isNaN(this.maximumRangeValueIncStdDev)) {
+                result = new Range(this.minimumRangeValueIncStdDev, 
+                        this.maximumRangeValueIncStdDev);
+            }
         }
         else {
             if (!Double.isNaN(this.minimumRangeValue) 
-                    && !Double.isNaN(this.maximumRangeValue))
-            result = new Range(this.minimumRangeValue, this.maximumRangeValue);            
+                    && !Double.isNaN(this.maximumRangeValue)) {
+                result = new Range(this.minimumRangeValue, 
+                        this.maximumRangeValue);
+            }
         }
         return result;
     }
