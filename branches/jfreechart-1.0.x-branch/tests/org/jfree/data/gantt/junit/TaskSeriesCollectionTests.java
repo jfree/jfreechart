f220b08680aab6b40d009e884a320e52a93ee289
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2007-10-05 09:23:32 +0000

    Removed CVS $Id tags.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/data/gantt/junit/TaskSeriesCollectionTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/data/gantt/junit/TaskSeriesCollectionTests.java
@@ -32,8 +32,6 @@
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
  *
- * $Id: TaskSeriesCollectionTests.java,v 1.1.2.2 2007/03/08 13:57:09 mungady Exp $
- *
  * Changes
  * -------
  * 30-Jul-2004 : Version 1 (DG);
