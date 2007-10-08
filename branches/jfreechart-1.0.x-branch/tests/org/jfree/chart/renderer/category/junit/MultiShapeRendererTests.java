412614ffa19f8380c4d53557e8fdd15e1c4b0d5d
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2007-10-08 13:33:40 +0000

    2007-10-08  David Gilbert  <david.gilbert@object-refinery.com>
    	* source/org/jfree/chart/renderer/category/MultiShapeRenderer.java: 
    	New file,
    	* source/org/jfree/data/statistics/DefaultMultiValueCategoryDataset.java:
    	Likewise,
    	* source/org/jfree/data/statistics/MultiValueCategoryDataset.java: 
    	Likewise,
    	* tests/org/jfree/chart/renderer/category/junit/MultiShapeRendererTests.java:
    	Likewise,
    	* tests/org/jfree/chart/renderer/category/junit/RendererCategoryPackageTests.java
    	(suite): Added MultiShapeRendererTests,
    	* tests/org/jfree/data/statistics/junit/DataStatisticsPackageTests.java
    	(suite): Added DefaultMultiValueCategoryDatasetTests,
    	* tests/org/jfree/data/statistics/junit/DefaultMultiValueCategoryDatasetTests.java:
    	New file.

---
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/renderer/category/junit/MultiShapeRendererTests.java
@@ -0,0 +1,217 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
+ * USA.  
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * in the United States and other countries.]
+ *
+ * ----------------------------
+ * MultiShapeRendererTests.java
+ * ----------------------------
+ * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 08-Oct-2007 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.chart.renderer.category.junit;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jfree.chart.renderer.category.MultiShapeRenderer;
+
+/**
+ * Tests for the {@link MultiShapeRenderer} class.
+ */
+public class MultiShapeRendererTests extends TestCase {
+
+    /**
+     * Returns the tests as a test suite.
+     *
+     * @return The test suite.
+     */
+    public static Test suite() {
+        return new TestSuite(MultiShapeRendererTests.class);
+    }
+
+    /**
+     * Constructs a new set of tests.
+     *
+     * @param name  the name of the tests.
+     */
+    public MultiShapeRendererTests(String name) {
+        super(name);
+    }
+
+    /**
+     * Test that the equals() method distinguishes all fields.
+     */
+    public void testEquals() {
+        
+        MultiShapeRenderer r1 = new MultiShapeRenderer();
+        MultiShapeRenderer r2 = new MultiShapeRenderer();
+        assertEquals(r1, r2);
+        
+        r1.setSeriesShapesFilled(1, true);
+        assertFalse(r1.equals(r2));
+        r2.setSeriesShapesFilled(1, true);
+        assertTrue(r1.equals(r2));
+        
+        r1.setBaseShapesFilled(false);
+        assertFalse(r1.equals(r2));
+        r2.setBaseShapesFilled(false);
+        assertTrue(r1.equals(r2));
+        
+        r1.setUseFillPaint(true);
+        assertFalse(r1.equals(r2));
+        r2.setUseFillPaint(true);
+        assertTrue(r1.equals(r2));
+        
+        r1.setDrawOutlines(true);
+        assertFalse(r1.equals(r2));
+        r2.setDrawOutlines(true);
+        assertTrue(r1.equals(r2));
+        
+        r1.setUseOutlinePaint(true);
+        assertFalse(r1.equals(r2));
+        r2.setUseOutlinePaint(true);
+        assertTrue(r1.equals(r2));
+        
+        r1.setUseSeriesOffset(false);
+        assertFalse(r1.equals(r2));
+        r2.setUseSeriesOffset(false);
+        assertTrue(r1.equals(r2));
+        
+    }
+
+    /**
+     * Two objects that are equal are required to return the same hashCode. 
+     */
+    public void testHashcode() {
+        MultiShapeRenderer r1 = new MultiShapeRenderer();
+        MultiShapeRenderer r2 = new MultiShapeRenderer();
+        assertTrue(r1.equals(r2));
+        int h1 = r1.hashCode();
+        int h2 = r2.hashCode();
+        assertEquals(h1, h2);
+    }
+    
+    /**
+     * Confirm that cloning works.
+     */
+    public void testCloning() {
+        MultiShapeRenderer r1 = new MultiShapeRenderer();
+        MultiShapeRenderer r2 = null;
+        try {
+            r2 = (MultiShapeRenderer) r1.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(r1 != r2);
+        assertTrue(r1.getClass() == r2.getClass());
+        assertTrue(r1.equals(r2));
+        
+        assertTrue(checkIndependence(r1, r2));
+        
+    }
+
+    /**
+     * Checks that the two renderers are equal but independent of one another.
+     * 
+     * @param r1  renderer 1.
+     * @param r2  renderer 2.
+     * 
+     * @return A boolean.
+     */
+    private boolean checkIndependence(MultiShapeRenderer r1, 
+            MultiShapeRenderer r2) {
+
+        // should be equal...
+        if (!r1.equals(r2)) {
+            return false;
+        }
+        
+        // and independent...
+        r1.setSeriesShapesFilled(1, true);
+        if (r1.equals(r2)) {
+            return false;
+        }
+        r2.setSeriesShapesFilled(1, true);
+        if (!r1.equals(r2)) {
+            return false;
+        }
+        
+        r1.setBaseShapesFilled(false);
+        r2.setBaseShapesFilled(true);
+        if (r1.equals(r2)) {
+            return false;
+        }
+        r2.setBaseShapesFilled(false);
+        if (!r1.equals(r2)) {
+            return false;
+        }
+        return true;
+    
+    }
+
+    /**
+     * Serialize an instance, restore it, and check for equality.
+     */
+    public void testSerialization() {
+
+        MultiShapeRenderer r1 = new MultiShapeRenderer();
+        MultiShapeRenderer r2 = null;
+
+        try {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(buffer);
+            out.writeObject(r1);
+            out.close();
+
+            ObjectInput in = new ObjectInputStream(
+                    new ByteArrayInputStream(buffer.toByteArray()));
+            r2 = (MultiShapeRenderer) in.readObject();
+            in.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(r1, r2);
+
+    }
+    
+}
