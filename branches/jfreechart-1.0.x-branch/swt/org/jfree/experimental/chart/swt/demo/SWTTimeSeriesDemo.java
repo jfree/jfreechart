8a35600a656d97772e10df1658d13fc348d5a0f1
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2007-10-24 09:39:20 +0000

    Updated e-mail address.

--- branches/jfreechart-1.0.x-branch/swt/org/jfree/experimental/chart/swt/demo/SWTTimeSeriesDemo.java
+++ branches/jfreechart-1.0.x-branch/swt/org/jfree/experimental/chart/swt/demo/SWTTimeSeriesDemo.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2006, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,10 +27,10 @@
  * ---------------------
  * SWTTimeSeriesDemo.java
  * ---------------------
- * (C) Copyright 2006, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2006, 2007, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
- * Contributor(s):   Henry Proudhon (henry.proudhon AT insa-lyon.fr);
+ * Contributor(s):   Henry Proudhon (henry.proudhon AT ensmp.fr);
  *
  * Changes
  * -------
