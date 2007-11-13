173ef129574f517685c11ce1080a77b26e821f1c
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2007-11-13 15:42:14 +0000

    New tests.

---
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/axis/junit/ModuloAxisTests.java
@@ -0,0 +1,149 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
+ * USA.  
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * in the United States and other countries.]
+ *
+ * --------------------
+ * ModuloAxisTests.java
+ * --------------------
+ * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 13-Nov-2007 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.chart.axis.junit;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jfree.chart.axis.ModuloAxis;
+import org.jfree.data.Range;
+
+/**
+ * Tests for the {@link ModuloAxis} class.
+ */
+public class ModuloAxisTests extends TestCase {
+
+    /**
+     * Returns the tests as a test suite.
+     *
+     * @return The test suite.
+     */
+    public static Test suite() {
+        return new TestSuite(ModuloAxisTests.class);
+    }
+
+    /**
+     * Constructs a new set of tests.
+     *
+     * @param name  the name of the tests.
+     */
+    public ModuloAxisTests(String name) {
+        super(name);
+    }
+    
+    /**
+     * Confirm that cloning works.
+     */
+    public void testCloning() {
+        ModuloAxis a1 = new ModuloAxis("Test", new Range(0.0, 1.0));
+        ModuloAxis a2 = null;
+        try {
+            a2 = (ModuloAxis) a1.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(a1 != a2);
+        assertTrue(a1.getClass() == a2.getClass());
+        assertTrue(a1.equals(a2));
+    }
+
+    /**
+     * Confirm that the equals method can distinguish all the required fields.
+     */
+    public void testEquals() {
+        ModuloAxis a1 = new ModuloAxis("Test", new Range(0.0, 1.0));
+        ModuloAxis a2 = new ModuloAxis("Test", new Range(0.0, 1.0));
+        assertTrue(a1.equals(a2));
+
+        a1.setDisplayRange(0.1, 1.1);
+        assertFalse(a1.equals(a2));
+        a2.setDisplayRange(0.1, 1.1);
+        assertTrue(a1.equals(a2));
+    }
+
+    /**
+     * Two objects that are equal are required to return the same hashCode. 
+     */
+    public void testHashCode() {
+        ModuloAxis a1 = new ModuloAxis("Test", new Range(0.0, 1.0));
+        ModuloAxis a2 = new ModuloAxis("Test", new Range(0.0, 1.0));
+        assertTrue(a1.equals(a2));
+        int h1 = a1.hashCode();
+        int h2 = a2.hashCode();
+        assertEquals(h1, h2);
+    }
+    
+    /**
+     * Serialize an instance, restore it, and check for equality.
+     */
+    public void testSerialization() {
+
+        ModuloAxis a1 = new ModuloAxis("Test", new Range(0.0, 1.0));
+        ModuloAxis a2 = null;
+
+        try {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(buffer);
+            out.writeObject(a1);
+            out.close();
+
+            ObjectInput in = new ObjectInputStream(
+                    new ByteArrayInputStream(buffer.toByteArray()));
+            a2 = (ModuloAxis) in.readObject();
+            in.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(a1, a2);
+
+    }
+
+}
