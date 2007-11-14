b6b62b3601a66e4dd35e3707ca749db710a305df
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2007-11-14 16:53:52 +0000

    2007-11-14  David Gilbert  <david.gilbert@object-refinery.com>
    	* source/org/jfree/chart/renderer/category/StatisticalBarRenderer.java
    	(errorIndicatorStroke): New field,
    	(StatisticalBarRenderer): Initialise new field,
    	(getErrorIndicatorStroke): New method,
    	(setErrorIndicatorStroke): Likewise,
    	(drawHorizontalItem): Fix drawBarOutline and gradientPaintTransformer
    	omissions, and use new errorIndicatorStroke field,
    	(drawVerticalItem): Likewise,
    	(equals): Updated for new field,
    	(writeObject): Likewise,
    	(readObject): Likewise.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/category/StatisticalBarRenderer.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/category/StatisticalBarRenderer.java
@@ -50,14 +50,19 @@
  * 12-Jul-2006 : Added support for item labels (DG);
  * 02-Feb-2007 : Removed author tags all over JFreeChart sources (DG);
  * 28-Aug-2007 : Fixed NullPointerException - see bug 1779941 (DG);
+ * 14-Nov-2007 : Added errorIndicatorStroke, and fixed bugs with drawBarOutline
+ *               and gradientPaintTransformer attributes being ignored (DG);
  * 
  */
 
 package org.jfree.chart.renderer.category;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.Paint;
+import java.awt.Stroke;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.io.IOException;
@@ -75,7 +80,9 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.statistics.StatisticalCategoryDataset;
 import org.jfree.io.SerialUtilities;
+import org.jfree.ui.GradientPaintTransformer;
 import org.jfree.ui.RectangleEdge;
+import org.jfree.util.ObjectUtilities;
 import org.jfree.util.PaintUtilities;
 import org.jfree.util.PublicCloneable;
 
@@ -95,11 +102,19 @@ public class StatisticalBarRenderer extends BarRenderer
     private transient Paint errorIndicatorPaint;
     
     /**
+     * The stroke used to draw the error indicators. 
+     * 
+     * @since 1.0.8
+     */
+    private transient Stroke errorIndicatorStroke;
+    
+    /**
      * Default constructor.
      */
     public StatisticalBarRenderer() {
         super();
         this.errorIndicatorPaint = Color.gray;
+        this.errorIndicatorStroke = new BasicStroke(1.0f);
     }
 
     /**
@@ -124,6 +139,35 @@ public class StatisticalBarRenderer extends BarRenderer
      */
     public void setErrorIndicatorPaint(Paint paint) {
         this.errorIndicatorPaint = paint;
+        notifyListeners(new RendererChangeEvent(this));
+    }
+    
+    /**
+     * Returns the stroke used to draw the error indicators.  If this is 
+     * <code>null</code>, the renderer will use the item outline stroke).
+     * 
+     * @return The stroke (possibly <code>null</code>).
+     * 
+     * @see #setErrorIndicatorPaint(Paint)
+     *
+     * @since 1.0.8
+     */
+    public Stroke getErrorIndicatorStroke() {
+        return this.errorIndicatorStroke;
+    }
+    
+    /**
+     * Sets the stroke used to draw the error indicators, and sends a 
+     * {@link RendererChangeEvent} to all registered listeners.  If you set
+     * this to <code>null</code>, the renderer will use the item outline
+     * stroke.
+     * 
+     * @param stroke  the stroke (<code>null</code> permitted).
+     * 
+     * @since 1.0.8
+     */
+    public void setErrorIndicatorStroke(Stroke stroke) {
+        this.errorIndicatorStroke = stroke;
         notifyListeners(new RendererChangeEvent(this));
     }
     
@@ -261,13 +305,23 @@ public class StatisticalBarRenderer extends BarRenderer
 
         Rectangle2D bar = new Rectangle2D.Double(rectX, rectY, rectWidth, 
                 rectHeight);
-        Paint seriesPaint = getItemPaint(row, column);
-        g2.setPaint(seriesPaint);
+        Paint itemPaint = getItemPaint(row, column);
+        GradientPaintTransformer t = getGradientPaintTransformer();
+        if (t != null && itemPaint instanceof GradientPaint) {
+            itemPaint = t.transform((GradientPaint) itemPaint, bar);
+        }
+        g2.setPaint(itemPaint);
         g2.fill(bar);
-        if (state.getBarWidth() > 3) {
-            g2.setStroke(getItemStroke(row, column));
-            g2.setPaint(getItemOutlinePaint(row, column));
-            g2.draw(bar);
+        // draw the outline...
+        if (isDrawBarOutline() 
+                && state.getBarWidth() > BAR_OUTLINE_WIDTH_THRESHOLD) {
+            Stroke stroke = getItemOutlineStroke(row, column);
+            Paint paint = getItemOutlinePaint(row, column);
+            if (stroke != null && paint != null) {
+                g2.setStroke(stroke);
+                g2.setPaint(paint);
+                g2.draw(bar);
+            }
         }
 
         // standard deviation lines
@@ -284,6 +338,12 @@ public class StatisticalBarRenderer extends BarRenderer
             }
             else {
                 g2.setPaint(getItemOutlinePaint(row, column));   
+            }
+            if (this.errorIndicatorStroke != null) {
+                g2.setStroke(this.errorIndicatorStroke);
+            }
+            else {
+                g2.setStroke(getItemOutlineStroke(row, column));
             }
             Line2D line = null;
             line = new Line2D.Double(lowVal, rectY + rectHeight / 2.0d, 
@@ -404,13 +464,23 @@ public class StatisticalBarRenderer extends BarRenderer
 
         Rectangle2D bar = new Rectangle2D.Double(rectX, rectY, rectWidth, 
                 rectHeight);
-        Paint seriesPaint = getItemPaint(row, column);
-        g2.setPaint(seriesPaint);
+        Paint itemPaint = getItemPaint(row, column);
+        GradientPaintTransformer t = getGradientPaintTransformer();
+        if (t != null && itemPaint instanceof GradientPaint) {
+            itemPaint = t.transform((GradientPaint) itemPaint, bar);
+        }
+        g2.setPaint(itemPaint);
         g2.fill(bar);
-        if (state.getBarWidth() > 3) {
-            g2.setStroke(getItemStroke(row, column));
-            g2.setPaint(getItemOutlinePaint(row, column));
-            g2.draw(bar);
+        // draw the outline...
+        if (isDrawBarOutline() 
+                && state.getBarWidth() > BAR_OUTLINE_WIDTH_THRESHOLD) {
+            Stroke stroke = getItemOutlineStroke(row, column);
+            Paint paint = getItemOutlinePaint(row, column);
+            if (stroke != null && paint != null) {
+                g2.setStroke(stroke);
+                g2.setPaint(paint);
+                g2.draw(bar);
+            }
         }
 
         // standard deviation lines
@@ -428,6 +498,13 @@ public class StatisticalBarRenderer extends BarRenderer
             else {
                 g2.setPaint(getItemOutlinePaint(row, column));   
             }
+            if (this.errorIndicatorStroke != null) {
+                g2.setStroke(this.errorIndicatorStroke);
+            }
+            else {
+                g2.setStroke(getItemOutlineStroke(row, column));
+            }
+            
             Line2D line = null;
             line = new Line2D.Double(rectX + rectWidth / 2.0d, lowVal,
                                      rectX + rectWidth / 2.0d, highVal);
@@ -476,6 +553,10 @@ public class StatisticalBarRenderer extends BarRenderer
                 that.errorIndicatorPaint)) {
             return false;
         }
+        if (!ObjectUtilities.equal(this.errorIndicatorStroke, 
+                that.errorIndicatorStroke)) {
+            return false;
+        }
         return true;
     }
     
@@ -489,6 +570,7 @@ public class StatisticalBarRenderer extends BarRenderer
     private void writeObject(ObjectOutputStream stream) throws IOException {
         stream.defaultWriteObject();
         SerialUtilities.writePaint(this.errorIndicatorPaint, stream);
+        SerialUtilities.writeStroke(this.errorIndicatorStroke, stream);
     }
 
     /**
@@ -503,6 +585,7 @@ public class StatisticalBarRenderer extends BarRenderer
         throws IOException, ClassNotFoundException {
         stream.defaultReadObject();
         this.errorIndicatorPaint = SerialUtilities.readPaint(stream);
+        this.errorIndicatorStroke = SerialUtilities.readStroke(stream);
     }
 
 }
