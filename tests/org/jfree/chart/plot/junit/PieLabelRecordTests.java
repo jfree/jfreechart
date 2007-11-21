d3133cdbe806fe6656d16f3a07278226c90afbb1
David Gilbert <jfree@users.noreply.github.com>
2007-11-21 15:27:46 +0000

    Committed.

---
+++ tests/org/jfree/chart/plot/junit/PieLabelRecordTests.java
@@ -0,0 +1,164 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
+ * USA.  
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * in the United States and other countries.]
+ *
+ * ------------------------
+ * PieLabelRecordTests.java
+ * ------------------------
+ * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 21-Nov-2007 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.chart.plot.junit;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jfree.chart.plot.CategoryMarker;
+import org.jfree.chart.plot.PieLabelRecord;
+import org.jfree.text.TextBox;
+
+/**
+ * Some tests for the {@link CategoryMarker} class.
+ */
+public class PieLabelRecordTests extends TestCase {
+
+    /**
+     * Returns the tests as a test suite.
+     *
+     * @return The test suite.
+     */
+    public static Test suite() {
+        return new TestSuite(PieLabelRecordTests.class);
+    }
+
+    /**
+     * Constructs a new set of tests.
+     *
+     * @param name  the name of the tests.
+     */
+    public PieLabelRecordTests(String name) {
+        super(name);
+    }
+
+    /**
+     * Confirm that the equals method can distinguish all the required fields.
+     */
+    public void testEquals() {
+        PieLabelRecord p1 = new PieLabelRecord("A", 1.0, 2.0, new TextBox("B"), 
+                3.0, 4.0, 5.0);
+        PieLabelRecord p2 = new PieLabelRecord("A", 1.0, 2.0, new TextBox("B"), 
+                3.0, 4.0, 5.0);
+        assertTrue(p1.equals(p2));
+        assertTrue(p2.equals(p1));
+        
+        p1 = new PieLabelRecord("B", 1.0, 2.0, new TextBox("B"), 3.0, 4.0, 5.0);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.0, 2.0, new TextBox("B"), 3.0, 4.0, 5.0);
+        assertTrue(p1.equals(p2));
+
+        p1 = new PieLabelRecord("B", 1.1, 2.0, new TextBox("B"), 3.0, 4.0, 5.0);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.1, 2.0, new TextBox("B"), 3.0, 4.0, 5.0);
+        assertTrue(p1.equals(p2));
+
+        p1 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("B"), 3.0, 4.0, 5.0);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("B"), 3.0, 4.0, 5.0);
+        assertTrue(p1.equals(p2));
+
+        p1 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.0, 4.0, 5.0);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.0, 4.0, 5.0);
+        assertTrue(p1.equals(p2));
+
+        p1 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.3, 4.0, 5.0);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.3, 4.0, 5.0);
+        assertTrue(p1.equals(p2));
+
+        p1 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.3, 4.4, 5.0);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.3, 4.4, 5.0);
+        assertTrue(p1.equals(p2));
+
+        p1 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.3, 4.4, 5.5);
+        assertFalse(p1.equals(p2));
+        p2 = new PieLabelRecord("B", 1.1, 2.2, new TextBox("C"), 3.3, 4.4, 5.5);
+        assertTrue(p1.equals(p2));
+    
+    }
+        
+    /**
+     * Confirm that cloning is not implemented.
+     */
+    public void testCloning() {
+        PieLabelRecord p1 = new PieLabelRecord("A", 1.0, 2.0, new TextBox("B"), 
+                3.0, 4.0, 5.0);
+        assertFalse(p1 instanceof Cloneable);
+    }
+
+   /**
+     * Serialize an instance, restore it, and check for equality.
+     */
+    public void testSerialization() {
+
+        PieLabelRecord p1 = new PieLabelRecord("A", 1.0, 2.0, new TextBox("B"), 
+                3.0, 4.0, 5.0);
+        PieLabelRecord p2 = null;
+        try {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(buffer);
+            out.writeObject(p1);
+            out.close();
+
+            ObjectInput in = new ObjectInputStream(
+                    new ByteArrayInputStream(buffer.toByteArray()));
+            p2 = (PieLabelRecord) in.readObject();
+            in.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        boolean b = p1.equals(p2);
+        assertTrue(b);
+
+    }
+}
---
+++ tests/org/jfree/chart/renderer/junit/OutlierTests.java
@@ -0,0 +1,130 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
+ * USA.  
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * in the United States and other countries.]
+ *
+ * -----------------
+ * OutlierTests.java
+ * -----------------
+ * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 21-Nov-2007 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.chart.renderer.junit;
+
+import java.awt.geom.Point2D;
+import java.io.Serializable;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jfree.chart.renderer.Outlier;
+
+
+/**
+ * Tests for the {@link Outlier} class.
+ */
+public class OutlierTests extends TestCase {
+
+    /**
+     * Returns the tests as a test suite.
+     *
+     * @return The test suite.
+     */
+    public static Test suite() {
+        return new TestSuite(OutlierTests.class);
+    }
+
+    /**
+     * Constructs a new set of tests.
+     *
+     * @param name  the name of the tests.
+     */
+    public OutlierTests(String name) {
+        super(name);
+    }
+    
+    private static final double EPSILON = 0.000000001;
+    
+    /** 
+     * Simple check for the default constructor.
+     */
+    public void testConstructor() {
+        Outlier out = new Outlier(1.0, 2.0, 3.0);
+        assertEquals(-2.0, out.getX(), EPSILON);
+        assertEquals(-1.0, out.getY(), EPSILON);
+        assertEquals(3.0, out.getRadius(), EPSILON);
+    }
+        
+    /**
+     * A test for the equals() method.
+     */
+    public void testEquals() {
+        Outlier out1 = new Outlier(1.0, 2.0, 3.0);
+        Outlier out2 = new Outlier(1.0, 2.0, 3.0);
+        assertTrue(out1.equals(out2));
+        assertTrue(out2.equals(out1));
+        
+        out1.setPoint(new Point2D.Double(2.0, 2.0));
+        assertFalse(out1.equals(out2));
+        out2.setPoint(new Point2D.Double(2.0, 2.0));
+        assertTrue(out1.equals(out2));
+
+        out1.setPoint(new Point2D.Double(2.0, 3.0));
+        assertFalse(out1.equals(out2));
+        out2.setPoint(new Point2D.Double(2.0, 3.0));
+        assertTrue(out1.equals(out2));
+
+        out1.setRadius(4.0);
+        assertFalse(out1.equals(out2));
+        out2.setRadius(4.0);
+        assertTrue(out1.equals(out2));
+    }
+    
+    /**
+     * Confirm that cloning is not implemented.
+     */
+    public void testCloning() {
+        Outlier out1 = new Outlier(1.0, 2.0, 3.0);
+        assertFalse(out1 instanceof Cloneable);
+    }
+    
+    /**
+     * Confirm that serialization is not implemented.
+     */
+    public void testSerialization() {
+        Outlier out1 = new Outlier(1.0, 2.0, 3.0);
+        assertFalse(out1 instanceof Serializable);
+    }
+
+}
+
