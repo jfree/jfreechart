721b1f2b63c3aca8ceff0a52164d221df476500f
David Gilbert <jfree@users.noreply.github.com>
2007-11-23 12:00:37 +0000

    New test.

--- tests/org/jfree/data/category/junit/DefaultCategoryDatasetTests.java
+++ tests/org/jfree/data/category/junit/DefaultCategoryDatasetTests.java
@@ -350,4 +350,65 @@ public class DefaultCategoryDatasetTests extends TestCase {
     	d.addValue(3.0, "R2", "C2");
     	assertEquals(3.0, d.getValue("R2", "C2").doubleValue(), EPSILON);
     }
+    
+    /**
+     * Some checks for the removeColumn(Comparable) method.
+     */
+    public void testRemoveColumn() {
+        DefaultCategoryDataset d = new DefaultCategoryDataset();
+        d.addValue(1.0, "R1", "C1");
+        d.addValue(2.0, "R2", "C2");
+        assertEquals(2, d.getColumnCount());
+        d.removeColumn("C2");
+        assertEquals(1, d.getColumnCount());
+        
+        boolean pass = false;
+        try {
+            d.removeColumn("XXX");
+        }
+        catch (UnknownKeyException e) {
+            pass = true;
+        }
+        assertTrue(pass);
+        
+        pass = false;
+        try {
+            d.removeColumn(null);
+        }
+        catch (IllegalArgumentException e) {
+            pass = true;
+        }
+        assertTrue(pass);
+    }
+
+    /**
+     * Some checks for the removeRow(Comparable) method.
+     */
+    public void testRemoveRow() {
+        DefaultCategoryDataset d = new DefaultCategoryDataset();
+        d.addValue(1.0, "R1", "C1");
+        d.addValue(2.0, "R2", "C2");
+        assertEquals(2, d.getRowCount());
+        d.removeRow("R2");
+        assertEquals(1, d.getRowCount());
+        
+        boolean pass = false;
+        try {
+            d.removeRow("XXX");
+        }
+        catch (UnknownKeyException e) {
+            pass = true;
+        }
+        assertTrue(pass);
+        
+        pass = false;
+        try {
+            d.removeRow(null);
+        }
+        catch (IllegalArgumentException e) {
+            pass = true;
+        }
+        assertTrue(pass);
+    }
+
 }
