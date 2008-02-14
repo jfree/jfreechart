ab7b9144e507546595de39a60c8a3adfab3885b7
David Gilbert <jfree@users.noreply.github.com>
2008-02-14 03:39:39 +0000

    Modify version number so we know we're working from SVN.

--- source/org/jfree/chart/resources/JFreeChartResources.java
+++ source/org/jfree/chart/resources/JFreeChartResources.java
@@ -55,7 +55,7 @@ public class JFreeChartResources extends ListResourceBundle {
     /** The resources to be localised. */
     private static final Object[][] CONTENTS = {
         {"project.name",      "JFreeChart"},
-        {"project.version",   "1.0.9"},
+        {"project.version",   "1.0.9+svn"},
         {"project.info",      "http://www.jfree.org/jfreechart/index.html"},
         {"project.copyright", 
          "(C)opyright 2000-2008, by Object Refinery Limited and Contributors"}
