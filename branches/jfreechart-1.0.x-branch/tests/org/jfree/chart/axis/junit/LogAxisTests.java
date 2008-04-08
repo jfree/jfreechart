757fee5414742e6858d5073c62e937ff09b13e87
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-04-08 20:42:06 +0000

    Fixed testEquals() and removed trailing whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/axis/junit/LogAxisTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/axis/junit/LogAxisTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------
  * LogAxisTests.java
  * -----------------
- * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2007, 2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,6 +35,7 @@
  * Changes
  * -------
  * 11-Jul-2007 : Version 1 (DG);
+ * 08-Apr-2008 : Fixed incorrect testEquals() method (DG);
  *
  */
 
@@ -85,7 +86,7 @@ public class LogAxisTests extends TestCase {
     public LogAxisTests(String name) {
         super(name);
     }
-    
+
     /**
      * Confirm that cloning works.
      */
@@ -107,7 +108,6 @@ public class LogAxisTests extends TestCase {
      * Confirm that the equals method can distinguish all the required fields.
      */
     public void testEquals() {
-        
         LogAxis a1 = new LogAxis("Test");
         LogAxis a2 = new LogAxis("Test");
         assertTrue(a1.equals(a2));
@@ -116,20 +116,20 @@ public class LogAxisTests extends TestCase {
         assertFalse(a1.equals(a2));
         a2.setBase(2.0);
         assertTrue(a1.equals(a2));
-        
+
         a1.setSmallestValue(0.1);
         assertFalse(a1.equals(a2));
         a2.setSmallestValue(0.1);
         assertTrue(a1.equals(a2));
-        
-        a1.setMinorTickCount(9);
+
+        a1.setMinorTickCount(8);
         assertFalse(a1.equals(a2));
-        a2.setMinorTickCount(9);
+        a2.setMinorTickCount(8);
         assertTrue(a1.equals(a2));
     }
 
     /**
-     * Two objects that are equal are required to return the same hashCode. 
+     * Two objects that are equal are required to return the same hashCode.
      */
     public void testHashCode() {
         LogAxis a1 = new LogAxis("Test");
@@ -141,33 +141,33 @@ public class LogAxisTests extends TestCase {
     }
 
     private static final double EPSILON = 0.0000001;
-    
+
     /**
      * Test the translation of Java2D values to data values.
      */
     public void testTranslateJava2DToValue() {
         LogAxis axis = new LogAxis();
-        axis.setRange(50.0, 100.0); 
+        axis.setRange(50.0, 100.0);
         Rectangle2D dataArea = new Rectangle2D.Double(10.0, 50.0, 400.0, 300.0);
-        double y1 = axis.java2DToValue(75.0, dataArea, RectangleEdge.LEFT);  
-        assertEquals(94.3874312681693, y1, EPSILON); 
-        double y2 = axis.java2DToValue(75.0, dataArea, RectangleEdge.RIGHT);   
-        assertEquals(94.3874312681693, y2, EPSILON); 
-        double x1 = axis.java2DToValue(75.0, dataArea, RectangleEdge.TOP);   
-        assertEquals(55.961246381405, x1, EPSILON); 
-        double x2 = axis.java2DToValue(75.0, dataArea, RectangleEdge.BOTTOM);   
-        assertEquals(55.961246381405, x2, EPSILON); 
+        double y1 = axis.java2DToValue(75.0, dataArea, RectangleEdge.LEFT);
+        assertEquals(94.3874312681693, y1, EPSILON);
+        double y2 = axis.java2DToValue(75.0, dataArea, RectangleEdge.RIGHT);
+        assertEquals(94.3874312681693, y2, EPSILON);
+        double x1 = axis.java2DToValue(75.0, dataArea, RectangleEdge.TOP);
+        assertEquals(55.961246381405, x1, EPSILON);
+        double x2 = axis.java2DToValue(75.0, dataArea, RectangleEdge.BOTTOM);
+        assertEquals(55.961246381405, x2, EPSILON);
         axis.setInverted(true);
-        double y3 = axis.java2DToValue(75.0, dataArea, RectangleEdge.LEFT);  
-        assertEquals(52.9731547179647, y3, EPSILON); 
-        double y4 = axis.java2DToValue(75.0, dataArea, RectangleEdge.RIGHT);   
-        assertEquals(52.9731547179647, y4, EPSILON); 
-        double x3 = axis.java2DToValue(75.0, dataArea, RectangleEdge.TOP);   
-        assertEquals(89.3475453695651, x3, EPSILON); 
-        double x4 = axis.java2DToValue(75.0, dataArea, RectangleEdge.BOTTOM);   
-        assertEquals(89.3475453695651, x4, EPSILON); 
+        double y3 = axis.java2DToValue(75.0, dataArea, RectangleEdge.LEFT);
+        assertEquals(52.9731547179647, y3, EPSILON);
+        double y4 = axis.java2DToValue(75.0, dataArea, RectangleEdge.RIGHT);
+        assertEquals(52.9731547179647, y4, EPSILON);
+        double x3 = axis.java2DToValue(75.0, dataArea, RectangleEdge.TOP);
+        assertEquals(89.3475453695651, x3, EPSILON);
+        double x4 = axis.java2DToValue(75.0, dataArea, RectangleEdge.BOTTOM);
+        assertEquals(89.3475453695651, x4, EPSILON);
     }
-    
+
     /**
      * Serialize an instance, restore it, and check for equality.
      */
@@ -203,22 +203,22 @@ public class LogAxisTests extends TestCase {
         dataset.setValue(100.0, "Row 1", "Column 1");
         dataset.setValue(200.0, "Row 1", "Column 2");
         JFreeChart chart = ChartFactory.createBarChart(
-            "Test", 
+            "Test",
             "Categories",
             "Value",
             dataset,
             PlotOrientation.VERTICAL,
-            false, 
+            false,
             false,
             false
         );
         CategoryPlot plot = (CategoryPlot) chart.getPlot();
         LogAxis axis = new LogAxis("Log(Y)");
         plot.setRangeAxis(axis);
-        assertEquals(0.0, axis.getLowerBound(), EPSILON);    
-        assertEquals(2.6066426411261268E7, axis.getUpperBound(), EPSILON); 
+        assertEquals(0.0, axis.getLowerBound(), EPSILON);
+        assertEquals(2.6066426411261268E7, axis.getUpperBound(), EPSILON);
     }
-    
+
     /**
      * A simple test for the auto-range calculation looking at a
      * NumberAxis used as the range axis for a CategoryPlot.  In this
@@ -234,18 +234,18 @@ public class LogAxisTests extends TestCase {
         CategoryPlot plot = (CategoryPlot) chart.getPlot();
         LogAxis axis = new LogAxis("Log(Y)");
         plot.setRangeAxis(axis);
-        assertEquals(96.59363289248458, axis.getLowerBound(), EPSILON);    
-        assertEquals(207.0529847682752, axis.getUpperBound(), EPSILON);    
-        
+        assertEquals(96.59363289248458, axis.getLowerBound(), EPSILON);
+        assertEquals(207.0529847682752, axis.getUpperBound(), EPSILON);
+
         // now replacing the dataset should update the axis range...
         DefaultCategoryDataset dataset2 = new DefaultCategoryDataset();
         dataset2.setValue(900.0, "Row 1", "Column 1");
         dataset2.setValue(1000.0, "Row 1", "Column 2");
         plot.setDataset(dataset2);
-        assertEquals(895.2712433374774, axis.getLowerBound(), EPSILON);    
-        assertEquals(1005.2819262292991, axis.getUpperBound(), EPSILON);    
+        assertEquals(895.2712433374774, axis.getLowerBound(), EPSILON);
+        assertEquals(1005.2819262292991, axis.getUpperBound(), EPSILON);
     }
-        
+
     /**
      * Checks that the auto-range for the domain axis on an XYPlot is
      * working as expected.
@@ -258,22 +258,22 @@ public class LogAxisTests extends TestCase {
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(series);
         JFreeChart chart = ChartFactory.createScatterPlot(
-            "Test", 
+            "Test",
             "X",
             "Y",
             dataset,
             PlotOrientation.VERTICAL,
-            false, 
+            false,
             false,
             false
         );
         XYPlot plot = (XYPlot) chart.getPlot();
         LogAxis axis = new LogAxis("Log(Y)");
         plot.setRangeAxis(axis);
-        assertEquals(0.9465508226401592, axis.getLowerBound(), EPSILON);    
-        assertEquals(3.1694019256486126, axis.getUpperBound(), EPSILON);    
+        assertEquals(0.9465508226401592, axis.getLowerBound(), EPSILON);
+        assertEquals(3.1694019256486126, axis.getUpperBound(), EPSILON);
     }
-    
+
     /**
      * Checks that the auto-range for the range axis on an XYPlot is
      * working as expected.
@@ -286,22 +286,22 @@ public class LogAxisTests extends TestCase {
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(series);
         JFreeChart chart = ChartFactory.createScatterPlot(
-            "Test", 
+            "Test",
             "X",
             "Y",
             dataset,
             PlotOrientation.VERTICAL,
-            false, 
+            false,
             false,
             false
         );
         XYPlot plot = (XYPlot) chart.getPlot();
         LogAxis axis = new LogAxis("Log(Y)");
         plot.setRangeAxis(axis);
-        assertEquals(0.9465508226401592, axis.getLowerBound(), EPSILON);    
-        assertEquals(3.1694019256486126, axis.getUpperBound(), EPSILON);    
+        assertEquals(0.9465508226401592, axis.getLowerBound(), EPSILON);
+        assertEquals(3.1694019256486126, axis.getUpperBound(), EPSILON);
     }
-    
+
     /**
      * Some checks for the setLowerBound() method.
      */
