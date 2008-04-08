ac5854838c46c92462a9638707fcc4f32f32e2d1
David Gilbert <jfree@users.noreply.github.com>
2008-04-08 20:27:36 +0000

    Removed trailing whitespace.

--- tests/org/jfree/chart/renderer/xy/junit/HighLowRendererTests.java
+++ tests/org/jfree/chart/renderer/xy/junit/HighLowRendererTests.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -------------------------
@@ -92,25 +92,25 @@ public class HighLowRendererTests extends TestCase {
         HighLowRenderer r1 = new HighLowRenderer();
         HighLowRenderer r2 = new HighLowRenderer();
         assertEquals(r1, r2);
-        
+
         // drawOpenTicks
         r1.setDrawOpenTicks(false);
         assertFalse(r1.equals(r2));
         r2.setDrawOpenTicks(false);
         assertTrue(r1.equals(r2));
-        
+
         // drawCloseTicks
         r1.setDrawCloseTicks(false);
         assertFalse(r1.equals(r2));
         r2.setDrawCloseTicks(false);
         assertTrue(r1.equals(r2));
-        
+
         // openTickPaint
         r1.setOpenTickPaint(Color.red);
         assertFalse(r1.equals(r2));
         r2.setOpenTickPaint(Color.red);
         assertTrue(r1.equals(r2));
-        
+
         // closeTickPaint
         r1.setCloseTickPaint(Color.blue);
         assertFalse(r1.equals(r2));
@@ -119,7 +119,7 @@ public class HighLowRendererTests extends TestCase {
     }
 
     /**
-     * Two objects that are equal are required to return the same hashCode. 
+     * Two objects that are equal are required to return the same hashCode.
      */
     public void testHashcode() {
         HighLowRenderer r1 = new HighLowRenderer();
@@ -129,7 +129,7 @@ public class HighLowRendererTests extends TestCase {
         int h2 = r2.hashCode();
         assertEquals(h1, h2);
     }
-    
+
     /**
      * Confirm that cloning works.
      */
@@ -181,30 +181,29 @@ public class HighLowRendererTests extends TestCase {
      */
     public void testFindRangeBounds() {
         HighLowRenderer renderer = new HighLowRenderer();
-        
-        OHLCDataItem item1 = new OHLCDataItem(new Date(1L), 2.0, 4.0, 1.0, 3.0, 
+
+        OHLCDataItem item1 = new OHLCDataItem(new Date(1L), 2.0, 4.0, 1.0, 3.0,
                 100);
-        OHLCDataset dataset = new DefaultOHLCDataset("S1", 
+        OHLCDataset dataset = new DefaultOHLCDataset("S1",
                 new OHLCDataItem[] {item1});
         Range range = renderer.findRangeBounds(dataset);
         assertEquals(new Range(1.0, 4.0), range);
 
-        OHLCDataItem item2 = new OHLCDataItem(new Date(1L), -1.0, 3.0, -1.0, 
+        OHLCDataItem item2 = new OHLCDataItem(new Date(1L), -1.0, 3.0, -1.0,
                 3.0, 100);
-        dataset = new DefaultOHLCDataset("S1", new OHLCDataItem[] {item1, 
+        dataset = new DefaultOHLCDataset("S1", new OHLCDataItem[] {item1,
                 item2});
         range = renderer.findRangeBounds(dataset);
         assertEquals(new Range(-1.0, 4.0), range);
-        
+
         // try an empty dataset - should return a null range
         dataset = new DefaultOHLCDataset("S1", new OHLCDataItem[] {});
         range = renderer.findRangeBounds(dataset);
         assertNull(range);
-        
+
         // try a null dataset - should return a null range
         range = renderer.findRangeBounds(null);
         assertNull(range);
     }
 
-    
 }
