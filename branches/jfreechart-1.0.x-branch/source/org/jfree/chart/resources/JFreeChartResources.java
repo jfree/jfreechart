3e29175995c81262c826b9e94a362733d78910b7
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-04-09 12:16:51 +0000

    Updated version number to 1.0.10.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/resources/JFreeChartResources.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/resources/JFreeChartResources.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------------
@@ -55,9 +55,9 @@ public class JFreeChartResources extends ListResourceBundle {
     /** The resources to be localised. */
     private static final Object[][] CONTENTS = {
         {"project.name",      "JFreeChart"},
-        {"project.version",   "1.0.9+svn"},
+        {"project.version",   "1.0.10"},
         {"project.info",      "http://www.jfree.org/jfreechart/index.html"},
-        {"project.copyright", 
+        {"project.copyright",
          "(C)opyright 2000-2008, by Object Refinery Limited and Contributors"}
     };
 
