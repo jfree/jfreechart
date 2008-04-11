a5d3718565f315f29ebd628c286fbcf27e00d84b
David Gilbert <jfree@users.noreply.github.com>
2008-04-11 14:49:57 +0000

    2008-04-11  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/chart/urls/CustomXYURLGenerator.java
        (clone): Implemented,
        * tests/org/jfree/chart/urls/junit/CustomPieURLGeneratorTests.java: 
        New file,
        * tests/org/jfree/chart/urls/junit/CustomXYURLGeneratorTests.java
        (testEquals): New test method,
        (testCloning): Likewise,
        * tests/org/jfree/chart/urls/junit/UrlsPackageTests.java
        (suite): Added CustomPieURLGeneratorTests.

--- tests/org/jfree/chart/urls/junit/UrlsPackageTests.java
+++ tests/org/jfree/chart/urls/junit/UrlsPackageTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------------
  * UrlsPackageTests.java
  * ---------------------
- * (C) Copyright 2003-2007, by Object Refinery Limited.
+ * (C) Copyright 2003-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -39,6 +39,7 @@
  * 01-Mar-2004 : Added StandardXYURLGeneratorTests (DG);
  * 19-Jan-2005 : Added main() method to run JUnit in text mode (DG);
  * 17-Apr-2007 : Added TimeSeriesURLGeneratorTests (DG);
+ * 11-Apr-2008 : Added new tests (DG);
  *
  */
 
@@ -52,7 +53,6 @@ import junit.framework.TestSuite;
  * A collection of tests for the org.jfree.chart.urls package.
  * <P>
  * These tests can be run using JUnit (http://www.junit.org).
- *
  */
 public class UrlsPackageTests extends TestCase {
 
@@ -63,6 +63,7 @@ public class UrlsPackageTests extends TestCase {
      */
     public static Test suite() {
         TestSuite suite = new TestSuite("org.jfree.chart.urls");
+        suite.addTestSuite(CustomPieURLGeneratorTests.class);
         suite.addTestSuite(CustomXYURLGeneratorTests.class);
         suite.addTestSuite(StandardCategoryURLGeneratorTests.class);
         suite.addTestSuite(StandardPieURLGeneratorTests.class);
@@ -79,10 +80,10 @@ public class UrlsPackageTests extends TestCase {
     public UrlsPackageTests(String name) {
         super(name);
     }
-    
+
     /**
      * Runs the test suite using JUnit's text-based runner.
-     * 
+     *
      * @param args  ignored.
      */
     public static void main(String[] args) {
