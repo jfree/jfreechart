fb3b3f966965d0c913203dc1255afa7781fb0cc8
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-04-23 08:50:29 +0000

    Removed whitespace.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/urls/StandardCategoryURLGenerator.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/urls/StandardCategoryURLGenerator.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------------------------
  * StandardCategoryURLGenerator.java
  * ---------------------------------
- * (C) Copyright 2002-2007, by Richard Atkinson and Contributors.
+ * (C) Copyright 2002-2008, by Richard Atkinson and Contributors.
  *
  * Original Author:  Richard Atkinson;
  * Contributors:     David Gilbert (for Object Refinery Limited);
@@ -36,8 +36,8 @@
  * Changes:
  * --------
  * 05-Aug-2002 : Version 1, contributed by Richard Atkinson;
- * 29-Aug-2002 : Reversed seriesParameterName and itemParameterName in 
- *               constructor.  Never should have been the other way round.  
+ * 29-Aug-2002 : Reversed seriesParameterName and itemParameterName in
+ *               constructor.  Never should have been the other way round.
  *               Also updated JavaDoc (RA);
  * 09-Oct-2002 : Fixed errors reported by Checkstyle (DG);
  * 05-Nov-2002 : Base dataset is now TableDataset not CategoryDataset (DG);
@@ -60,15 +60,15 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.util.ObjectUtilities;
 
 /**
- * A URL generator that can be assigned to a 
+ * A URL generator that can be assigned to a
  * {@link org.jfree.chart.renderer.category.CategoryItemRenderer}.
  */
-public class StandardCategoryURLGenerator implements CategoryURLGenerator, 
-                                                     Cloneable, Serializable {
+public class StandardCategoryURLGenerator implements CategoryURLGenerator,
+        Cloneable, Serializable {
 
     /** For serialization. */
     private static final long serialVersionUID = 2276668053074881909L;
-    
+
     /** Prefix to the URL */
     private String prefix = "index.html";
 
@@ -92,7 +92,7 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
      */
     public StandardCategoryURLGenerator(String prefix) {
         if (prefix == null) {
-            throw new IllegalArgumentException("Null 'prefix' argument.");   
+            throw new IllegalArgumentException("Null 'prefix' argument.");
         }
         this.prefix = prefix;
     }
@@ -101,7 +101,7 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
      * Constructor that overrides all the defaults.
      *
      * @param prefix  the prefix to the URL (<code>null</code> not permitted).
-     * @param seriesParameterName  the name of the series parameter to go in 
+     * @param seriesParameterName  the name of the series parameter to go in
      *                             each URL (<code>null</code> not permitted).
      * @param categoryParameterName  the name of the category parameter to go in
      *                               each URL (<code>null</code> not permitted).
@@ -111,15 +111,15 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
                                         String categoryParameterName) {
 
         if (prefix == null) {
-            throw new IllegalArgumentException("Null 'prefix' argument.");   
+            throw new IllegalArgumentException("Null 'prefix' argument.");
         }
         if (seriesParameterName == null) {
             throw new IllegalArgumentException(
-                    "Null 'seriesParameterName' argument.");   
+                    "Null 'seriesParameterName' argument.");
         }
         if (categoryParameterName == null) {
             throw new IllegalArgumentException(
-                    "Null 'categoryParameterName' argument.");   
+                    "Null 'categoryParameterName' argument.");
         }
         this.prefix = prefix;
         this.seriesParameterName = seriesParameterName;
@@ -136,7 +136,7 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
      *
      * @return The generated URL.
      */
-    public String generateURL(CategoryDataset dataset, int series, 
+    public String generateURL(CategoryDataset dataset, int series,
                               int category) {
         String url = this.prefix;
         Comparable seriesKey = dataset.getRowKey(series);
@@ -145,26 +145,26 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
         url += firstParameter ? "?" : "&amp;";
         url += this.seriesParameterName + "=" + URLUtilities.encode(
                 seriesKey.toString(), "UTF-8");
-        url += "&amp;" + this.categoryParameterName + "=" 
+        url += "&amp;" + this.categoryParameterName + "="
                 + URLUtilities.encode(categoryKey.toString(), "UTF-8");
         return url;
     }
 
     /**
      * Returns an independent copy of the URL generator.
-     * 
+     *
      * @return A clone.
-     * 
-     * @throws CloneNotSupportedException not thrown by this class, but 
+     *
+     * @throws CloneNotSupportedException not thrown by this class, but
      *         subclasses (if any) might.
      */
     public Object clone() throws CloneNotSupportedException {
-    
         // all attributes are immutable, so we can just return the super.clone()
+    	// FIXME: in fact, the generator itself is immutable, so cloning is
+    	// not necessary
         return super.clone();
-        
     }
-    
+
     /**
      * Tests the generator for equality with an arbitrary object.
      *
@@ -184,11 +184,11 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
             return false;
         }
 
-        if (!ObjectUtilities.equal(this.seriesParameterName, 
+        if (!ObjectUtilities.equal(this.seriesParameterName,
                 that.seriesParameterName)) {
             return false;
         }
-        if (!ObjectUtilities.equal(this.categoryParameterName, 
+        if (!ObjectUtilities.equal(this.categoryParameterName,
                 that.categoryParameterName)) {
             return false;
         }
@@ -197,19 +197,19 @@ public class StandardCategoryURLGenerator implements CategoryURLGenerator,
 
     /**
      * Returns a hash code.
-     * 
+     *
      * @return A hash code.
      */
     public int hashCode() {
         int result;
         result = (this.prefix != null ? this.prefix.hashCode() : 0);
-        result = 29 * result 
-            + (this.seriesParameterName != null 
+        result = 29 * result
+            + (this.seriesParameterName != null
                     ? this.seriesParameterName.hashCode() : 0);
-        result = 29 * result 
-            + (this.categoryParameterName != null 
+        result = 29 * result
+            + (this.categoryParameterName != null
                     ? this.categoryParameterName.hashCode() : 0);
         return result;
     }
-    
+
 }
