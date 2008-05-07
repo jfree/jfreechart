5e6aeb49612252ec6d568ef6e214d0c6303fb81d
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 13:42:19 +0000

    Removed tabs.

--- source/org/jfree/chart/urls/CustomXYURLGenerator.java
+++ source/org/jfree/chart/urls/CustomXYURLGenerator.java
@@ -60,7 +60,7 @@ import org.jfree.util.PublicCloneable;
 public class CustomXYURLGenerator implements XYURLGenerator, Cloneable,
         PublicCloneable, Serializable {
 
-	/** For serialization. */
+    /** For serialization. */
     private static final long serialVersionUID = -8565933356596551832L;
 
     /** Storage for the URLs. */
@@ -139,10 +139,10 @@ public class CustomXYURLGenerator implements XYURLGenerator, Cloneable,
      *     is copied).
      */
     public void addURLSeries(List urls) {
-    	List listToAdd = null;
-    	if (urls != null) {
+        List listToAdd = null;
+        if (urls != null) {
             listToAdd = new java.util.ArrayList(urls);
-    	}
+        }
         this.urlSeries.add(listToAdd);
     }
 
