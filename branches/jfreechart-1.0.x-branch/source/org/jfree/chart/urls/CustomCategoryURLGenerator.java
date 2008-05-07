b7be3d8d3d9c67384230d6bc35b0c9c20a0920f0
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-05-07 13:01:38 +0000

    Removed tabs.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/urls/CustomCategoryURLGenerator.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/urls/CustomCategoryURLGenerator.java
@@ -52,132 +52,132 @@ import org.jfree.util.PublicCloneable;
  * A custom URL generator.
  */
 public class CustomCategoryURLGenerator implements CategoryURLGenerator,
-		Cloneable, PublicCloneable, Serializable {
+        Cloneable, PublicCloneable, Serializable {
 
-	/** Storage for the URLs. */
-	private ArrayList urlSeries = new ArrayList();
+    /** Storage for the URLs. */
+    private ArrayList urlSeries = new ArrayList();
 
-	/**
-	 * Default constructor.
-	 */
-	public CustomCategoryURLGenerator() {
-		super();
-	}
+    /**
+     * Default constructor.
+     */
+    public CustomCategoryURLGenerator() {
+        super();
+    }
 
-	/**
-	 * Returns the number of URL lists stored by the renderer.
-	 *
-	 * @return The list count.
-	 */
-	public int getListCount() {
-		return this.urlSeries.size();
-	}
+    /**
+     * Returns the number of URL lists stored by the renderer.
+     *
+     * @return The list count.
+     */
+    public int getListCount() {
+        return this.urlSeries.size();
+    }
 
-	/**
-	 * Returns the number of URLs in a given list.
-	 *
-	 * @param list  the list index (zero based).
-	 *
-	 * @return The URL count.
-	 */
-	public int getURLCount(int list) {
-		int result = 0;
-		List urls = (List) this.urlSeries.get(list);
-		if (urls != null) {
-			result = urls.size();
-		}
-		return result;
-	}
+    /**
+     * Returns the number of URLs in a given list.
+     *
+     * @param list  the list index (zero based).
+     *
+     * @return The URL count.
+     */
+    public int getURLCount(int list) {
+        int result = 0;
+        List urls = (List) this.urlSeries.get(list);
+        if (urls != null) {
+            result = urls.size();
+        }
+        return result;
+    }
 
-	/**
-	 * Returns the URL for an item.
-	 *
-	 * @param series  the series index.
-	 * @param item  the item index.
-	 *
-	 * @return The URL (possibly <code>null</code>).
-	 */
-	public String getURL(int series, int item) {
-		String result = null;
-		if (series < getListCount()) {
-			List urls = (List) this.urlSeries.get(series);
-			if (urls != null) {
-				if (item < urls.size()) {
-					result = (String) urls.get(item);
-				}
-			}
-		}
-		return result;
-	}
+    /**
+     * Returns the URL for an item.
+     *
+     * @param series  the series index.
+     * @param item  the item index.
+     *
+     * @return The URL (possibly <code>null</code>).
+     */
+    public String getURL(int series, int item) {
+        String result = null;
+        if (series < getListCount()) {
+            List urls = (List) this.urlSeries.get(series);
+            if (urls != null) {
+                if (item < urls.size()) {
+                    result = (String) urls.get(item);
+                }
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * Generates a URL.
-	 *
-	 * @param dataset  the dataset (ignored in this implementation).
-	 * @param series  the series (zero-based index).
-	 * @param item  the item (zero-based index).
-	 *
-	 * @return A string containing the URL (possibly <code>null</code>).
-	 */
-	public String generateURL(CategoryDataset dataset, int series, int item) {
-		return getURL(series, item);
-	}
+    /**
+     * Generates a URL.
+     *
+     * @param dataset  the dataset (ignored in this implementation).
+     * @param series  the series (zero-based index).
+     * @param item  the item (zero-based index).
+     *
+     * @return A string containing the URL (possibly <code>null</code>).
+     */
+    public String generateURL(CategoryDataset dataset, int series, int item) {
+        return getURL(series, item);
+    }
 
-	/**
-	 * Adds a list of URLs.
-	 *
-	 * @param urls  the list of URLs (<code>null</code> permitted).
-	 */
-	public void addURLSeries(List urls) {
-    	List listToAdd = null;
-    	if (urls != null) {
+    /**
+     * Adds a list of URLs.
+     *
+     * @param urls  the list of URLs (<code>null</code> permitted).
+     */
+    public void addURLSeries(List urls) {
+        List listToAdd = null;
+        if (urls != null) {
             listToAdd = new java.util.ArrayList(urls);
-    	}
+        }
         this.urlSeries.add(listToAdd);
-	}
+    }
 
-	/**
-	 * Tests if this object is equal to another.
-	 *
-	 * @param obj  the other object.
-	 *
-	 * @return A boolean.
-	 */
-	public boolean equals(Object obj) {
-		if (obj == this) {
-			return true;
-		}
-		if (!(obj instanceof CustomCategoryURLGenerator)) {
-			return false;
-		}
-		CustomCategoryURLGenerator generator = (CustomCategoryURLGenerator) obj;
-		int listCount = getListCount();
-		if (listCount != generator.getListCount()) {
-			return false;
-		}
+    /**
+     * Tests if this object is equal to another.
+     *
+     * @param obj  the other object.
+     *
+     * @return A boolean.
+     */
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof CustomCategoryURLGenerator)) {
+            return false;
+        }
+        CustomCategoryURLGenerator generator = (CustomCategoryURLGenerator) obj;
+        int listCount = getListCount();
+        if (listCount != generator.getListCount()) {
+            return false;
+        }
 
-		for (int series = 0; series < listCount; series++) {
-			int urlCount = getURLCount(series);
-			if (urlCount != generator.getURLCount(series)) {
-				return false;
-			}
+        for (int series = 0; series < listCount; series++) {
+            int urlCount = getURLCount(series);
+            if (urlCount != generator.getURLCount(series)) {
+                return false;
+            }
 
-			for (int item = 0; item < urlCount; item++) {
-				String u1 = getURL(series, item);
-				String u2 = generator.getURL(series, item);
-				if (u1 != null) {
-					if (!u1.equals(u2)) {
-						return false;
-					}
-				} else {
-					if (u2 != null) {
-						return false;
-					}
-				}
-			}
-		}
-		return true;
-	}
+            for (int item = 0; item < urlCount; item++) {
+                String u1 = getURL(series, item);
+                String u2 = generator.getURL(series, item);
+                if (u1 != null) {
+                    if (!u1.equals(u2)) {
+                        return false;
+                    }
+                } else {
+                    if (u2 != null) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
     /**
      * Returns a new generator that is a copy of, and independent from, this
