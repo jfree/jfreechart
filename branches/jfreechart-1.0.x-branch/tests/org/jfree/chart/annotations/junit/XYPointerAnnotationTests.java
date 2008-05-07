f54698ecd8cd60df3a4d61328ce9e680d3eac238
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-05-07 19:50:03 +0000

    Fixed Checkstyle warnings.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/annotations/junit/XYPointerAnnotationTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/annotations/junit/XYPointerAnnotationTests.java
@@ -199,7 +199,7 @@ public class XYPointerAnnotationTests extends TestCase {
     public void testPublicCloneable() {
         XYPointerAnnotation a1 = new XYPointerAnnotation("Label", 10.0, 20.0,
                 Math.PI);
-    	assertTrue(a1 instanceof PublicCloneable);
+        assertTrue(a1 instanceof PublicCloneable);
     }
 
     /**
