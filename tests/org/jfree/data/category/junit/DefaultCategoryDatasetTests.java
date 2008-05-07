f2a9cced06da7fbc686f75a78aada904d3d0a30b
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 13:32:30 +0000

    Removed tabs.

--- tests/org/jfree/data/category/junit/DefaultCategoryDatasetTests.java
+++ tests/org/jfree/data/category/junit/DefaultCategoryDatasetTests.java
@@ -343,12 +343,12 @@ public class DefaultCategoryDatasetTests extends TestCase {
      * A test for bug 1835955.
      */
     public void testBug1835955() {
-    	DefaultCategoryDataset d = new DefaultCategoryDataset();
-    	d.addValue(1.0, "R1", "C1");
-    	d.addValue(2.0, "R2", "C2");
-    	d.removeColumn("C2");
-    	d.addValue(3.0, "R2", "C2");
-    	assertEquals(3.0, d.getValue("R2", "C2").doubleValue(), EPSILON);
+        DefaultCategoryDataset d = new DefaultCategoryDataset();
+        d.addValue(1.0, "R1", "C1");
+        d.addValue(2.0, "R2", "C2");
+        d.removeColumn("C2");
+        d.addValue(3.0, "R2", "C2");
+        assertEquals(3.0, d.getValue("R2", "C2").doubleValue(), EPSILON);
     }
     
     /**
