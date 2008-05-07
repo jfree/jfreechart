a13e549fd4031c6ceb2c6d3e4438db9589f40ba5
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 15:19:48 +0000

    Fixed Checkstyle warnings.

--- tests/org/jfree/chart/labels/junit/BubbleXYItemLabelGeneratorTests.java
+++ tests/org/jfree/chart/labels/junit/BubbleXYItemLabelGeneratorTests.java
@@ -159,10 +159,10 @@ public class BubbleXYItemLabelGeneratorTests extends TestCase {
      * Simple check that hashCode is implemented.
      */
     public void testHashCode() {
-    	BubbleXYItemLabelGenerator g1
-    	        = new BubbleXYItemLabelGenerator();
-    	BubbleXYItemLabelGenerator g2
-    	        = new BubbleXYItemLabelGenerator();
+        BubbleXYItemLabelGenerator g1
+                = new BubbleXYItemLabelGenerator();
+        BubbleXYItemLabelGenerator g2
+                = new BubbleXYItemLabelGenerator();
         assertTrue(g1.equals(g2));
         assertTrue(g1.hashCode() == g2.hashCode());
     }
