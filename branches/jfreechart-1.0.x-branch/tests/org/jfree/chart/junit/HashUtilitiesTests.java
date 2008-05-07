aaea62485425b55535a33486496f0415818f203a
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-05-07 14:51:38 +0000

    Fixed whitespace warnings.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/junit/HashUtilitiesTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/junit/HashUtilitiesTests.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------
@@ -73,13 +73,13 @@ public class HashUtilitiesTests extends TestCase {
      * Some sanity checks for the hashCodeForDoubleArray() method.
      */
     public void testHashCodeForDoubleArray() {
-        double[] a1 = new double[] { 1.0 };
-        double[] a2 = new double[] { 1.0 };
+        double[] a1 = new double[] {1.0};
+        double[] a2 = new double[] {1.0};
         int h1 = HashUtilities.hashCodeForDoubleArray(a1);
         int h2 = HashUtilities.hashCodeForDoubleArray(a2);
         assertTrue(h1 == h2);
-        
-        double[] a3 = new double[] { 0.5, 1.0 };
+
+        double[] a3 = new double[] {0.5, 1.0};
         int h3 = HashUtilities.hashCodeForDoubleArray(a3);
         assertFalse(h1 == h3);
     }
