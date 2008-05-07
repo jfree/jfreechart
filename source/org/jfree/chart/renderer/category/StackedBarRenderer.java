34892823d2a8e6d64d4142975b87298949066043
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 09:01:32 +0000

    Removed trailing whitespace.

--- source/org/jfree/chart/renderer/category/StackedBarRenderer.java
+++ source/org/jfree/chart/renderer/category/StackedBarRenderer.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------
@@ -38,7 +38,7 @@
  * -------
  * 19-Oct-2001 : Version 1 (DG);
  * 22-Oct-2001 : Renamed DataSource.java --> Dataset.java etc. (DG);
- * 23-Oct-2001 : Changed intro and trail gaps on bar plots to use percentage of 
+ * 23-Oct-2001 : Changed intro and trail gaps on bar plots to use percentage of
  *               available space rather than a fixed number of units (DG);
  * 15-Nov-2001 : Modified to allow for null data values (DG);
  * 22-Nov-2001 : Modified to allow for negative data values (DG);
@@ -47,16 +47,16 @@
  * 15-Feb-2002 : Added isStacked() method (DG);
  * 14-Mar-2002 : Modified to implement the CategoryItemRenderer interface (DG);
  * 24-May-2002 : Incorporated tooltips into chart entities (DG);
- * 11-Jun-2002 : Added check for (permitted) null info object, bug and fix 
+ * 11-Jun-2002 : Added check for (permitted) null info object, bug and fix
  *               reported by David Basten.  Also updated Javadocs. (DG);
  * 25-Jun-2002 : Removed redundant import (DG);
  * 26-Jun-2002 : Small change to entity (DG);
- * 05-Aug-2002 : Small modification to drawCategoryItem method to support URLs 
+ * 05-Aug-2002 : Small modification to drawCategoryItem method to support URLs
  *               for HTML image maps (RA);
- * 08-Aug-2002 : Added optional linking lines, contributed by Thierry 
+ * 08-Aug-2002 : Added optional linking lines, contributed by Thierry
  *               Saura (DG);
  * 26-Sep-2002 : Fixed errors reported by Checkstyle (DG);
- * 24-Oct-2002 : Amendments for changes in CategoryDataset interface and 
+ * 24-Oct-2002 : Amendments for changes in CategoryDataset interface and
  *               CategoryToolTipGenerator interface (DG);
  * 05-Nov-2002 : Replaced references to CategoryDataset with TableDataset (DG);
  * 26-Nov-2002 : Replaced isStacked() method with getRangeType() method (DG);
@@ -68,25 +68,25 @@
  * 16-Sep-2003 : Changed ChartRenderingInfo --> PlotRenderingInfo (DG);
  * 21-Oct-2003 : Moved bar width into renderer state (DG);
  * 26-Nov-2003 : Added code to respect maxBarWidth attribute (DG);
- * 05-Nov-2004 : Changed to a two-pass renderer so that item labels are not 
+ * 05-Nov-2004 : Changed to a two-pass renderer so that item labels are not
  *               overwritten by other bars (DG);
  * 07-Jan-2005 : Renamed getRangeExtent() --> findRangeBounds() (DG);
- * 29-Mar-2005 : Modified drawItem() method so that a zero value is handled 
+ * 29-Mar-2005 : Modified drawItem() method so that a zero value is handled
  *               within the code for positive rather than negative values (DG);
- * 20-Apr-2005 : Renamed CategoryLabelGenerator 
+ * 20-Apr-2005 : Renamed CategoryLabelGenerator
  *               --> CategoryItemLabelGenerator (DG);
  * 17-May-2005 : Added flag to allow rendering values as percentages - inspired
  *               by patch 1200886 submitted by John Xiao (DG);
  * 09-Jun-2005 : Added accessor methods for the renderAsPercentages flag,
- *               provided equals() method, and use addItemEntity from 
+ *               provided equals() method, and use addItemEntity from
  *               superclass (DG);
  * 09-Jun-2005 : Added support for GradientPaint - see bug report 1215670 (DG);
  * 22-Sep-2005 : Renamed getMaxBarWidth() --> getMaximumBarWidth() (DG);
- * 29-Sep-2005 : Use outline stroke in drawItem method - see bug report 
+ * 29-Sep-2005 : Use outline stroke in drawItem method - see bug report
  *               1304139 (DG);
  * ------------- JFREECHART 1.0.x ---------------------------------------------
  * 11-Oct-2006 : Source reformatting (DG);
- * 
+ *
  */
 
 package org.jfree.chart.renderer.category;
@@ -116,43 +116,43 @@ import org.jfree.ui.TextAnchor;
 import org.jfree.util.PublicCloneable;
 
 /**
- * A stacked bar renderer for use with the 
+ * A stacked bar renderer for use with the
  * {@link org.jfree.chart.plot.CategoryPlot} class.
  */
-public class StackedBarRenderer extends BarRenderer 
-                                implements Cloneable, PublicCloneable, 
+public class StackedBarRenderer extends BarRenderer
+                                implements Cloneable, PublicCloneable,
                                            Serializable {
 
     /** For serialization. */
     static final long serialVersionUID = 6402943811500067531L;
-    
+
     /** A flag that controls whether the bars display values or percentages. */
     private boolean renderAsPercentages;
-    
+
     /**
-     * Creates a new renderer.  By default, the renderer has no tool tip 
-     * generator and no URL generator.  These defaults have been chosen to 
-     * minimise the processing required to generate a default chart.  If you 
-     * require tool tips or URLs, then you can easily add the required 
+     * Creates a new renderer.  By default, the renderer has no tool tip
+     * generator and no URL generator.  These defaults have been chosen to
+     * minimise the processing required to generate a default chart.  If you
+     * require tool tips or URLs, then you can easily add the required
      * generators.
      */
     public StackedBarRenderer() {
         this(false);
     }
-    
+
     /**
      * Creates a new renderer.
-     * 
+     *
      * @param renderAsPercentages  a flag that controls whether the data values
      *                             are rendered as percentages.
      */
     public StackedBarRenderer(boolean renderAsPercentages) {
         super();
         this.renderAsPercentages = renderAsPercentages;
-        
-        // set the default item label positions, which will only be used if 
+
+        // set the default item label positions, which will only be used if
         // the user requests visible item labels...
-        ItemLabelPosition p = new ItemLabelPosition(ItemLabelAnchor.CENTER, 
+        ItemLabelPosition p = new ItemLabelPosition(ItemLabelAnchor.CENTER,
                 TextAnchor.CENTER);
         setBasePositiveItemLabelPosition(p);
         setBaseNegativeItemLabelPosition(p);
@@ -162,53 +162,53 @@ public class StackedBarRenderer extends BarRenderer
 
     /**
      * Returns <code>true</code> if the renderer displays each item value as
-     * a percentage (so that the stacked bars add to 100%), and 
+     * a percentage (so that the stacked bars add to 100%), and
      * <code>false</code> otherwise.
-     * 
+     *
      * @return A boolean.
-     * 
+     *
      * @see #setRenderAsPercentages(boolean)
      */
     public boolean getRenderAsPercentages() {
-        return this.renderAsPercentages;   
+        return this.renderAsPercentages;
     }
-    
+
     /**
      * Sets the flag that controls whether the renderer displays each item
      * value as a percentage (so that the stacked bars add to 100%), and sends
      * a {@link RendererChangeEvent} to all registered listeners.
-     * 
+     *
      * @param asPercentages  the flag.
-     * 
+     *
      * @see #getRenderAsPercentages()
      */
     public void setRenderAsPercentages(boolean asPercentages) {
-        this.renderAsPercentages = asPercentages; 
+        this.renderAsPercentages = asPercentages;
         fireChangeEvent();
     }
-    
+
     /**
-     * Returns the number of passes (<code>2</code>) required by this renderer. 
+     * Returns the number of passes (<code>2</code>) required by this renderer.
      * The first pass is used to draw the bars, the second pass is used to
      * draw the item labels (if visible).
-     * 
+     *
      * @return The number of passes required by the renderer.
      */
     public int getPassCount() {
         return 2;
     }
-    
+
     /**
      * Returns the range of values the renderer requires to display all the
      * items from the specified dataset.
-     * 
+     *
      * @param dataset  the dataset (<code>null</code> permitted).
-     * 
+     *
      * @return The range (or <code>null</code> if the dataset is empty).
      */
     public Range findRangeBounds(CategoryDataset dataset) {
         if (this.renderAsPercentages) {
-            return new Range(0.0, 1.0);   
+            return new Range(0.0, 1.0);
         }
         else {
             return DatasetUtilities.findStackedRangeBounds(dataset, getBase());
@@ -217,14 +217,14 @@ public class StackedBarRenderer extends BarRenderer
 
     /**
      * Calculates the bar width and stores it in the renderer state.
-     * 
+     *
      * @param plot  the plot.
      * @param dataArea  the data area.
      * @param rendererIndex  the renderer index.
      * @param state  the renderer state.
      */
-    protected void calculateBarWidth(CategoryPlot plot, 
-                                     Rectangle2D dataArea, 
+    protected void calculateBarWidth(CategoryPlot plot,
+                                     Rectangle2D dataArea,
                                      int rendererIndex,
                                      CategoryItemRendererState state) {
 
@@ -247,7 +247,7 @@ public class StackedBarRenderer extends BarRenderer
                 categoryMargin = xAxis.getCategoryMargin();
             }
 
-            double used = space * (1 - xAxis.getLowerMargin() 
+            double used = space * (1 - xAxis.getLowerMargin()
                                      - xAxis.getUpperMargin()
                                      - categoryMargin);
             if (columns > 0) {
@@ -284,23 +284,23 @@ public class StackedBarRenderer extends BarRenderer
                          int row,
                          int column,
                          int pass) {
-     
+
         // nothing is drawn for null values...
         Number dataValue = dataset.getValue(row, column);
         if (dataValue == null) {
             return;
         }
-        
+
         double value = dataValue.doubleValue();
         double total = 0.0;  // only needed if calculating percentages
         if (this.renderAsPercentages) {
             total = DataUtilities.calculateColumnTotal(dataset, column);
             value = value / total;
         }
-        
+
         PlotOrientation orientation = plot.getOrientation();
-        double barW0 = domainAxis.getCategoryMiddle(column, getColumnCount(), 
-                dataArea, plot.getDomainAxisEdge()) 
+        double barW0 = domainAxis.getCategoryMiddle(column, getColumnCount(),
+                dataArea, plot.getDomainAxisEdge())
                 - state.getBarWidth() / 2.0;
 
         double positiveBase = getBase();
@@ -326,15 +326,15 @@ public class StackedBarRenderer extends BarRenderer
         double translatedValue;
         RectangleEdge location = plot.getRangeAxisEdge();
         if (value >= 0.0) {
-            translatedBase = rangeAxis.valueToJava2D(positiveBase, dataArea, 
+            translatedBase = rangeAxis.valueToJava2D(positiveBase, dataArea,
                     location);
-            translatedValue = rangeAxis.valueToJava2D(positiveBase + value, 
+            translatedValue = rangeAxis.valueToJava2D(positiveBase + value,
                     dataArea, location);
         }
         else {
-            translatedBase = rangeAxis.valueToJava2D(negativeBase, dataArea, 
+            translatedBase = rangeAxis.valueToJava2D(negativeBase, dataArea,
                     location);
-            translatedValue = rangeAxis.valueToJava2D(negativeBase + value, 
+            translatedValue = rangeAxis.valueToJava2D(negativeBase + value,
                     dataArea, location);
         }
         double barL0 = Math.min(translatedBase, translatedValue);
@@ -343,11 +343,11 @@ public class StackedBarRenderer extends BarRenderer
 
         Rectangle2D bar = null;
         if (orientation == PlotOrientation.HORIZONTAL) {
-            bar = new Rectangle2D.Double(barL0, barW0, barLength, 
+            bar = new Rectangle2D.Double(barL0, barW0, barLength,
                     state.getBarWidth());
         }
         else {
-            bar = new Rectangle2D.Double(barW0, barL0, state.getBarWidth(), 
+            bar = new Rectangle2D.Double(barW0, barL0, state.getBarWidth(),
                     barLength);
         }
         if (pass == 0) {
@@ -358,7 +358,7 @@ public class StackedBarRenderer extends BarRenderer
             }
             g2.setPaint(itemPaint);
             g2.fill(bar);
-            if (isDrawBarOutline() 
+            if (isDrawBarOutline()
                     && state.getBarWidth() > BAR_OUTLINE_WIDTH_THRESHOLD) {
                 g2.setStroke(getItemOutlineStroke(row, column));
                 g2.setPaint(getItemOutlinePaint(row, column));
@@ -372,32 +372,32 @@ public class StackedBarRenderer extends BarRenderer
             }
         }
         else if (pass == 1) {
-            CategoryItemLabelGenerator generator = getItemLabelGenerator(row, 
+            CategoryItemLabelGenerator generator = getItemLabelGenerator(row,
                     column);
             if (generator != null && isItemLabelVisible(row, column)) {
-                drawItemLabel(g2, dataset, row, column, plot, generator, bar, 
+                drawItemLabel(g2, dataset, row, column, plot, generator, bar,
                         (value < 0.0));
             }
-        }        
+        }
     }
 
     /**
      * Tests this renderer for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
         if (obj == this) {
-            return true;   
+            return true;
         }
         if (!(obj instanceof StackedBarRenderer)) {
-            return false;   
+            return false;
         }
         StackedBarRenderer that = (StackedBarRenderer) obj;
         if (this.renderAsPercentages != that.renderAsPercentages) {
-            return false;   
+            return false;
         }
         return super.equals(obj);
     }
