6ae4cb2fe04f5b2a31b23b5031a70af7dd9b6945
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 15:31:25 +0000

    Fixed Checkstyle warnings.

--- tests/org/jfree/chart/labels/junit/BoxAndWhiskerToolTipGeneratorTests.java
+++ tests/org/jfree/chart/labels/junit/BoxAndWhiskerToolTipGeneratorTests.java
@@ -92,19 +92,19 @@ public class BoxAndWhiskerToolTipGeneratorTests extends TestCase {
 
         // tooltip format
         g1 = new BoxAndWhiskerToolTipGenerator("{0} --> {1} {2}",
-        		new DecimalFormat("0.0"));
+                new DecimalFormat("0.0"));
         g2 = new BoxAndWhiskerToolTipGenerator("{1} {2}",
-        		new DecimalFormat("0.0"));
+                new DecimalFormat("0.0"));
         assertFalse(g1.equals(g2));
         g2 = new BoxAndWhiskerToolTipGenerator("{0} --> {1} {2}",
-        		new DecimalFormat("0.0"));
+                new DecimalFormat("0.0"));
         assertTrue(g1.equals(g2));
 
         // Y format
         g1 = new BoxAndWhiskerToolTipGenerator("{0} --> {1} {2}",
-        		new DecimalFormat("0.0"));
+                new DecimalFormat("0.0"));
         g2 = new BoxAndWhiskerToolTipGenerator("{0} --> {1} {2}",
-        		new DecimalFormat("0.00"));
+                new DecimalFormat("0.00"));
         assertFalse(g1.equals(g2));
     }
 
