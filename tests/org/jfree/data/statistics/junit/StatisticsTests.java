7f3af03057ee471d6264a7e57ab1d2c19f358ecd
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 15:33:54 +0000

    Fixed Checkstyle warnings.

--- tests/org/jfree/data/statistics/junit/StatisticsTests.java
+++ tests/org/jfree/data/statistics/junit/StatisticsTests.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * --------------------
@@ -56,7 +56,7 @@ import org.jfree.data.statistics.Statistics;
  * Tests for the {@link Statistics} class.
  */
 public class StatisticsTests extends TestCase {
-    
+
     /**
      * Returns the tests as a test suite.
      *
@@ -76,11 +76,11 @@ public class StatisticsTests extends TestCase {
     }
 
     /**
-     * Some checks for the calculateMean(Number[]) and 
+     * Some checks for the calculateMean(Number[]) and
      * calculateMean(Number[], boolean) methods.
      */
     public void testCalculateMean_Array() {
-        
+
         // try null array
         boolean pass = false;
         try {
@@ -90,7 +90,7 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         pass = false;
         try {
             Statistics.calculateMean((Number[]) null, false);
@@ -99,37 +99,37 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         // try an array containing no items
         assertTrue(Double.isNaN(Statistics.calculateMean(new Number[0])));
-        assertTrue(Double.isNaN(Statistics.calculateMean(new Number[0], 
+        assertTrue(Double.isNaN(Statistics.calculateMean(new Number[0],
                 false)));
-        
+
         // try an array containing a single Number
-        Number[] values = new Number[] { new Double(1.0) };
+        Number[] values = new Number[] {new Double(1.0)};
         assertEquals(1.0, Statistics.calculateMean(values), EPSILON);
         assertEquals(1.0, Statistics.calculateMean(values, true), EPSILON);
         assertEquals(1.0, Statistics.calculateMean(values, false), EPSILON);
-        
+
         // try an array containing a single Number and a null
-        values = new Number[] { new Double(1.0), null };
+        values = new Number[] {new Double(1.0), null};
         assertTrue(Double.isNaN(Statistics.calculateMean(values)));
         assertTrue(Double.isNaN(Statistics.calculateMean(values, true)));
         assertEquals(1.0, Statistics.calculateMean(values, false), EPSILON);
 
         // try an array containing a single Number and a NaN
-        values = new Number[] { new Double(1.0), new Double(Double.NaN) };
+        values = new Number[] {new Double(1.0), new Double(Double.NaN)};
         assertTrue(Double.isNaN(Statistics.calculateMean(values)));
         assertTrue(Double.isNaN(Statistics.calculateMean(values, true)));
         assertEquals(1.0, Statistics.calculateMean(values, false), EPSILON);
     }
-    
+
     /**
-     * Some checks for the calculateMean(Collection) and 
+     * Some checks for the calculateMean(Collection) and
      * calculateMean(Collection, boolean) methods.
      */
     public void testCalculateMean_Collection() {
-        
+
         // try a null collection
         boolean pass = false;
         try {
@@ -139,7 +139,7 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         pass = false;
         try {
             Statistics.calculateMean((Collection) null, false);
@@ -148,19 +148,19 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         // try an empty collection
         List values = new ArrayList();
         assertTrue(Double.isNaN(Statistics.calculateMean(values)));
         assertTrue(Double.isNaN(Statistics.calculateMean(values, true)));
         assertTrue(Double.isNaN(Statistics.calculateMean(values, false)));
-        
+
         // try a collection with a single number
         values.add(new Double(9.0));
         assertEquals(9.0, Statistics.calculateMean(values), EPSILON);
         assertEquals(9.0, Statistics.calculateMean(values, true), EPSILON);
         assertEquals(9.0, Statistics.calculateMean(values, false), EPSILON);
-        
+
         // try a collection with a single number plus a null
         values.add(null);
         assertTrue(Double.isNaN(Statistics.calculateMean(values)));
@@ -183,28 +183,28 @@ public class StatisticsTests extends TestCase {
         values.add(new Double(2.0));
         double mean = Statistics.calculateMean(values);
         assertEquals(4.0, mean, EPSILON);
-        
+
         // a Collection containing a NaN will return Double.NaN for the result
         values.add(new Double(Double.NaN));
         assertTrue(Double.isNaN(Statistics.calculateMean(values)));
     }
-    
+
     static final double EPSILON = 0.0000000001;
-    
+
     /**
      * Some checks for the calculateMedian(List, boolean) method.
      */
     public void testCalculateMedian() {
-        
+
         // check null list
         assertTrue(Double.isNaN(Statistics.calculateMedian(null, false)));
         assertTrue(Double.isNaN(Statistics.calculateMedian(null, true)));
-        
+
         // check empty list
         List list = new ArrayList();
         assertTrue(Double.isNaN(Statistics.calculateMedian(list, false)));
         assertTrue(Double.isNaN(Statistics.calculateMedian(list, true)));
-        
+
         // check list containing null
         list.add(null);
         boolean pass = false;
@@ -215,7 +215,7 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         pass = false;
         try {
             Statistics.calculateMedian(list, true);
@@ -236,7 +236,7 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         pass = false;
         try {
             Statistics.calculateMedian(list, true);
@@ -245,9 +245,9 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
     }
-    
+
     /**
      * A test for the calculateMedian() method.
      */
@@ -299,7 +299,7 @@ public class StatisticsTests extends TestCase {
         double median = Statistics.calculateMedian(values);
         assertEquals(4.0, median, 0.0000001);
     }
-    
+
     /**
      * A test using some real data that caused a problem at one point.
      */
@@ -371,7 +371,7 @@ public class StatisticsTests extends TestCase {
         double median = Statistics.calculateMedian(values, 0, 2);
         assertEquals(3.0, median, 0.0000001);
     }
-    
+
     /**
      * A simple test for the correlation calculation.
      */
@@ -379,72 +379,72 @@ public class StatisticsTests extends TestCase {
         Number[] data1 = new Number[3];
         data1[0] = new Double(1);
         data1[1] = new Double(2);
-        data1[2] = new Double(3);        
+        data1[2] = new Double(3);
         Number[] data2 = new Number[3];
         data2[0] = new Double(1);
         data2[1] = new Double(2);
-        data2[2] = new Double(3);    
+        data2[2] = new Double(3);
         double r = Statistics.getCorrelation(data1, data2);
-        assertEquals(1.0, r, 0.00000001);        
+        assertEquals(1.0, r, 0.00000001);
     }
 
     /**
      * A simple test for the correlation calculation.
-     * 
+     *
      * http://trochim.human.cornell.edu/kb/statcorr.htm
      */
     public void testCorrelation2() {
         Number[] data1 = new Number[20];
         data1[0] = new Double(68);
         data1[1] = new Double(71);
-        data1[2] = new Double(62);        
-        data1[3] = new Double(75);        
-        data1[4] = new Double(58);        
-        data1[5] = new Double(60);        
-        data1[6] = new Double(67);        
-        data1[7] = new Double(68);        
-        data1[8] = new Double(71);        
-        data1[9] = new Double(69);        
-        data1[10] = new Double(68);        
-        data1[11] = new Double(67);        
-        data1[12] = new Double(63);        
-        data1[13] = new Double(62);        
-        data1[14] = new Double(60);        
-        data1[15] = new Double(63);        
-        data1[16] = new Double(65);        
-        data1[17] = new Double(67);        
-        data1[18] = new Double(63);        
-        data1[19] = new Double(61);        
+        data1[2] = new Double(62);
+        data1[3] = new Double(75);
+        data1[4] = new Double(58);
+        data1[5] = new Double(60);
+        data1[6] = new Double(67);
+        data1[7] = new Double(68);
+        data1[8] = new Double(71);
+        data1[9] = new Double(69);
+        data1[10] = new Double(68);
+        data1[11] = new Double(67);
+        data1[12] = new Double(63);
+        data1[13] = new Double(62);
+        data1[14] = new Double(60);
+        data1[15] = new Double(63);
+        data1[16] = new Double(65);
+        data1[17] = new Double(67);
+        data1[18] = new Double(63);
+        data1[19] = new Double(61);
         Number[] data2 = new Number[20];
         data2[0] = new Double(4.1);
         data2[1] = new Double(4.6);
-        data2[2] = new Double(3.8);    
-        data2[3] = new Double(4.4);    
-        data2[4] = new Double(3.2);    
-        data2[5] = new Double(3.1);    
-        data2[6] = new Double(3.8);    
-        data2[7] = new Double(4.1);    
-        data2[8] = new Double(4.3);    
-        data2[9] = new Double(3.7);    
-        data2[10] = new Double(3.5);    
-        data2[11] = new Double(3.2);    
-        data2[12] = new Double(3.7);    
-        data2[13] = new Double(3.3);    
-        data2[14] = new Double(3.4);    
-        data2[15] = new Double(4.0);    
-        data2[16] = new Double(4.1);    
-        data2[17] = new Double(3.8);    
-        data2[18] = new Double(3.4);    
-        data2[19] = new Double(3.6);    
+        data2[2] = new Double(3.8);
+        data2[3] = new Double(4.4);
+        data2[4] = new Double(3.2);
+        data2[5] = new Double(3.1);
+        data2[6] = new Double(3.8);
+        data2[7] = new Double(4.1);
+        data2[8] = new Double(4.3);
+        data2[9] = new Double(3.7);
+        data2[10] = new Double(3.5);
+        data2[11] = new Double(3.2);
+        data2[12] = new Double(3.7);
+        data2[13] = new Double(3.3);
+        data2[14] = new Double(3.4);
+        data2[15] = new Double(4.0);
+        data2[16] = new Double(4.1);
+        data2[17] = new Double(3.8);
+        data2[18] = new Double(3.4);
+        data2[19] = new Double(3.6);
         double r = Statistics.getCorrelation(data1, data2);
-        assertEquals(0.7306356862792885, r, 0.000000000001);        
+        assertEquals(0.7306356862792885, r, 0.000000000001);
     }
-    
+
     /**
      * Some checks for the getStdDev() method.
      */
     public void testGetStdDev() {
-        
+
         // try null argument
         boolean pass = false;
         try {
@@ -454,7 +454,7 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         // try zero length array
         pass = false;
         try {
@@ -464,9 +464,9 @@ public class StatisticsTests extends TestCase {
             pass = true;
         }
         assertTrue(pass);
-        
+
         // try single value
-        assertTrue(Double.isNaN(Statistics.getStdDev(new Double[] 
+        assertTrue(Double.isNaN(Statistics.getStdDev(new Double[]
                 {new Double(1.0)})));
     }
 
