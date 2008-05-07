ec357e592f36e4be636e50792e3a9678a75c3407
David Gilbert <jfree@users.noreply.github.com>
2008-05-07 19:25:38 +0000

    Fixed Checkstyle warnings.

--- tests/org/jfree/chart/annotations/junit/XYTextAnnotationTests.java
+++ tests/org/jfree/chart/annotations/junit/XYTextAnnotationTests.java
@@ -179,7 +179,7 @@ public class XYTextAnnotationTests extends TestCase {
      */
     public void testPublicCloneable() {
         XYTextAnnotation a1 = new XYTextAnnotation("Text", 10.0, 20.0);
-    	assertTrue(a1 instanceof PublicCloneable);
+        assertTrue(a1 instanceof PublicCloneable);
     }
 
     /**
