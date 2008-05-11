1895348ad1e6b49b410ee9daeea22a6dead6816d
David Gilbert <jfree@users.noreply.github.com>
2008-05-11 19:58:52 +0000

    2008-05-11  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/chart/block/EmptyBlock.java
        (arrange): Use constraint.

--- source/org/jfree/chart/block/EmptyBlock.java
+++ source/org/jfree/chart/block/EmptyBlock.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------
@@ -38,7 +38,9 @@
  * 04-Feb-2005 : Now cloneable and serializable (DG);
  * 20-Apr-2005 : Added new draw() method (DG);
  * 08-Apr-2008 : Added support for margin and border (DG);
- * 
+ * 08-May-2008 : Updated arrange() method to recognise
+ *               incoming constraint (DG);
+ *
  */
 
 package org.jfree.chart.block;
@@ -53,16 +55,15 @@ import org.jfree.util.PublicCloneable;
 /**
  * An empty block with a fixed size.
  */
-public class EmptyBlock extends AbstractBlock 
-                        implements Block, Cloneable, PublicCloneable,
-                                   Serializable {
-    
+public class EmptyBlock extends AbstractBlock
+        implements Block, Cloneable, PublicCloneable, Serializable {
+
     /** For serialization. */
     private static final long serialVersionUID = -4083197869412648579L;
-    
+
     /**
      * Creates a new block with the specified width and height.
-     * 
+     *
      * @param width  the width.
      * @param height  the height.
      */
@@ -72,38 +73,39 @@ public class EmptyBlock extends AbstractBlock
     }
 
     /**
-     * Arranges the contents of the block, within the given constraints, and 
+     * Arranges the contents of the block, within the given constraints, and
      * returns the block size.
-     * 
+     *
      * @param g2  the graphics device.
      * @param constraint  the constraint (<code>null</code> not permitted).
-     * 
+     *
      * @return The block size (in Java2D units, never <code>null</code>).
      */
     public Size2D arrange(Graphics2D g2, RectangleConstraint constraint) {
-        return new Size2D(calculateTotalWidth(getWidth()), 
+        Size2D base = new Size2D(calculateTotalWidth(getWidth()),
                 calculateTotalHeight(getHeight()));
-    }
+        return constraint.calculateConstrainedSize(base);
+}
 
     /**
      * Draws the block.  Since the block is empty, there is nothing to draw
      * except the optional border.
-     * 
+     *
      * @param g2  the graphics device.
      * @param area  the area.
      */
     public void draw(Graphics2D g2, Rectangle2D area) {
         draw(g2, area, null);
     }
-    
+
     /**
-     * Draws the block within the specified area.  Since the block is empty, 
+     * Draws the block within the specified area.  Since the block is empty,
      * there is nothing to draw except the optional border.
-     * 
+     *
      * @param g2  the graphics device.
      * @param area  the area.
      * @param params  ignored (<code>null</code> permitted).
-     * 
+     *
      * @return Always <code>null</code>.
      */
     public Object draw(Graphics2D g2, Rectangle2D area, Object params) {
@@ -114,13 +116,13 @@ public class EmptyBlock extends AbstractBlock
 
     /**
      * Returns a clone of the block.
-     * 
+     *
      * @return A clone.
-     * 
+     *
      * @throws CloneNotSupportedException if there is a problem cloning.
      */
     public Object clone() throws CloneNotSupportedException {
-        return super.clone();   
+        return super.clone();
     }
 
 }
