72e59e90d18a70fc95403f95ea5cf700ada534ab
David Gilbert <jfree@users.noreply.github.com>
2008-05-12 09:34:13 +0000

    2008-05-12  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/data/category/SlidingCategoryDataset.java: New class,
        * source/org/jfree/data/gantt/SlidingGanttCategoryDatsaet.java: Likewise,
        * tests/org/jfree/data/category/SlidingCategoryDatasetTests.java: 
        New tests,
        * tests/org/jfree/data/gantt/SlidingGanttCategoryDatasetTests.java: 
        Likewise.

---
+++ tests/org/jfree/data/gantt/junit/SlidingGanttCategoryDatasetTests.java
@@ -0,0 +1,189 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * -------------------------------------
+ * SlidingGanttCategoryDatasetTests.java
+ * -------------------------------------
+ * (C) Copyright 2008, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 08-May-2008 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.data.gantt.junit;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jfree.data.gantt.SlidingGanttCategoryDataset;
+import org.jfree.data.gantt.Task;
+import org.jfree.data.gantt.TaskSeries;
+import org.jfree.data.gantt.TaskSeriesCollection;
+
+/**
+ * Tests for the {@link SlidingGanttCategoryDataset} class.
+ */
+public class SlidingGanttCategoryDatasetTests extends TestCase {
+
+    /**
+     * Returns the tests as a test suite.
+     *
+     * @return The test suite.
+     */
+    public static Test suite() {
+        return new TestSuite(SlidingGanttCategoryDatasetTests.class);
+    }
+
+    /**
+     * Constructs a new set of tests.
+     *
+     * @param name  the name of the tests.
+     */
+    public SlidingGanttCategoryDatasetTests(String name) {
+        super(name);
+    }
+
+    /**
+     * Some checks for the equals() method.
+     */
+    public void testEquals() {
+    	TaskSeries s1 = new TaskSeries("Series");
+    	s1.add(new Task("Task 1", new Date(0L), new Date(1L)));
+    	s1.add(new Task("Task 2", new Date(10L), new Date(11L)));
+    	s1.add(new Task("Task 3", new Date(20L), new Date(21L)));
+    	TaskSeriesCollection u1 = new TaskSeriesCollection();
+    	u1.add(s1);
+    	SlidingGanttCategoryDataset d1 = new SlidingGanttCategoryDataset(
+    			u1, 0, 5);
+    	TaskSeries s2 = new TaskSeries("Series");
+    	s2.add(new Task("Task 1", new Date(0L), new Date(1L)));
+    	s2.add(new Task("Task 2", new Date(10L), new Date(11L)));
+    	s2.add(new Task("Task 3", new Date(20L), new Date(21L)));
+    	TaskSeriesCollection u2 = new TaskSeriesCollection();
+    	u2.add(s2);
+    	SlidingGanttCategoryDataset d2 = new SlidingGanttCategoryDataset(
+    			u2, 0, 5);
+    	assertTrue(d1.equals(d2));
+
+    	d1.setFirstCategoryIndex(1);
+    	assertFalse(d1.equals(d2));
+    	d2.setFirstCategoryIndex(1);
+    	assertTrue(d1.equals(d2));
+
+    	d1.setMaximumCategoryCount(99);
+    	assertFalse(d1.equals(d2));
+    	d2.setMaximumCategoryCount(99);
+    	assertTrue(d1.equals(d2));
+
+    	s1.add(new Task("Task 2", new Date(10L), new Date(11L)));
+    	assertFalse(d1.equals(d2));
+    	s2.add(new Task("Task 2", new Date(10L), new Date(11L)));
+    	assertTrue(d1.equals(d2));
+    }
+
+    /**
+     * Confirm that cloning works.
+     */
+    public void testCloning() {
+    	TaskSeries s1 = new TaskSeries("Series");
+    	s1.add(new Task("Task 1", new Date(0L), new Date(1L)));
+    	TaskSeriesCollection u1 = new TaskSeriesCollection();
+    	u1.add(s1);
+    	SlidingGanttCategoryDataset d1 = new SlidingGanttCategoryDataset(
+    			u1, 0, 5);
+    	SlidingGanttCategoryDataset d2 = null;
+        try {
+            d2 = (SlidingGanttCategoryDataset) d1.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(d1 != d2);
+        assertTrue(d1.getClass() == d2.getClass());
+        assertTrue(d1.equals(d2));
+
+        // basic check for independence
+    	s1.add(new Task("Task 2", new Date(10L), new Date(11L)));
+        assertFalse(d1.equals(d2));
+        TaskSeriesCollection u2
+                = (TaskSeriesCollection) d2.getUnderlyingDataset();
+        TaskSeries s2 = u2.getSeries("Series");
+        s2.add(new Task("Task 2", new Date(10L), new Date(11L)));
+        assertTrue(d1.equals(d2));
+    }
+
+    /**
+     * Serialize an instance, restore it, and check for equality.
+     */
+    public void testSerialization() {
+    	TaskSeries s1 = new TaskSeries("Series");
+    	s1.add(new Task("Task 1", new Date(0L), new Date(1L)));
+    	TaskSeriesCollection u1 = new TaskSeriesCollection();
+    	u1.add(s1);
+    	SlidingGanttCategoryDataset d1 = new SlidingGanttCategoryDataset(
+    			u1, 0, 5);
+    	SlidingGanttCategoryDataset d2 = null;
+        try {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(buffer);
+            out.writeObject(d1);
+            out.close();
+
+            ObjectInput in = new ObjectInputStream(
+                    new ByteArrayInputStream(buffer.toByteArray()));
+            d2 = (SlidingGanttCategoryDataset) in.readObject();
+            in.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(d1, d2);
+
+        // basic check for independence
+    	s1.add(new Task("Task 2", new Date(10L), new Date(11L)));
+        assertFalse(d1.equals(d2));
+        TaskSeriesCollection u2
+                = (TaskSeriesCollection) d2.getUnderlyingDataset();
+        TaskSeries s2 = u2.getSeries("Series");
+        s2.add(new Task("Task 2", new Date(10L), new Date(11L)));
+        assertTrue(d1.equals(d2));
+    }
+
+}
