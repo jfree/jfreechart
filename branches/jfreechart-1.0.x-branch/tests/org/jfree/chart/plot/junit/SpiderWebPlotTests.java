610f37a08c6baa8675092edc07d7d1350bebe2c4
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-06-02 10:49:11 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/plot/junit/SpiderWebPlotTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/plot/junit/SpiderWebPlotTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------
  * SpiderWebPlotTests.java
  * -----------------------
- * (C) Copyright 2005-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2005-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -102,43 +102,43 @@ public class SpiderWebPlotTests extends TestCase {
         SpiderWebPlot p2 = new SpiderWebPlot(new DefaultCategoryDataset());
         assertTrue(p1.equals(p2));
         assertTrue(p2.equals(p1));
-        
+
         // dataExtractOrder
         p1.setDataExtractOrder(TableOrder.BY_COLUMN);
         assertFalse(p1.equals(p2));
         p2.setDataExtractOrder(TableOrder.BY_COLUMN);
         assertTrue(p1.equals(p2));
-        
+
         // headPercent
         p1.setHeadPercent(0.321);
         assertFalse(p1.equals(p2));
         p2.setHeadPercent(0.321);
         assertTrue(p1.equals(p2));
-        
+
         // interiorGap
         p1.setInteriorGap(0.123);
         assertFalse(p1.equals(p2));
         p2.setInteriorGap(0.123);
         assertTrue(p1.equals(p2));
-        
+
         // startAngle
         p1.setStartAngle(0.456);
         assertFalse(p1.equals(p2));
         p2.setStartAngle(0.456);
         assertTrue(p1.equals(p2));
-        
-        // direction 
+
+        // direction
         p1.setDirection(Rotation.ANTICLOCKWISE);
         assertFalse(p1.equals(p2));
         p2.setDirection(Rotation.ANTICLOCKWISE);
         assertTrue(p1.equals(p2));
-        
+
         // maxValue
         p1.setMaxValue(123.4);
         assertFalse(p1.equals(p2));
         p2.setMaxValue(123.4);
         assertTrue(p1.equals(p2));
-        
+
         // legendItemShape
         p1.setLegendItemShape(new Rectangle(1, 2, 3, 4));
         assertFalse(p1.equals(p2));
@@ -146,66 +146,66 @@ public class SpiderWebPlotTests extends TestCase {
         assertTrue(p1.equals(p2));
 
         // seriesPaint
-        p1.setSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        p1.setSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.white));
         assertFalse(p1.equals(p2));
-        p2.setSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        p2.setSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.white));
         assertTrue(p1.equals(p2));
 
         // seriesPaintList
-        p1.setSeriesPaint(1, new GradientPaint(1.0f, 2.0f, Color.yellow, 
+        p1.setSeriesPaint(1, new GradientPaint(1.0f, 2.0f, Color.yellow,
                 3.0f, 4.0f, Color.white));
         assertFalse(p1.equals(p2));
-        p2.setSeriesPaint(1, new GradientPaint(1.0f, 2.0f, Color.yellow, 
+        p2.setSeriesPaint(1, new GradientPaint(1.0f, 2.0f, Color.yellow,
                 3.0f, 4.0f, Color.white));
         assertTrue(p1.equals(p2));
-        
+
         // baseSeriesPaint
-        p1.setBaseSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        p1.setBaseSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.black));
         assertFalse(p1.equals(p2));
-        p2.setBaseSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        p2.setBaseSeriesPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.black));
         assertTrue(p1.equals(p2));
-        
+
         // seriesOutlinePaint
-        p1.setSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.blue, 
+        p1.setSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.blue,
                 3.0f, 4.0f, Color.black));
         assertFalse(p1.equals(p2));
-        p2.setSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.blue, 
+        p2.setSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.blue,
                 3.0f, 4.0f, Color.black));
         assertTrue(p1.equals(p2));
 
         // seriesOutlinePaintList
-        p1.setSeriesOutlinePaint(1, new GradientPaint(1.0f, 2.0f, Color.blue, 
+        p1.setSeriesOutlinePaint(1, new GradientPaint(1.0f, 2.0f, Color.blue,
                 3.0f, 4.0f, Color.green));
         assertFalse(p1.equals(p2));
-        p2.setSeriesOutlinePaint(1, new GradientPaint(1.0f, 2.0f, Color.blue, 
+        p2.setSeriesOutlinePaint(1, new GradientPaint(1.0f, 2.0f, Color.blue,
                 3.0f, 4.0f, Color.green));
         assertTrue(p1.equals(p2));
 
         // baseSeriesOutlinePaint
-        p1.setBaseSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.cyan, 
+        p1.setBaseSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.cyan,
                 3.0f, 4.0f, Color.green));
         assertFalse(p1.equals(p2));
-        p2.setBaseSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.cyan, 
+        p2.setBaseSeriesOutlinePaint(new GradientPaint(1.0f, 2.0f, Color.cyan,
                 3.0f, 4.0f, Color.green));
         assertTrue(p1.equals(p2));
-        
+
         // seriesOutlineStroke
         BasicStroke s = new BasicStroke(1.23f);
         p1.setSeriesOutlineStroke(s);
         assertFalse(p1.equals(p2));
         p2.setSeriesOutlineStroke(s);
         assertTrue(p1.equals(p2));
-        
+
         // seriesOutlineStrokeList
         p1.setSeriesOutlineStroke(1, s);
         assertFalse(p1.equals(p2));
         p2.setSeriesOutlineStroke(1, s);
         assertTrue(p1.equals(p2));
-        
+
         // baseSeriesOutlineStroke
         p1.setBaseSeriesOutlineStroke(s);
         assertFalse(p1.equals(p2));
@@ -217,7 +217,7 @@ public class SpiderWebPlotTests extends TestCase {
         assertFalse(p1.equals(p2));
         p2.setWebFilled(false);
         assertTrue(p1.equals(p2));
-        
+
         // axisLabelGap
         p1.setAxisLabelGap(0.11);
         assertFalse(p1.equals(p2));
@@ -229,12 +229,12 @@ public class SpiderWebPlotTests extends TestCase {
         assertFalse(p1.equals(p2));
         p2.setLabelFont(new Font("Serif", Font.PLAIN, 9));
         assertTrue(p1.equals(p2));
-        
+
         // labelPaint
-        p1.setLabelPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        p1.setLabelPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.blue));
         assertFalse(p1.equals(p2));
-        p2.setLabelPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        p2.setLabelPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.blue));
         assertTrue(p1.equals(p2));
 
@@ -245,7 +245,7 @@ public class SpiderWebPlotTests extends TestCase {
         p2.setLabelGenerator(new StandardCategoryItemLabelGenerator("XYZ: {0}",
                 new DecimalFormat("0.000")));
         assertTrue(p1.equals(p2));
-        
+
         // toolTipGenerator
         p1.setToolTipGenerator(new StandardCategoryToolTipGenerator());
         assertFalse(p1.equals(p2));
@@ -257,13 +257,13 @@ public class SpiderWebPlotTests extends TestCase {
         assertFalse(p1.equals(p2));
         p2.setURLGenerator(new StandardCategoryURLGenerator());
         assertTrue(p1.equals(p2));
-        
+
         // axisLinePaint
         p1.setAxisLinePaint(Color.red);
         assertFalse(p1.equals(p2));
         p2.setAxisLinePaint(Color.red);
         assertTrue(p1.equals(p2));
-        
+
         // axisLineStroke
         p1.setAxisLineStroke(new BasicStroke(1.1f));
         assertFalse(p1.equals(p2));
@@ -288,31 +288,31 @@ public class SpiderWebPlotTests extends TestCase {
         assertTrue(p1 != p2);
         assertTrue(p1.getClass() == p2.getClass());
         assertTrue(p1.equals(p2));
-        
+
         // change the legendItemShape
         legendShape.setRect(4.0, 3.0, 2.0, 1.0);
         assertFalse(p1.equals(p2));
         p2.setLegendItemShape(legendShape);
         assertTrue(p1.equals(p2));
-        
+
         // change a series paint
         p1.setSeriesPaint(1, Color.black);
         assertFalse(p1.equals(p2));
         p2.setSeriesPaint(1, Color.black);
         assertTrue(p1.equals(p2));
-        
+
         // change a series outline paint
         p1.setSeriesOutlinePaint(0, Color.red);
         assertFalse(p1.equals(p2));
         p2.setSeriesOutlinePaint(0, Color.red);
         assertTrue(p1.equals(p2));
-        
+
         // change a series outline stroke
         p1.setSeriesOutlineStroke(0, new BasicStroke(1.1f));
         assertFalse(p1.equals(p2));
         p2.setSeriesOutlineStroke(0, new BasicStroke(1.1f));
         assertTrue(p1.equals(p2));
-        
+
     }
 
     /**
@@ -340,9 +340,9 @@ public class SpiderWebPlotTests extends TestCase {
         assertEquals(p1, p2);
 
     }
- 
+
     /**
-     * Draws the chart with a null info object to make sure that no exceptions 
+     * Draws the chart with a null info object to make sure that no exceptions
      * are thrown.
      */
     public void testDrawWithNullInfo() {
@@ -356,7 +356,7 @@ public class SpiderWebPlotTests extends TestCase {
         JFreeChart chart = new JFreeChart(plot);
         boolean success = false;
         try {
-            BufferedImage image = new BufferedImage(200 , 100, 
+            BufferedImage image = new BufferedImage(200 , 100,
                     BufferedImage.TYPE_INT_RGB);
             Graphics2D g2 = image.createGraphics();
             chart.draw(g2, new Rectangle2D.Double(0, 0, 200, 100), null, null);
@@ -367,6 +367,6 @@ public class SpiderWebPlotTests extends TestCase {
             success = false;
         }
         assertTrue(success);
-    }    
-    
+    }
+
 }
