0ffd6c79386d64389d83d926c5a92bfbb77b68be
David Gilbert <jfree@users.noreply.github.com>
2008-06-02 18:47:17 +0000

    Whitespace.

--- source/org/jfree/chart/renderer/xy/StackedXYAreaRenderer2.java
+++ source/org/jfree/chart/renderer/xy/StackedXYAreaRenderer2.java
@@ -2,55 +2,55 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------------------
  * StackedXYAreaRenderer2.java
  * ---------------------------
- * (C) Copyright 2004-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2004-2008, by Object Refinery Limited and Contributors.
  *
- * Original Author:  David Gilbert (for Object Refinery Limited), based on 
+ * Original Author:  David Gilbert (for Object Refinery Limited), based on
  *                   the StackedXYAreaRenderer class by Richard Atkinson;
  * Contributor(s):   -;
  *
  * Changes:
  * --------
  * 30-Apr-2004 : Version 1 (DG);
- * 15-Jul-2004 : Switched getX() with getXValue() and getY() with 
+ * 15-Jul-2004 : Switched getX() with getXValue() and getY() with
  *               getYValue() (DG);
  * 10-Sep-2004 : Removed getRangeType() method (DG);
  * 06-Jan-2004 : Renamed getRangeExtent() --> findRangeBounds (DG);
  * 28-Mar-2005 : Use getXValue() and getYValue() from dataset (DG);
  * 03-Oct-2005 : Add entity generation to drawItem() method (DG);
  * ------------- JFREECHART 1.0.x ---------------------------------------------
- * 22-Aug-2006 : Handle null and empty datasets correctly in the 
+ * 22-Aug-2006 : Handle null and empty datasets correctly in the
  *               findRangeBounds() method (DG);
- * 22-Sep-2006 : Added a flag to allow rounding of x-coordinates (after 
+ * 22-Sep-2006 : Added a flag to allow rounding of x-coordinates (after
  *               translation to Java2D space) in order to avoid the striping
- *               that can result from anti-aliasing (thanks to Doug 
+ *               that can result from anti-aliasing (thanks to Doug
  *               Clayton) (DG);
  * 30-Nov-2006 : Added accessor methods for the roundXCoordinates flag (DG);
- * 
+ *
  */
 
 package org.jfree.chart.renderer.xy;
@@ -79,25 +79,23 @@ import org.jfree.util.PublicCloneable;
 /**
  * A stacked area renderer for the {@link XYPlot} class.
  */
-public class StackedXYAreaRenderer2 extends XYAreaRenderer2 
-                                    implements Cloneable, 
-                                               PublicCloneable,
-                                               Serializable {
+public class StackedXYAreaRenderer2 extends XYAreaRenderer2
+        implements Cloneable, PublicCloneable, Serializable {
 
     /** For serialization. */
     private static final long serialVersionUID = 7752676509764539182L;
-    
+
     /**
-     * This flag controls whether or not the x-coordinates (in Java2D space) 
+     * This flag controls whether or not the x-coordinates (in Java2D space)
      * are rounded to integers.  When set to true, this can avoid the vertical
      * striping that anti-aliasing can generate.  However, the rounding may not
-     * be appropriate for output in high resolution formats (for example, 
+     * be appropriate for output in high resolution formats (for example,
      * vector graphics formats such as SVG and PDF).
-     * 
+     *
      * @since 1.0.3
      */
     private boolean roundXCoordinates;
-    
+
     /**
      * Creates a new renderer.
      */
@@ -112,35 +110,35 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
      *                        is none.
      * @param urlGenerator  the URL generator (<code>null</code> permitted).
      */
-    public StackedXYAreaRenderer2(XYToolTipGenerator labelGenerator, 
+    public StackedXYAreaRenderer2(XYToolTipGenerator labelGenerator,
                                   XYURLGenerator urlGenerator) {
         super(labelGenerator, urlGenerator);
         this.roundXCoordinates = true;
     }
-    
+
     /**
      * Returns the flag that controls whether or not the x-coordinates (in
      * Java2D space) are rounded to integer values.
-     * 
+     *
      * @return The flag.
-     * 
+     *
      * @since 1.0.4
-     * 
+     *
      * @see #setRoundXCoordinates(boolean)
      */
     public boolean getRoundXCoordinates() {
         return this.roundXCoordinates;
     }
-    
+
     /**
-     * Sets the flag that controls whether or not the x-coordinates (in 
-     * Java2D space) are rounded to integer values, and sends a 
+     * Sets the flag that controls whether or not the x-coordinates (in
+     * Java2D space) are rounded to integer values, and sends a
      * {@link RendererChangeEvent} to all registered listeners.
-     * 
+     *
      * @param round  the new flag value.
-     * 
+     *
      * @since 1.0.4
-     * 
+     *
      * @see #getRoundXCoordinates()
      */
     public void setRoundXCoordinates(boolean round) {
@@ -149,12 +147,12 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
     }
 
     /**
-     * Returns the range of values the renderer requires to display all the 
+     * Returns the range of values the renderer requires to display all the
      * items from the specified dataset.
-     * 
+     *
      * @param dataset  the dataset (<code>null</code> permitted).
-     * 
-     * @return The range (or <code>null</code> if the dataset is 
+     *
+     * @return The range (or <code>null</code> if the dataset is
      *         <code>null</code> or empty).
      */
     public Range findRangeBounds(XYDataset dataset) {
@@ -166,7 +164,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
         TableXYDataset d = (TableXYDataset) dataset;
         int itemCount = d.getItemCount();
         for (int i = 0; i < itemCount; i++) {
-            double[] stackValues = getStackValues((TableXYDataset) dataset, 
+            double[] stackValues = getStackValues((TableXYDataset) dataset,
                     d.getSeriesCount(), i);
             min = Math.min(min, stackValues[0]);
             max = Math.max(max, stackValues[1]);
@@ -179,7 +177,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
 
     /**
      * Returns the number of passes required by the renderer.
-     * 
+     *
      * @return 1.
      */
     public int getPassCount() {
@@ -193,7 +191,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
      * @param state  the renderer state.
      * @param dataArea  the area within which the data is being drawn.
      * @param info  collects information about the drawing.
-     * @param plot  the plot (can be used to obtain standard color information 
+     * @param plot  the plot (can be used to obtain standard color information
      *              etc).
      * @param domainAxis  the domain axis.
      * @param rangeAxis  the range axis.
@@ -224,34 +222,34 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
         }
 
         TableXYDataset tdataset = (TableXYDataset) dataset;
-        
+
         // get the data point...
         double x1 = dataset.getXValue(series, item);
         double y1 = dataset.getYValue(series, item);
         if (Double.isNaN(y1)) {
             y1 = 0.0;
-        }        
+        }
         double[] stack1 = getStackValues(tdataset, series, item);
-        
-        // get the previous point and the next point so we can calculate a 
+
+        // get the previous point and the next point so we can calculate a
         // "hot spot" for the area (used by the chart entity)...
         double x0 = dataset.getXValue(series, Math.max(item - 1, 0));
         double y0 = dataset.getYValue(series, Math.max(item - 1, 0));
         if (Double.isNaN(y0)) {
             y0 = 0.0;
         }
-        double[] stack0 = getStackValues(tdataset, series, Math.max(item - 1, 
+        double[] stack0 = getStackValues(tdataset, series, Math.max(item - 1,
                 0));
-        
+
         int itemCount = dataset.getItemCount(series);
-        double x2 = dataset.getXValue(series, Math.min(item + 1, 
+        double x2 = dataset.getXValue(series, Math.min(item + 1,
                 itemCount - 1));
-        double y2 = dataset.getYValue(series, Math.min(item + 1, 
+        double y2 = dataset.getYValue(series, Math.min(item + 1,
                 itemCount - 1));
         if (Double.isNaN(y2)) {
             y2 = 0.0;
         }
-        double[] stack2 = getStackValues(tdataset, series, Math.min(item + 1, 
+        double[] stack2 = getStackValues(tdataset, series, Math.min(item + 1,
                 itemCount - 1));
 
         double xleft = (x0 + x1) / 2.0;
@@ -260,38 +258,38 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
         double[] stackRight = averageStackValues(stack1, stack2);
         double[] adjStackLeft = adjustedStackValues(stack0, stack1);
         double[] adjStackRight = adjustedStackValues(stack1, stack2);
-        
+
         RectangleEdge edge0 = plot.getDomainAxisEdge();
-        
+
         float transX1 = (float) domainAxis.valueToJava2D(x1, dataArea, edge0);
-        float transXLeft = (float) domainAxis.valueToJava2D(xleft, dataArea, 
+        float transXLeft = (float) domainAxis.valueToJava2D(xleft, dataArea,
                 edge0);
-        float transXRight = (float) domainAxis.valueToJava2D(xright, dataArea, 
+        float transXRight = (float) domainAxis.valueToJava2D(xright, dataArea,
                 edge0);
-        
+
         if (this.roundXCoordinates) {
             transX1 = Math.round(transX1);
             transXLeft = Math.round(transXLeft);
             transXRight = Math.round(transXRight);
         }
         float transY1;
-        
+
         RectangleEdge edge1 = plot.getRangeAxisEdge();
-        
+
         GeneralPath left = new GeneralPath();
         GeneralPath right = new GeneralPath();
         if (y1 >= 0.0) {  // handle positive value
-            transY1 = (float) rangeAxis.valueToJava2D(y1 + stack1[1], dataArea, 
+            transY1 = (float) rangeAxis.valueToJava2D(y1 + stack1[1], dataArea,
                     edge1);
-            float transStack1 = (float) rangeAxis.valueToJava2D(stack1[1], 
+            float transStack1 = (float) rangeAxis.valueToJava2D(stack1[1],
                     dataArea, edge1);
             float transStackLeft = (float) rangeAxis.valueToJava2D(
                     adjStackLeft[1], dataArea, edge1);
-            
+
             // LEFT POLYGON
             if (y0 >= 0.0) {
                 double yleft = (y0 + y1) / 2.0 + stackLeft[1];
-                float transYLeft 
+                float transYLeft
                     = (float) rangeAxis.valueToJava2D(yleft, dataArea, edge1);
                 left.moveTo(transX1, transY1);
                 left.lineTo(transX1, transStack1);
@@ -311,7 +309,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
             // RIGHT POLYGON
             if (y2 >= 0.0) {
                 double yright = (y1 + y2) / 2.0 + stackRight[1];
-                float transYRight 
+                float transYRight
                     = (float) rangeAxis.valueToJava2D(yright, dataArea, edge1);
                 right.moveTo(transX1, transStack1);
                 right.lineTo(transX1, transY1);
@@ -326,10 +324,10 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
                 right.closePath();
             }
         }
-        else {  // handle negative value 
+        else {  // handle negative value
             transY1 = (float) rangeAxis.valueToJava2D(y1 + stack1[0], dataArea,
                     edge1);
-            float transStack1 = (float) rangeAxis.valueToJava2D(stack1[0], 
+            float transStack1 = (float) rangeAxis.valueToJava2D(stack1[0],
                     dataArea, edge1);
             float transStackLeft = (float) rangeAxis.valueToJava2D(
                     adjStackLeft[0], dataArea, edge1);
@@ -343,7 +341,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
             }
             else {
                 double yleft = (y0 + y1) / 2.0 + stackLeft[0];
-                float transYLeft = (float) rangeAxis.valueToJava2D(yleft, 
+                float transYLeft = (float) rangeAxis.valueToJava2D(yleft,
                         dataArea, edge1);
                 left.moveTo(transX1, transY1);
                 left.lineTo(transX1, transStack1);
@@ -353,7 +351,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
             }
             float transStackRight = (float) rangeAxis.valueToJava2D(
                     adjStackRight[0], dataArea, edge1);
-            
+
             // RIGHT POLYGON
             if (y2 >= 0.0) {
                 right.moveTo(transX1, transStack1);
@@ -363,7 +361,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
             }
             else {
                 double yright = (y1 + y2) / 2.0 + stackRight[0];
-                float transYRight = (float) rangeAxis.valueToJava2D(yright, 
+                float transYRight = (float) rangeAxis.valueToJava2D(yright,
                         dataArea, edge1);
                 right.moveTo(transX1, transStack1);
                 right.lineTo(transX1, transY1);
@@ -379,22 +377,22 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
             g2.setPaint(itemPaint);
             g2.fill(left);
             g2.fill(right);
-        } 
-        
+        }
+
         // add an entity for the item...
         if (entities != null) {
             GeneralPath gp = new GeneralPath(left);
             gp.append(right, false);
             entityArea = gp;
-            addEntity(entities, entityArea, dataset, series, item, 
+            addEntity(entities, entityArea, dataset, series, item,
                     transX1, transY1);
         }
 
     }
 
     /**
-     * Calculates the stacked values (one positive and one negative) of all 
-     * series up to, but not including, <code>series</code> for the specified 
+     * Calculates the stacked values (one positive and one negative) of all
+     * series up to, but not including, <code>series</code> for the specified
      * item. It returns [0.0, 0.0] if <code>series</code> is the first series.
      *
      * @param dataset  the dataset (<code>null</code> not permitted).
@@ -402,33 +400,33 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
      * @param index  the item index.
      *
      * @return An array containing the cumulative negative and positive values
-     *     for all series values up to but excluding <code>series</code> 
+     *     for all series values up to but excluding <code>series</code>
      *     for <code>index</code>.
      */
-    private double[] getStackValues(TableXYDataset dataset, 
+    private double[] getStackValues(TableXYDataset dataset,
                                     int series, int index) {
         double[] result = new double[2];
         for (int i = 0; i < series; i++) {
             double v = dataset.getYValue(i, index);
             if (!Double.isNaN(v)) {
                 if (v >= 0.0) {
-                    result[1] += v;   
+                    result[1] += v;
                 }
                 else {
-                    result[0] += v;   
+                    result[0] += v;
                 }
             }
         }
         return result;
     }
-    
+
     /**
-     * Returns a pair of "stack" values calculated as the mean of the two 
+     * Returns a pair of "stack" values calculated as the mean of the two
      * specified stack value pairs.
-     * 
+     *
      * @param stack1  the first stack pair.
      * @param stack2  the second stack pair.
-     * 
+     *
      * @return A pair of average stack values.
      */
     private double[] averageStackValues(double[] stack1, double[] stack2) {
@@ -442,22 +440,22 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
      * Calculates adjusted stack values from the supplied values.  The value is
      * the mean of the supplied values, unless either of the supplied values
      * is zero, in which case the adjusted value is zero also.
-     * 
+     *
      * @param stack1  the first stack pair.
      * @param stack2  the second stack pair.
-     * 
+     *
      * @return A pair of average stack values.
      */
     private double[] adjustedStackValues(double[] stack1, double[] stack2) {
         double[] result = new double[2];
         if (stack1[0] == 0.0 || stack2[0] == 0.0) {
-            result[0] = 0.0;   
+            result[0] = 0.0;
         }
         else {
             result[0] = (stack1[0] + stack2[0]) / 2.0;
         }
         if (stack1[1] == 0.0 || stack2[1] == 0.0) {
-            result[1] = 0.0;   
+            result[1] = 0.0;
         }
         else {
             result[1] = (stack1[1] + stack2[1]) / 2.0;
@@ -467,9 +465,9 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
 
     /**
      * Tests this renderer for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
@@ -485,7 +483,7 @@ public class StackedXYAreaRenderer2 extends XYAreaRenderer2
         }
         return super.equals(obj);
     }
-    
+
     /**
      * Returns a clone of the renderer.
      *
