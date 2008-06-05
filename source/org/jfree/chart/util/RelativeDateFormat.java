eba8b1eaaeb38d90c3c811e4086fb530b59238cc
David Gilbert <jfree@users.noreply.github.com>
2008-06-05 09:39:42 +0000

    Removed 1.4+ code in main().

--- source/org/jfree/chart/util/RelativeDateFormat.java
+++ source/org/jfree/chart/util/RelativeDateFormat.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------
@@ -35,9 +35,9 @@
  * Changes:
  * --------
  * 01-Nov-2006 : Version 1 (DG);
- * 23-Nov-2006 : Added argument checks, updated equals(), added clone() and 
+ * 23-Nov-2006 : Added argument checks, updated equals(), added clone() and
  *               hashCode() (DG);
- * 15-Feb-2008 : Applied patch 1873328 by Michael Siemer, with minor 
+ * 15-Feb-2008 : Applied patch 1873328 by Michael Siemer, with minor
  *               modifications (DG);
  *
  */
@@ -60,56 +60,56 @@ import java.util.GregorianCalendar;
  * @since 1.0.3
  */
 public class RelativeDateFormat extends DateFormat {
-    
+
     /** The base milliseconds for the elapsed time calculation. */
     private long baseMillis;
-    
+
     /**
      * A flag that controls whether or not a zero day count is displayed.
      */
     private boolean showZeroDays;
-    
+
     /**
      * A flag that controls whether or not a zero hour count is displayed.
-     * 
+     *
      * @since 1.0.10
      */
     private boolean showZeroHours;
 
-    /** 
+    /**
      * A formatter for the day count (most likely not critical until the
-     * day count exceeds 999). 
+     * day count exceeds 999).
      */
     private NumberFormat dayFormatter;
 
     /**
-     * A prefix prepended to the start of the format if the relative date is 
+     * A prefix prepended to the start of the format if the relative date is
      * positive.
      *
      * @since 1.0.10
      */
     private String positivePrefix;
-    
+
     /**
      * A string appended after the day count.
      */
     private String daySuffix;
-    
+
     /**
      * A string appended after the hours.
      */
     private String hourSuffix;
-    
+
     /**
      * A string appended after the minutes.
      */
     private String minuteSuffix;
-    
+
     /**
      * A formatter for the seconds (and milliseconds).
      */
     private NumberFormat secondFormatter;
-    
+
     /**
      * A string appended after the seconds.
      */
@@ -124,30 +124,30 @@ public class RelativeDateFormat extends DateFormat {
      * A constant for the number of milliseconds in one day.
      */
     private static long MILLISECONDS_IN_ONE_DAY = 24 * MILLISECONDS_IN_ONE_HOUR;
-    
+
     /**
      * Creates a new instance.
      */
     public RelativeDateFormat() {
-        this(0L);  
+        this(0L);
     }
-    
+
     /**
      * Creates a new instance.
-     * 
+     *
      * @param time  the date/time (<code>null</code> not permitted).
      */
     public RelativeDateFormat(Date time) {
         this(time.getTime());
     }
-    
+
     /**
      * Creates a new instance.
-     * 
+     *
      * @param baseMillis  the time zone (<code>null</code> not permitted).
      */
     public RelativeDateFormat(long baseMillis) {
-        super();        
+        super();
         this.baseMillis = baseMillis;
         this.showZeroDays = false;
         this.showZeroHours = true;
@@ -161,67 +161,67 @@ public class RelativeDateFormat extends DateFormat {
         this.secondFormatter.setMinimumFractionDigits(3);
         this.secondSuffix = "s";
 
-        // we don't use the calendar or numberFormat fields, but equals(Object) 
+        // we don't use the calendar or numberFormat fields, but equals(Object)
         // is failing without them being non-null
         this.calendar = new GregorianCalendar();
-        this.numberFormat = new DecimalFormat("0");    
+        this.numberFormat = new DecimalFormat("0");
     }
-    
+
     /**
-     * Returns the base date/time used to calculate the elapsed time for 
+     * Returns the base date/time used to calculate the elapsed time for
      * display.
-     * 
+     *
      * @return The base date/time in milliseconds since 1-Jan-1970.
-     * 
+     *
      * @see #setBaseMillis(long)
      */
     public long getBaseMillis() {
         return this.baseMillis;
     }
-    
+
     /**
-     * Sets the base date/time used to calculate the elapsed time for display.  
+     * Sets the base date/time used to calculate the elapsed time for display.
      * This should be specified in milliseconds using the same encoding as
      * <code>java.util.Date</code>.
-     * 
+     *
      * @param baseMillis  the base date/time in milliseconds.
-     * 
+     *
      * @see #getBaseMillis()
      */
     public void setBaseMillis(long baseMillis) {
         this.baseMillis = baseMillis;
     }
-    
+
     /**
-     * Returns the flag that controls whether or not zero day counts are 
+     * Returns the flag that controls whether or not zero day counts are
      * shown in the formatted output.
-     * 
+     *
      * @return The flag.
-     * 
+     *
      * @see #setShowZeroDays(boolean)
      */
     public boolean getShowZeroDays() {
         return this.showZeroDays;
     }
-    
+
     /**
      * Sets the flag that controls whether or not zero day counts are shown
      * in the formatted output.
-     * 
+     *
      * @param show  the flag.
-     * 
+     *
      * @see #getShowZeroDays()
      */
     public void setShowZeroDays(boolean show) {
         this.showZeroDays = show;
     }
-    
+
     /**
-     * Returns the flag that controls whether or not zero hour counts are 
+     * Returns the flag that controls whether or not zero hour counts are
      * shown in the formatted output.
-     * 
+     *
      * @return The flag.
-     * 
+     *
      * @see #setShowZeroHours(boolean)
      *
      * @since 1.0.10
@@ -229,13 +229,13 @@ public class RelativeDateFormat extends DateFormat {
     public boolean getShowZeroHours() {
         return this.showZeroHours;
     }
-    
+
     /**
      * Sets the flag that controls whether or not zero hour counts are shown
      * in the formatted output.
-     * 
+     *
      * @param show  the flag.
-     * 
+     *
      * @see #getShowZeroHours()
      *
      * @since 1.0.10
@@ -243,13 +243,13 @@ public class RelativeDateFormat extends DateFormat {
     public void setShowZeroHours(boolean show) {
         this.showZeroHours = show;
     }
-    
+
     /**
      * Returns the string that is prepended to the format if the relative time
      * is positive.
-     * 
+     *
      * @return The string (never <code>null</code>).
-     * 
+     *
      * @see #setPositivePrefix(String)
      *
      * @since 1.0.10
@@ -257,13 +257,13 @@ public class RelativeDateFormat extends DateFormat {
     public String getPositivePrefix() {
         return this.positivePrefix;
     }
-    
+
     /**
-     * Sets the string that is prepended to the format if the relative time is 
+     * Sets the string that is prepended to the format if the relative time is
      * positive.
-     * 
+     *
      * @param prefix  the prefix (<code>null</code> not permitted).
-     * 
+     *
      * @see #getPositivePrefix()
      *
      * @since 1.0.10
@@ -274,23 +274,23 @@ public class RelativeDateFormat extends DateFormat {
         }
         this.positivePrefix = prefix;
     }
-    
+
     /**
      * Returns the string that is appended to the day count.
-     * 
+     *
      * @return The string.
-     * 
+     *
      * @see #setDaySuffix(String)
      */
     public String getDaySuffix() {
         return this.daySuffix;
     }
-    
+
     /**
      * Sets the string that is appended to the day count.
-     * 
+     *
      * @param suffix  the suffix (<code>null</code> not permitted).
-     * 
+     *
      * @see #getDaySuffix()
      */
     public void setDaySuffix(String suffix) {
@@ -302,20 +302,20 @@ public class RelativeDateFormat extends DateFormat {
 
     /**
      * Returns the string that is appended to the hour count.
-     * 
+     *
      * @return The string.
-     * 
+     *
      * @see #setHourSuffix(String)
      */
     public String getHourSuffix() {
         return this.hourSuffix;
     }
-    
+
     /**
      * Sets the string that is appended to the hour count.
-     * 
+     *
      * @param suffix  the suffix (<code>null</code> not permitted).
-     * 
+     *
      * @see #getHourSuffix()
      */
     public void setHourSuffix(String suffix) {
@@ -327,20 +327,20 @@ public class RelativeDateFormat extends DateFormat {
 
     /**
      * Returns the string that is appended to the minute count.
-     * 
+     *
      * @return The string.
-     * 
+     *
      * @see #setMinuteSuffix(String)
      */
     public String getMinuteSuffix() {
         return this.minuteSuffix;
     }
-    
+
     /**
      * Sets the string that is appended to the minute count.
-     * 
+     *
      * @param suffix  the suffix (<code>null</code> not permitted).
-     * 
+     *
      * @see #getMinuteSuffix()
      */
     public void setMinuteSuffix(String suffix) {
@@ -352,20 +352,20 @@ public class RelativeDateFormat extends DateFormat {
 
     /**
      * Returns the string that is appended to the second count.
-     * 
+     *
      * @return The string.
-     * 
+     *
      * @see #setSecondSuffix(String)
      */
     public String getSecondSuffix() {
         return this.secondSuffix;
     }
-    
+
     /**
      * Sets the string that is appended to the second count.
-     * 
+     *
      * @param suffix  the suffix (<code>null</code> not permitted).
-     * 
+     *
      * @see #getSecondSuffix()
      */
     public void setSecondSuffix(String suffix) {
@@ -374,10 +374,10 @@ public class RelativeDateFormat extends DateFormat {
         }
         this.secondSuffix = suffix;
     }
-    
+
     /**
      * Sets the formatter for the seconds and milliseconds.
-     * 
+     *
      * @param formatter  the formatter (<code>null</code> not permitted).
      */
     public void setSecondFormatter(NumberFormat formatter) {
@@ -390,11 +390,11 @@ public class RelativeDateFormat extends DateFormat {
     /**
      * Formats the given date as the amount of elapsed time (relative to the
      * base date specified in the constructor).
-     * 
+     *
      * @param date  the date.
      * @param toAppendTo  the string buffer.
      * @param fieldPosition  the field position.
-     * 
+     *
      * @return The formatted date.
      */
     public StringBuffer format(Date date, StringBuffer toAppendTo,
@@ -405,11 +405,11 @@ public class RelativeDateFormat extends DateFormat {
         if (elapsed < 0) {
             elapsed *= -1L;
             signPrefix = "-";
-        } 
+        }
         else {
             signPrefix = this.positivePrefix;
         }
-        
+
         long days = elapsed / MILLISECONDS_IN_ONE_DAY;
         elapsed = elapsed - (days * MILLISECONDS_IN_ONE_DAY);
         long hours = elapsed / MILLISECONDS_IN_ONE_HOUR;
@@ -426,28 +426,28 @@ public class RelativeDateFormat extends DateFormat {
             toAppendTo.append(String.valueOf(hours) + getHourSuffix());
         }
         toAppendTo.append(String.valueOf(minutes) + getMinuteSuffix());
-        toAppendTo.append(this.secondFormatter.format(seconds) 
+        toAppendTo.append(this.secondFormatter.format(seconds)
                 + getSecondSuffix());
-        return toAppendTo;   
+        return toAppendTo;
     }
 
     /**
      * Parses the given string (not implemented).
-     * 
+     *
      * @param source  the date string.
      * @param pos  the parse position.
-     * 
+     *
      * @return <code>null</code>, as this method has not been implemented.
      */
     public Date parse(String source, ParsePosition pos) {
-        return null;   
+        return null;
     }
 
     /**
      * Tests this formatter for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
@@ -490,15 +490,15 @@ public class RelativeDateFormat extends DateFormat {
         }
         return true;
     }
-    
+
     /**
      * Returns a hash code for this instance.
-     * 
+     *
      * @return A hash code.
      */
     public int hashCode() {
         int result = 193;
-        result = 37 * result 
+        result = 37 * result
                 + (int) (this.baseMillis ^ (this.baseMillis >>> 32));
         result = 37 * result + this.positivePrefix.hashCode();
         result = 37 * result + this.daySuffix.hashCode();
@@ -511,7 +511,7 @@ public class RelativeDateFormat extends DateFormat {
 
     /**
      * Returns a clone of this instance.
-     * 
+     *
      * @return A clone.
      */
     public Object clone() {
@@ -520,32 +520,32 @@ public class RelativeDateFormat extends DateFormat {
         clone.secondFormatter = (NumberFormat) this.secondFormatter.clone();
         return clone;
     }
-    
+
     /**
      * Some test code.
-     * 
+     *
      * @param args  ignored.
      */
     public static void main(String[] args) {
         GregorianCalendar c0 = new GregorianCalendar(2006, 10, 1, 0, 0, 0);
         GregorianCalendar c1 = new GregorianCalendar(2006, 10, 1, 11, 37, 43);
         c1.set(Calendar.MILLISECOND, 123);
-        
+
         System.out.println("Default: ");
-        RelativeDateFormat rdf = new RelativeDateFormat(c0.getTimeInMillis());
+        RelativeDateFormat rdf = new RelativeDateFormat(c0.getTime().getTime());
         System.out.println(rdf.format(c1.getTime()));
         System.out.println();
-        
+
         System.out.println("Hide milliseconds: ");
         rdf.setSecondFormatter(new DecimalFormat("0"));
-        System.out.println(rdf.format(c1.getTime()));        
+        System.out.println(rdf.format(c1.getTime()));
         System.out.println();
 
         System.out.println("Show zero day output: ");
         rdf.setShowZeroDays(true);
         System.out.println(rdf.format(c1.getTime()));
         System.out.println();
-        
+
         System.out.println("Alternative suffixes: ");
         rdf.setShowZeroDays(false);
         rdf.setDaySuffix(":");
