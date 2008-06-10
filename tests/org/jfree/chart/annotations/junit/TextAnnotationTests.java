41e0d12fd661f73e113ef2dff539086a2fb0208d
David Gilbert <jfree@users.noreply.github.com>
2008-06-10 10:53:19 +0000

    Whitespace.

--- tests/org/jfree/chart/annotations/junit/TextAnnotationTests.java
+++ tests/org/jfree/chart/annotations/junit/TextAnnotationTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------------
  * TextAnnotationTests.java
  * ------------------------
- * (C) Copyright 2003-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2003-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -80,53 +80,53 @@ public class TextAnnotationTests extends TestCase {
      * Confirm that the equals method can distinguish all the required fields.
      */
     public void testEquals() {
-        
+
         TextAnnotation a1 = new CategoryTextAnnotation("Test", "Category", 1.0);
         TextAnnotation a2 = new CategoryTextAnnotation("Test", "Category", 1.0);
         assertTrue(a1.equals(a2));
-        
-        // text 
+
+        // text
         a1.setText("Text");
         assertFalse(a1.equals(a2));
         a2.setText("Text");
         assertTrue(a1.equals(a2));
 
-        // font 
+        // font
         a1.setFont(new Font("Serif", Font.BOLD, 18));
         assertFalse(a1.equals(a2));
         a2.setFont(new Font("Serif", Font.BOLD, 18));
         assertTrue(a1.equals(a2));
 
-        // paint 
-        a1.setPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        // paint
+        a1.setPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.pink));
         assertFalse(a1.equals(a2));
-        a2.setPaint(new GradientPaint(1.0f, 2.0f, Color.red, 
+        a2.setPaint(new GradientPaint(1.0f, 2.0f, Color.red,
                 3.0f, 4.0f, Color.pink));
         assertTrue(a1.equals(a2));
 
-        // textAnchor 
+        // textAnchor
         a1.setTextAnchor(TextAnchor.BOTTOM_LEFT);
         assertFalse(a1.equals(a2));
         a2.setTextAnchor(TextAnchor.BOTTOM_LEFT);
         assertTrue(a1.equals(a2));
 
-        // rotationAnchor 
+        // rotationAnchor
         a1.setRotationAnchor(TextAnchor.BOTTOM_LEFT);
         assertFalse(a1.equals(a2));
         a2.setRotationAnchor(TextAnchor.BOTTOM_LEFT);
         assertTrue(a1.equals(a2));
 
-        // rotationAngle 
+        // rotationAngle
         a1.setRotationAngle(Math.PI);
         assertFalse(a1.equals(a2));
         a2.setRotationAngle(Math.PI);
         assertTrue(a1.equals(a2));
 
     }
-    
+
     /**
-     * Two objects that are equal are required to return the same hashCode. 
+     * Two objects that are equal are required to return the same hashCode.
      */
     public void testHashCode() {
         TextAnnotation a1 = new CategoryTextAnnotation("Test", "Category", 1.0);
