79b667c162c7c3bae2552f9456e15aa0be37bd1f
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-06-16 20:26:00 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/PolarItemRenderer.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/PolarItemRenderer.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ----------------------
  * PolarItemRenderer.java
  * ----------------------
- * (C) Copyright 2004, 2007, by Solution Engineering, Inc. and Contributors.
+ * (C) Copyright 2004-2008, by Solution Engineering, Inc. and Contributors.
  *
  * Original Author:  Daniel Bridenbecker, Solution Engineering, Inc.;
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -55,10 +55,10 @@ import org.jfree.data.xy.XYDataset;
  * The interface for a renderer that can be used by the {@link PolarPlot} class.
  */
 public interface PolarItemRenderer {
-    
+
     /**
      * Plots the data for a given series.
-     * 
+     *
      * @param g2  the drawing surface.
      * @param dataArea  the data area.
      * @param info  collects plot rendering info.
@@ -66,46 +66,46 @@ public interface PolarItemRenderer {
      * @param dataset  the dataset.
      * @param seriesIndex  the series index.
      */
-    public void drawSeries(Graphics2D g2, 
-                           Rectangle2D dataArea, 
+    public void drawSeries(Graphics2D g2,
+                           Rectangle2D dataArea,
                            PlotRenderingInfo info,
                            PolarPlot plot,
                            XYDataset dataset,
                            int seriesIndex);
-    
+
     /**
      * Draw the angular gridlines - the spokes.
-     * 
+     *
      * @param g2  the drawing surface.
      * @param plot  the plot.
      * @param ticks  the ticks.
      * @param dataArea  the data area.
      */
-    public void drawAngularGridLines(Graphics2D g2, 
-                                     PolarPlot plot, 
+    public void drawAngularGridLines(Graphics2D g2,
+                                     PolarPlot plot,
                                      List ticks,
                                      Rectangle2D dataArea);
-    
+
     /**
      * Draw the radial gridlines - the rings.
-     * 
+     *
      * @param g2  the drawing surface.
      * @param plot  the plot.
      * @param radialAxis  the radial axis.
      * @param ticks  the ticks.
      * @param dataArea  the data area.
      */
-    public void drawRadialGridLines(Graphics2D g2, 
-                                    PolarPlot plot, 
+    public void drawRadialGridLines(Graphics2D g2,
+                                    PolarPlot plot,
                                     ValueAxis radialAxis,
                                     List ticks,
                                     Rectangle2D dataArea);
 
     /**
      * Return the legend for the given series.
-     * 
+     *
      * @param series  the series index.
-     * 
+     *
      * @return The legend item.
      */
     public LegendItem getLegendItem(int series);
@@ -120,7 +120,7 @@ public interface PolarItemRenderer {
     /**
      * Sets the plot that this renderer is assigned to.
      * <P>
-     * This method will be called by the plot class...you do not need to call 
+     * This method will be called by the plot class...you do not need to call
      * it yourself.
      *
      * @param plot  the plot.
@@ -129,17 +129,17 @@ public interface PolarItemRenderer {
 
     /**
      * Adds a change listener.
-     * 
+     *
      * @param listener  the listener.
      */
     public void addChangeListener(RendererChangeListener listener);
-    
+
     /**
      * Removes a change listener.
-     * 
+     *
      * @param listener  the listener.
      */
     public void removeChangeListener(RendererChangeListener listener);
-    
-    
+
+
 }
--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/RendererState.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/RendererState.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------
  * RendererState.java
  * ------------------
- * (C) Copyright 2003-2007, by Object Refinery Limited.
+ * (C) Copyright 2003-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,7 +35,7 @@
  * Changes:
  * --------
  * 07-Oct-2003 : Version 1 (DG);
- * 09-Jun-2005 : Added a convenience method to access the entity 
+ * 09-Jun-2005 : Added a convenience method to access the entity
  *               collection (DG);
  *
  */
@@ -50,33 +50,33 @@ import org.jfree.chart.plot.PlotRenderingInfo;
  * Represents the current state of a renderer.
  */
 public class RendererState {
-    
+
     /** The plot rendering info. */
     private PlotRenderingInfo info;
-    
+
     /**
      * Creates a new state object.
-     * 
+     *
      * @param info  the plot rendering info.
      */
     public RendererState(PlotRenderingInfo info) {
         this.info = info;
     }
-    
+
     /**
      * Returns the plot rendering info.
-     * 
+     *
      * @return The info.
      */
     public PlotRenderingInfo getInfo() {
         return this.info;
     }
-    
+
     /**
      * A convenience method that returns a reference to the entity
      * collection (may be <code>null</code>) being used to record
      * chart entities.
-     * 
+     *
      * @return The entity collection (possibly <code>null</code>).
      */
     public EntityCollection getEntityCollection() {
@@ -84,10 +84,10 @@ public class RendererState {
         if (this.info != null) {
             ChartRenderingInfo owner = this.info.getOwner();
             if (owner != null) {
-                result = owner.getEntityCollection(); 
+                result = owner.getEntityCollection();
             }
         }
         return result;
     }
-    
+
 }
