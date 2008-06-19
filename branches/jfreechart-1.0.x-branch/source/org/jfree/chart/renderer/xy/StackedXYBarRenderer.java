d2dd6e751f03294f7a927bf5f87f9e15b564b480
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-06-19 13:03:14 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/xy/StackedXYBarRenderer.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/renderer/xy/StackedXYBarRenderer.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -------------------------
  * StackedXYBarRenderer.java
  * -------------------------
- * (C) Copyright 2004-2007, by Andreas Schroeder and Contributors.
+ * (C) Copyright 2004-2008, by Andreas Schroeder and Contributors.
  *
  * Original Author:  Andreas Schroeder;
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -35,19 +35,19 @@
  * Changes
  * -------
  * 01-Apr-2004 : Version 1 (AS);
- * 15-Jul-2004 : Switched getX() with getXValue() and getY() with 
+ * 15-Jul-2004 : Switched getX() with getXValue() and getY() with
  *               getYValue() (DG);
- * 15-Aug-2004 : Added drawBarOutline to control draw/don't-draw bar 
+ * 15-Aug-2004 : Added drawBarOutline to control draw/don't-draw bar
  *               outlines (BN);
- * 10-Sep-2004 : drawBarOutline attribute is now inherited from XYBarRenderer 
- *               and double primitives are retrieved from the dataset rather 
+ * 10-Sep-2004 : drawBarOutline attribute is now inherited from XYBarRenderer
+ *               and double primitives are retrieved from the dataset rather
  *               than Number objects (DG);
  * 07-Jan-2005 : Updated for method name change in DatasetUtilities (DG);
  * 25-Jan-2005 : Modified to handle negative values correctly (DG);
  * ------------- JFREECHART 1.0.x ---------------------------------------------
  * 06-Dec-2006 : Added support for GradientPaint (DG);
  * 15-Mar-2007 : Added renderAsPercentages option (DG);
- * 
+ *
  */
 
 package org.jfree.chart.renderer.xy;
@@ -84,10 +84,10 @@ import org.jfree.ui.TextAnchor;
  * implements both interfaces.
  */
 public class StackedXYBarRenderer extends XYBarRenderer {
-  
+
     /** For serialization. */
     private static final long serialVersionUID = -7049101055533436444L;
-    
+
     /** A flag that controls whether the bars display values or percentages. */
     private boolean renderAsPercentages;
 
@@ -106,10 +106,10 @@ public class StackedXYBarRenderer extends XYBarRenderer {
     public StackedXYBarRenderer(double margin) {
         super(margin);
         this.renderAsPercentages = false;
-        
-        // set the default item label positions, which will only be used if 
+
+        // set the default item label positions, which will only be used if
         // the user requests visible item labels...
-        ItemLabelPosition p = new ItemLabelPosition(ItemLabelAnchor.CENTER, 
+        ItemLabelPosition p = new ItemLabelPosition(ItemLabelAnchor.CENTER,
                 TextAnchor.CENTER);
         setBasePositiveItemLabelPosition(p);
         setBaseNegativeItemLabelPosition(p);
@@ -119,40 +119,40 @@ public class StackedXYBarRenderer extends XYBarRenderer {
 
     /**
      * Returns <code>true</code> if the renderer displays each item value as
-     * a percentage (so that the stacked bars add to 100%), and 
+     * a percentage (so that the stacked bars add to 100%), and
      * <code>false</code> otherwise.
-     * 
+     *
      * @return A boolean.
-     * 
+     *
      * @see #setRenderAsPercentages(boolean)
-     * 
+     *
      * @since 1.0.5
      */
     public boolean getRenderAsPercentages() {
-        return this.renderAsPercentages;   
+        return this.renderAsPercentages;
     }
-    
+
     /**
      * Sets the flag that controls whether the renderer displays each item
      * value as a percentage (so that the stacked bars add to 100%), and sends
      * a {@link RendererChangeEvent} to all registered listeners.
-     * 
+     *
      * @param asPercentages  the flag.
-     * 
+     *
      * @see #getRenderAsPercentages()
-     * 
+     *
      * @since 1.0.5
      */
     public void setRenderAsPercentages(boolean asPercentages) {
-        this.renderAsPercentages = asPercentages; 
+        this.renderAsPercentages = asPercentages;
         fireChangeEvent();
     }
 
     /**
-     * Returns <code>2</code> to indicate that this renderer requires two 
-     * passes for drawing (item labels are drawn in the second pass so that 
+     * Returns <code>2</code> to indicate that this renderer requires two
+     * passes for drawing (item labels are drawn in the second pass so that
      * they always appear in front of all the bars).
-     * 
+     *
      * @return <code>2</code>.
      */
     public int getPassCount() {
@@ -160,8 +160,8 @@ public class StackedXYBarRenderer extends XYBarRenderer {
     }
 
     /**
-     * Initialises the renderer and returns a state object that should be 
-     * passed to all subsequent calls to the drawItem() method. Here there is 
+     * Initialises the renderer and returns a state object that should be
+     * passed to all subsequent calls to the drawItem() method. Here there is
      * nothing to do.
      *
      * @param g2  the graphics device.
@@ -182,11 +182,11 @@ public class StackedXYBarRenderer extends XYBarRenderer {
     }
 
     /**
-     * Returns the range of values the renderer requires to display all the 
+     * Returns the range of values the renderer requires to display all the
      * items from the specified dataset.
-     * 
+     *
      * @param dataset  the dataset (<code>null</code> permitted).
-     * 
+     *
      * @return The range (<code>null</code> if the dataset is <code>null</code>
      *         or empty).
      */
@@ -212,18 +212,18 @@ public class StackedXYBarRenderer extends XYBarRenderer {
      * @param state  the renderer state.
      * @param dataArea  the area within which the plot is being drawn.
      * @param info  collects information about the drawing.
-     * @param plot  the plot (can be used to obtain standard color information 
+     * @param plot  the plot (can be used to obtain standard color information
      *              etc).
      * @param domainAxis  the domain axis.
      * @param rangeAxis  the range axis.
      * @param dataset  the dataset.
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
-     * @param crosshairState  crosshair information for the plot 
+     * @param crosshairState  crosshair information for the plot
      *                        (<code>null</code> permitted).
      * @param pass  the pass index.
      */
-    public void drawItem(Graphics2D g2, 
+    public void drawItem(Graphics2D g2,
                          XYItemRendererState state,
                          Rectangle2D dataArea,
                          PlotRenderingInfo info,
@@ -235,10 +235,10 @@ public class StackedXYBarRenderer extends XYBarRenderer {
                          int item,
                          CrosshairState crosshairState,
                          int pass) {
-        
-        if (!(dataset instanceof IntervalXYDataset 
+
+        if (!(dataset instanceof IntervalXYDataset
                 && dataset instanceof TableXYDataset)) {
-            String message = "dataset (type " + dataset.getClass().getName() 
+            String message = "dataset (type " + dataset.getClass().getName()
                 + ") has wrong type:";
             boolean and = false;
             if (!IntervalXYDataset.class.isAssignableFrom(dataset.getClass())) {
@@ -260,24 +260,24 @@ public class StackedXYBarRenderer extends XYBarRenderer {
         if (Double.isNaN(value)) {
             return;
         }
-        
+
         // if we are rendering the values as percentages, we need to calculate
-        // the total for the current item.  Unfortunately here we end up 
+        // the total for the current item.  Unfortunately here we end up
         // repeating the calculation more times than is strictly necessary -
-        // hopefully I'll come back to this and find a way to add the 
+        // hopefully I'll come back to this and find a way to add the
         // total(s) to the renderer state.  The other problem is we implicitly
         // assume the dataset has no negative values...perhaps that can be
         // fixed too.
-        double total = 0.0;  
+        double total = 0.0;
         if (this.renderAsPercentages) {
             total = DatasetUtilities.calculateStackTotal(
                     (TableXYDataset) dataset, item);
             value = value / total;
         }
-        
+
         double positiveBase = 0.0;
         double negativeBase = 0.0;
-        
+
         for (int i = 0; i < series; i++) {
             double v = dataset.getYValue(i, item);
             if (!Double.isNaN(v)) {
@@ -297,15 +297,15 @@ public class StackedXYBarRenderer extends XYBarRenderer {
         double translatedValue;
         RectangleEdge edgeR = plot.getRangeAxisEdge();
         if (value > 0.0) {
-            translatedBase = rangeAxis.valueToJava2D(positiveBase, dataArea, 
+            translatedBase = rangeAxis.valueToJava2D(positiveBase, dataArea,
                     edgeR);
-            translatedValue = rangeAxis.valueToJava2D(positiveBase + value, 
+            translatedValue = rangeAxis.valueToJava2D(positiveBase + value,
                     dataArea, edgeR);
         }
         else {
-            translatedBase = rangeAxis.valueToJava2D(negativeBase, dataArea, 
+            translatedBase = rangeAxis.valueToJava2D(negativeBase, dataArea,
                     edgeR);
-            translatedValue = rangeAxis.valueToJava2D(negativeBase + value, 
+            translatedValue = rangeAxis.valueToJava2D(negativeBase + value,
                     dataArea, edgeR);
         }
 
@@ -314,7 +314,7 @@ public class StackedXYBarRenderer extends XYBarRenderer {
         if (Double.isNaN(startX)) {
             return;
         }
-        double translatedStartX = domainAxis.valueToJava2D(startX, dataArea, 
+        double translatedStartX = domainAxis.valueToJava2D(startX, dataArea,
                 edgeD);
 
         double endX = intervalDataset.getEndXValue(series, item);
@@ -323,7 +323,7 @@ public class StackedXYBarRenderer extends XYBarRenderer {
         }
         double translatedEndX = domainAxis.valueToJava2D(endX, dataArea, edgeD);
 
-        double translatedWidth = Math.max(1, Math.abs(translatedEndX 
+        double translatedWidth = Math.max(1, Math.abs(translatedEndX
                 - translatedStartX));
         double translatedHeight = Math.abs(translatedValue - translatedBase);
         if (getMargin() > 0.0) {
@@ -335,7 +335,7 @@ public class StackedXYBarRenderer extends XYBarRenderer {
         Rectangle2D bar = null;
         PlotOrientation orientation = plot.getOrientation();
         if (orientation == PlotOrientation.HORIZONTAL) {
-            bar = new Rectangle2D.Double(Math.min(translatedBase, 
+            bar = new Rectangle2D.Double(Math.min(translatedBase,
                     translatedValue), translatedEndX, translatedHeight,
                     translatedWidth);
         }
@@ -347,14 +347,14 @@ public class StackedXYBarRenderer extends XYBarRenderer {
 
         if (pass == 0) {
             Paint itemPaint = getItemPaint(series, item);
-            if (getGradientPaintTransformer() 
+            if (getGradientPaintTransformer()
                     != null && itemPaint instanceof GradientPaint) {
                 GradientPaint gp = (GradientPaint) itemPaint;
                 itemPaint = getGradientPaintTransformer().transform(gp, bar);
             }
             g2.setPaint(itemPaint);
             g2.fill(bar);
-            if (isDrawBarOutline() 
+            if (isDrawBarOutline()
                     && Math.abs(translatedEndX - translatedStartX) > 3) {
                 g2.setStroke(getItemStroke(series, item));
                 g2.setPaint(getItemOutlinePaint(series, item));
@@ -366,7 +366,7 @@ public class StackedXYBarRenderer extends XYBarRenderer {
                 EntityCollection entities = info.getOwner()
                         .getEntityCollection();
                 if (entities != null) {
-                    addEntity(entities, bar, dataset, series, item, 
+                    addEntity(entities, bar, dataset, series, item,
                             bar.getCenterX(), bar.getCenterY());
                 }
             }
@@ -375,39 +375,39 @@ public class StackedXYBarRenderer extends XYBarRenderer {
             // handle item label drawing, now that we know all the bars have
             // been drawn...
             if (isItemLabelVisible(series, item)) {
-                XYItemLabelGenerator generator = getItemLabelGenerator(series, 
+                XYItemLabelGenerator generator = getItemLabelGenerator(series,
                         item);
-                drawItemLabel(g2, dataset, series, item, plot, generator, bar, 
+                drawItemLabel(g2, dataset, series, item, plot, generator, bar,
                         value < 0.0);
             }
         }
 
     }
-    
+
     /**
      * Tests this renderer for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
         if (obj == this) {
-            return true;   
+            return true;
         }
         if (!(obj instanceof StackedXYBarRenderer)) {
-            return false;   
+            return false;
         }
         StackedXYBarRenderer that = (StackedXYBarRenderer) obj;
         if (this.renderAsPercentages != that.renderAsPercentages) {
-            return false;   
+            return false;
         }
         return super.equals(obj);
     }
-    
+
     /**
      * Returns a hash code for this instance.
-     * 
+     *
      * @return A hash code.
      */
     public int hashCode() {
@@ -415,5 +415,5 @@ public class StackedXYBarRenderer extends XYBarRenderer {
         result = result * 37 + (this.renderAsPercentages ? 1 : 0);
         return result;
     }
-    
+
 }
