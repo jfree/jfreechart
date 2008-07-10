5ffb9d1f8ce3cac7798d6dcc43aa26aa7b7c80c9
David Gilbert <jfree@users.noreply.github.com>
2008-07-10 14:13:20 +0000

    2008-07-10  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/data/time/ohlc/OHLCSeriesCollection.java
        (getXPosition): New method,
        (setXPosition): Likewise,
        (equals): Updated for xPosition attribute.

--- tests/org/jfree/data/time/ohlc/junit/OHLCSeriesTests.java
+++ tests/org/jfree/data/time/ohlc/junit/OHLCSeriesTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * --------------------
  * OHLCSeriesTests.java
  * --------------------
- * (C) Copyright 2006, 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2006-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -61,11 +61,16 @@ import org.jfree.data.time.ohlc.OHLCSeries;
 /**
  * Tests for the {@link OHLCSeries} class.
  */
-public class OHLCSeriesTests extends TestCase    
+public class OHLCSeriesTests extends TestCase
         implements SeriesChangeListener {
 
     SeriesChangeEvent lastEvent;
-    
+
+    /**
+     * Records a change event.
+     *
+     * @param event  the event.
+     */
     public void seriesChanged(SeriesChangeEvent event) {
         this.lastEvent = event;
     }
@@ -95,13 +100,13 @@ public class OHLCSeriesTests extends TestCase
         OHLCSeries s1 = new OHLCSeries("s1");
         OHLCSeries s2 = new OHLCSeries("s1");
         assertTrue(s1.equals(s2));
-        
+
         // seriesKey
         s1 = new OHLCSeries("s2");
         assertFalse(s1.equals(s2));
         s2 = new OHLCSeries("s2");
         assertTrue(s1.equals(s2));
-        
+
         // add a value
         s1.add(new Year(2006), 2.0, 4.0, 1.0, 3.0);
         assertFalse(s1.equals(s2));
@@ -147,7 +152,7 @@ public class OHLCSeriesTests extends TestCase
         OHLCSeries s1 = new OHLCSeries("s1");
         s1.add(new Year(2006), 2.0, 4.0, 1.0, 3.0);
         OHLCSeries s2 = null;
-        
+
         try {
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
             ObjectOutput out = new ObjectOutputStream(buffer);
@@ -165,7 +170,7 @@ public class OHLCSeriesTests extends TestCase
         assertEquals(s1, s2);
 
     }
-    
+
     /**
      * Simple test for the indexOf() method.
      */
@@ -188,14 +193,14 @@ public class OHLCSeriesTests extends TestCase
         s1.add(new Year(2011), 2.1, 4.1, 1.1, 3.1);
         s1.add(new Year(2010), 2.2, 4.2, 1.2, 3.2);
         assertEquals(3, s1.getItemCount());
-        
+
         s1.remove(new Year(2010));
         assertEquals(new Year(2011), s1.getPeriod(1));
-        
+
         s1.remove(new Year(2006));
         assertEquals(new Year(2011), s1.getPeriod(0));
     }
-    
+
     /**
      * If you add a duplicate period, an exception should be thrown.
      */
@@ -211,7 +216,7 @@ public class OHLCSeriesTests extends TestCase
         }
         assertTrue(pass);
     }
-    
+
     /**
      * A simple check that the maximumItemCount attribute is working.
      */
@@ -226,7 +231,7 @@ public class OHLCSeriesTests extends TestCase
         assertEquals(new Year(2007), s1.getPeriod(0));
         assertEquals(new Year(2008), s1.getPeriod(1));
     }
-    
+
     /**
      * Check that the maximum item count can be applied retrospectively.
      */
@@ -239,7 +244,7 @@ public class OHLCSeriesTests extends TestCase
         assertEquals(new Year(2007), s1.getPeriod(0));
         assertEquals(new Year(2008), s1.getPeriod(1));
     }
-    
+
     /**
      * Some checks for the clear() method.
      */
@@ -254,6 +259,6 @@ public class OHLCSeriesTests extends TestCase
         s1.clear();
         assertNotNull(this.lastEvent);
         assertTrue(s1.isEmpty());
-    } 
-    
+    }
+
 }
