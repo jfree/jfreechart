8e480e8846a8076bf03264d62b964f31ea92d3be
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-07-10 14:13:20 +0000

    2008-07-10  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/data/time/ohlc/OHLCSeriesCollection.java
        (getXPosition): New method,
        (setXPosition): Likewise,
        (equals): Updated for xPosition attribute.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/data/time/ohlc/junit/OHLCSeriesCollectionTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/data/time/ohlc/junit/OHLCSeriesCollectionTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------------------
  * OHLCSeriesCollectionTests.java
  * ------------------------------
- * (C) Copyright 2006, 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2006-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,6 +35,7 @@
  * Changes
  * -------
  * 04-Dec-2006 : Version 1 (DG);
+ * 10-Jul-2008 : Updated testEquals() method (DG);
  *
  */
 
@@ -51,6 +52,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.jfree.data.time.TimePeriodAnchor;
 import org.jfree.data.time.Year;
 import org.jfree.data.time.ohlc.OHLCSeries;
 import org.jfree.data.time.ohlc.OHLCSeriesCollection;
@@ -85,7 +87,7 @@ public class OHLCSeriesCollectionTests extends TestCase {
         OHLCSeriesCollection c1 = new OHLCSeriesCollection();
         OHLCSeriesCollection c2 = new OHLCSeriesCollection();
         assertEquals(c1, c2);
-        
+
         // add a series
         OHLCSeries s1 = new OHLCSeries("Series");
         s1.add(new Year(2006), 1.0, 1.1, 1.2, 1.3);
@@ -95,12 +97,18 @@ public class OHLCSeriesCollectionTests extends TestCase {
         s2.add(new Year(2006), 1.0, 1.1, 1.2, 1.3);
         c2.addSeries(s2);
         assertTrue(c1.equals(c2));
-        
+
         // add an empty series
         c1.addSeries(new OHLCSeries("Empty Series"));
         assertFalse(c1.equals(c2));
         c2.addSeries(new OHLCSeries("Empty Series"));
         assertTrue(c1.equals(c2));
+
+        c1.setXPosition(TimePeriodAnchor.END);
+        assertFalse(c1.equals(c2));
+        c2.setXPosition(TimePeriodAnchor.END);
+        assertTrue(c1.equals(c2));
+
     }
 
     /**
@@ -121,7 +129,7 @@ public class OHLCSeriesCollectionTests extends TestCase {
         assertTrue(c1 != c2);
         assertTrue(c1.getClass() == c2.getClass());
         assertTrue(c1.equals(c2));
-        
+
         // check independence
         s1.setDescription("XYZ");
         assertFalse(c1.equals(c2));
@@ -136,7 +144,7 @@ public class OHLCSeriesCollectionTests extends TestCase {
         s1.add(new Year(2006), 1.0, 1.1, 1.2, 1.3);
         c1.addSeries(s1);
         OHLCSeriesCollection c2 = null;
-        
+
         try {
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
             ObjectOutput out = new ObjectOutputStream(buffer);
@@ -153,9 +161,9 @@ public class OHLCSeriesCollectionTests extends TestCase {
         }
         assertEquals(c1, c2);
     }
-    
+
     /**
-     * A test for bug report 1170825 (originally affected XYSeriesCollection, 
+     * A test for bug report 1170825 (originally affected XYSeriesCollection,
      * this test is just copied over).
      */
     public void test1170825() {
@@ -172,5 +180,5 @@ public class OHLCSeriesCollectionTests extends TestCase {
             assertTrue(false);  // wrong outcome
         }
     }
-    
+
 }
