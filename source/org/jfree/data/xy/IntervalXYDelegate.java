57311e3dff4d678a85e3ebe39954a3c15875a5b5
David Gilbert <jfree@users.noreply.github.com>
2008-07-21 14:20:41 +0000

    Whitespace.

--- source/org/jfree/data/xy/IntervalXYDelegate.java
+++ source/org/jfree/data/xy/IntervalXYDelegate.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------
  * IntervalXYDelegate.java
  * -----------------------
- * (C) Copyright 2004, 2005, 2007, by Andreas Schroeder and Contributors.
+ * (C) Copyright 2004-2008, by Andreas Schroeder and Contributors.
  *
  * Original Author:  Andreas Schroeder;
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -35,18 +35,18 @@
  * Changes
  * -------
  * 31-Mar-2004 : Version 1 (AS);
- * 15-Jul-2004 : Switched getX() with getXValue() and getY() with 
+ * 15-Jul-2004 : Switched getX() with getXValue() and getY() with
  *               getYValue() (DG);
  * 18-Aug-2004 : Moved from org.jfree.data --> org.jfree.data.xy (DG);
  * 04-Nov-2004 : Added argument check for setIntervalWidth() method (DG);
  * 17-Nov-2004 : New methods to reflect changes in DomainInfo (DG);
- * 11-Jan-2005 : Removed deprecated methods in preparation for the 1.0.0 
+ * 11-Jan-2005 : Removed deprecated methods in preparation for the 1.0.0
  *               release (DG);
  * 21-Feb-2005 : Made public and added equals() method (DG);
- * 06-Oct-2005 : Implemented DatasetChangeListener to recalculate 
+ * 06-Oct-2005 : Implemented DatasetChangeListener to recalculate
  *               autoIntervalWidth (DG);
  * 02-Feb-2007 : Removed author tags all over JFreeChart sources (DG);
- *   
+ *
  */
 
 package org.jfree.data.xy;
@@ -64,29 +64,29 @@ import org.jfree.util.PublicCloneable;
 /**
  * A delegate that handles the specification or automatic calculation of the
  * interval surrounding the x-values in a dataset.  This is used to extend
- * a regular {@link XYDataset} to support the {@link IntervalXYDataset} 
+ * a regular {@link XYDataset} to support the {@link IntervalXYDataset}
  * interface.
- * <p> 
- * The decorator pattern was not used because of the several possibly 
- * implemented interfaces of the decorated instance (e.g. 
+ * <p>
+ * The decorator pattern was not used because of the several possibly
+ * implemented interfaces of the decorated instance (e.g.
  * {@link TableXYDataset}, {@link RangeInfo}, {@link DomainInfo} etc.).
  * <p>
  * The width can be set manually or calculated automatically. The switch
- * autoWidth allows to determine which behavior is used. The auto width 
+ * autoWidth allows to determine which behavior is used. The auto width
  * calculation tries to find the smallest gap between two x-values in the
- * dataset.  If there is only one item in the series, the auto width 
- * calculation fails and falls back on the manually set interval width (which 
- * is itself defaulted to 1.0). 
+ * dataset.  If there is only one item in the series, the auto width
+ * calculation fails and falls back on the manually set interval width (which
+ * is itself defaulted to 1.0).
  */
 public class IntervalXYDelegate implements DatasetChangeListener,
-                                           DomainInfo, Serializable, 
+                                           DomainInfo, Serializable,
                                            Cloneable, PublicCloneable {
-    
+
     /** For serialization. */
     private static final long serialVersionUID = -685166711639592857L;
-    
+
     /**
-     * The dataset to enhance. 
+     * The dataset to enhance.
      */
     private XYDataset dataset;
 
@@ -94,37 +94,37 @@ public class IntervalXYDelegate implements DatasetChangeListener,
      * A flag to indicate whether the width should be calculated automatically.
      */
     private boolean autoWidth;
-    
+
     /**
      * A value between 0.0 and 1.0 that indicates the position of the x-value
      * within the interval.
      */
-    private double intervalPositionFactor; 
-    
+    private double intervalPositionFactor;
+
     /**
      * The fixed interval width (defaults to 1.0).
      */
     private double fixedIntervalWidth;
-    
+
     /**
      * The automatically calculated interval width.
      */
     private double autoIntervalWidth;
-    
+
     /**
      * Creates a new delegate that.
-     * 
+     *
      * @param dataset  the underlying dataset (<code>null</code> not permitted).
      */
     public IntervalXYDelegate(XYDataset dataset) {
         this(dataset, true);
     }
-    
+
     /**
      * Creates a new delegate for the specified dataset.
-     * 
+     *
      * @param dataset  the underlying dataset (<code>null</code> not permitted).
-     * @param autoWidth  a flag that controls whether the interval width is 
+     * @param autoWidth  a flag that controls whether the interval width is
      *                   calculated automatically.
      */
     public IntervalXYDelegate(XYDataset dataset, boolean autoWidth) {
@@ -134,20 +134,20 @@ public class IntervalXYDelegate implements DatasetChangeListener,
         this.dataset = dataset;
         this.autoWidth = autoWidth;
         this.intervalPositionFactor = 0.5;
-        this.autoIntervalWidth = Double.POSITIVE_INFINITY; 
+        this.autoIntervalWidth = Double.POSITIVE_INFINITY;
         this.fixedIntervalWidth = 1.0;
     }
-    
+
     /**
-     * Returns <code>true</code> if the interval width is automatically 
+     * Returns <code>true</code> if the interval width is automatically
      * calculated, and <code>false</code> otherwise.
-     * 
+     *
      * @return A boolean.
      */
     public boolean isAutoWidth() {
         return this.autoWidth;
     }
-    
+
     /**
      * Sets the flag that indicates whether the interval width is automatically
      * calculated.  If the flag is set to <code>true</code>, the interval is
@@ -156,7 +156,7 @@ public class IntervalXYDelegate implements DatasetChangeListener,
      * Note: recalculating the interval amounts to changing the data values
      * represented by the dataset.  The calling dataset must fire an
      * appropriate {@link DatasetChangeEvent}.
-     * 
+     *
      * @param b  a boolean.
      */
     public void setAutoWidth(boolean b) {
@@ -165,10 +165,10 @@ public class IntervalXYDelegate implements DatasetChangeListener,
             this.autoIntervalWidth = recalculateInterval();
         }
     }
-    
+
     /**
      * Returns the interval position factor.
-     * 
+     *
      * @return The interval position factor.
      */
     public double getIntervalPositionFactor() {
@@ -179,16 +179,16 @@ public class IntervalXYDelegate implements DatasetChangeListener,
      * Sets the interval position factor.  This controls how the interval is
      * aligned to the x-value.  For a value of 0.5, the interval is aligned
      * with the x-value in the center.  For a value of 0.0, the interval is
-     * aligned with the x-value at the lower end of the interval, and for a 
+     * aligned with the x-value at the lower end of the interval, and for a
      * value of 1.0, the interval is aligned with the x-value at the upper
      * end of the interval.
-     * 
-     * Note that changing the interval position factor amounts to changing the 
-     * data values represented by the dataset.  Therefore, the dataset that is 
-     * using this delegate is responsible for generating the 
-     * appropriate {@link DatasetChangeEvent}.     
-     * 
-     * @param d  the new interval position factor (in the range 
+     *
+     * Note that changing the interval position factor amounts to changing the
+     * data values represented by the dataset.  Therefore, the dataset that is
+     * using this delegate is responsible for generating the
+     * appropriate {@link DatasetChangeEvent}.
+     *
+     * @param d  the new interval position factor (in the range
      *           <code>0.0</code> to <code>1.0</code> inclusive).
      */
     public void setIntervalPositionFactor(double d) {
@@ -201,22 +201,22 @@ public class IntervalXYDelegate implements DatasetChangeListener,
 
     /**
      * Returns the fixed interval width.
-     * 
+     *
      * @return The fixed interval width.
      */
     public double getFixedIntervalWidth() {
         return this.fixedIntervalWidth;
     }
-    
+
     /**
      * Sets the fixed interval width and, as a side effect, sets the
-     * <code>autoWidth</code> flag to <code>false</code>.  
-     * 
-     * Note that changing the interval width amounts to changing the data 
+     * <code>autoWidth</code> flag to <code>false</code>.
+     *
+     * Note that changing the interval width amounts to changing the data
      * values represented by the dataset.  Therefore, the dataset
-     * that is using this delegate is responsible for generating the 
+     * that is using this delegate is responsible for generating the
      * appropriate {@link DatasetChangeEvent}.
-     * 
+     *
      * @param w  the width (negative values not permitted).
      */
     public void setFixedIntervalWidth(double w) {
@@ -226,17 +226,17 @@ public class IntervalXYDelegate implements DatasetChangeListener,
         this.fixedIntervalWidth = w;
         this.autoWidth = false;
     }
-    
+
     /**
-     * Returns the interval width.  This method will return either the 
+     * Returns the interval width.  This method will return either the
      * auto calculated interval width or the manually specified interval
      * width, depending on the {@link #isAutoWidth()} result.
-     * 
+     *
      * @return The interval width to use.
      */
     public double getIntervalWidth() {
         if (isAutoWidth() && !Double.isInfinite(this.autoIntervalWidth)) {
-            // everything is fine: autoWidth is on, and an autoIntervalWidth 
+            // everything is fine: autoWidth is on, and an autoIntervalWidth
             // was set.
             return this.autoIntervalWidth;
         }
@@ -248,80 +248,80 @@ public class IntervalXYDelegate implements DatasetChangeListener,
 
     /**
      * Returns the start value of the x-interval for an item within a series.
-     * 
+     *
      * @param series  the series index.
      * @param item  the item index.
-     * 
+     *
      * @return The start value of the x-interval (possibly <code>null</code>).
-     * 
+     *
      * @see #getStartXValue(int, int)
      */
     public Number getStartX(int series, int item) {
         Number startX = null;
         Number x = this.dataset.getX(series, item);
         if (x != null) {
-            startX = new Double(x.doubleValue() 
-                     - (getIntervalPositionFactor() * getIntervalWidth())); 
+            startX = new Double(x.doubleValue()
+                     - (getIntervalPositionFactor() * getIntervalWidth()));
         }
         return startX;
     }
-    
+
     /**
      * Returns the start value of the x-interval for an item within a series.
-     * 
+     *
      * @param series  the series index.
      * @param item  the item index.
-     * 
+     *
      * @return The start value of the x-interval.
-     * 
+     *
      * @see #getStartX(int, int)
      */
     public double getStartXValue(int series, int item) {
-        return this.dataset.getXValue(series, item) 
+        return this.dataset.getXValue(series, item)
                 - getIntervalPositionFactor() * getIntervalWidth();
     }
-    
+
     /**
      * Returns the end value of the x-interval for an item within a series.
-     * 
+     *
      * @param series  the series index.
      * @param item  the item index.
-     * 
+     *
      * @return The end value of the x-interval (possibly <code>null</code>).
-     * 
+     *
      * @see #getEndXValue(int, int)
      */
     public Number getEndX(int series, int item) {
         Number endX = null;
         Number x = this.dataset.getX(series, item);
         if (x != null) {
-            endX = new Double(x.doubleValue() 
-                + ((1.0 - getIntervalPositionFactor()) * getIntervalWidth())); 
+            endX = new Double(x.doubleValue()
+                + ((1.0 - getIntervalPositionFactor()) * getIntervalWidth()));
         }
         return endX;
     }
 
     /**
      * Returns the end value of the x-interval for an item within a series.
-     * 
+     *
      * @param series  the series index.
      * @param item  the item index.
-     * 
+     *
      * @return The end value of the x-interval.
-     * 
+     *
      * @see #getEndX(int, int)
      */
     public double getEndXValue(int series, int item) {
-        return this.dataset.getXValue(series, item) 
+        return this.dataset.getXValue(series, item)
                 + (1.0 - getIntervalPositionFactor()) * getIntervalWidth();
     }
-    
+
     /**
      * Returns the minimum x-value in the dataset.
      *
      * @param includeInterval  a flag that determines whether or not the
      *                         x-interval is taken into account.
-     * 
+     *
      * @return The minimum value.
      */
     public double getDomainLowerBound(boolean includeInterval) {
@@ -338,7 +338,7 @@ public class IntervalXYDelegate implements DatasetChangeListener,
      *
      * @param includeInterval  a flag that determines whether or not the
      *                         x-interval is taken into account.
-     * 
+     *
      * @return The maximum value.
      */
     public double getDomainUpperBound(boolean includeInterval) {
@@ -354,9 +354,9 @@ public class IntervalXYDelegate implements DatasetChangeListener,
      * Returns the range of the values in the dataset's domain, including
      * or excluding the interval around each x-value as specified.
      *
-     * @param includeInterval  a flag that determines whether or not the 
+     * @param includeInterval  a flag that determines whether or not the
      *                         x-interval should be taken into account.
-     * 
+     *
      * @return The range.
      */
     public Range getDomainBounds(boolean includeInterval) {
@@ -366,18 +366,18 @@ public class IntervalXYDelegate implements DatasetChangeListener,
         if (includeInterval && range != null) {
             double lowerAdj = getIntervalWidth() * getIntervalPositionFactor();
             double upperAdj = getIntervalWidth() - lowerAdj;
-            range = new Range(range.getLowerBound() - lowerAdj, 
+            range = new Range(range.getLowerBound() - lowerAdj,
                 range.getUpperBound() + upperAdj);
         }
         return range;
     }
-    
+
     /**
-     * Handles events from the dataset by recalculating the interval if 
+     * Handles events from the dataset by recalculating the interval if
      * necessary.
-     * 
+     *
      * @param e  the event.
-     */    
+     */
     public void datasetChanged(DatasetChangeEvent e) {
         // TODO: by coding the event with some information about what changed
         // in the dataset, we could make the recalculation of the interval
@@ -386,10 +386,10 @@ public class IntervalXYDelegate implements DatasetChangeListener,
             this.autoIntervalWidth = recalculateInterval();
         }
     }
-    
+
     /**
      * Recalculate the minimum width "from scratch".
-     * 
+     *
      * @return The minimum width.
      */
     private double recalculateInterval() {
@@ -400,12 +400,12 @@ public class IntervalXYDelegate implements DatasetChangeListener,
         }
         return result;
     }
-    
+
     /**
      * Calculates the interval width for a given series.
-     *  
+     *
      * @param series  the series index.
-     * 
+     *
      * @return The interval width.
      */
     private double calculateIntervalForSeries(int series) {
@@ -421,41 +421,41 @@ public class IntervalXYDelegate implements DatasetChangeListener,
         }
         return result;
     }
-    
+
     /**
      * Tests the delegate for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
         if (obj == this) {
-            return true;   
+            return true;
         }
         if (!(obj instanceof IntervalXYDelegate)) {
-            return false;   
+            return false;
         }
         IntervalXYDelegate that = (IntervalXYDelegate) obj;
         if (this.autoWidth != that.autoWidth) {
-            return false;   
+            return false;
         }
         if (this.intervalPositionFactor != that.intervalPositionFactor) {
-            return false;   
+            return false;
         }
         if (this.fixedIntervalWidth != that.fixedIntervalWidth) {
-            return false;   
+            return false;
         }
         return true;
     }
-    
+
     /**
      * @return A clone of this delegate.
-     * 
+     *
      * @throws CloneNotSupportedException if the object cannot be cloned.
      */
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-    
+
 }
--- source/org/jfree/data/xy/Vector.java
+++ source/org/jfree/data/xy/Vector.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------
  * Vector.java
  * -----------
- * (C) Copyright 2007, by Object Refinery Limited.
+ * (C) Copyright 2007, 2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,7 +35,7 @@
  * Changes
  * -------
  * 30-Jan-2007 : Version 1 (DG);
- * 24-May-2007 : Added getLength() and getAngle() methods, thanks to 
+ * 24-May-2007 : Added getLength() and getAngle() methods, thanks to
  *               matinh (DG);
  * 25-May-2007 : Moved from experimental to the main source tree (DG);
  *
@@ -47,58 +47,58 @@ import java.io.Serializable;
 
 /**
  * A vector.
- * 
+ *
  * @since 1.0.6
  */
 public class Vector implements Serializable {
-    
+
     /** The vector x. */
     private double x;
-    
+
     /** The vector y. */
     private double y;
-    
-    /** 
+
+    /**
      * Creates a new instance of <code>Vector</code>.
      *
      * @param x  the x-component.
-     * @param y  the y-component.  
+     * @param y  the y-component.
      */
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    
+
     /**
      * Returns the x-value.
-     * 
+     *
      * @return The x-value.
      */
     public double getX() {
         return this.x;
     }
-    
+
     /**
      * Returns the y-value.
-     * 
+     *
      * @return The y-value.
      */
     public double getY() {
         return this.y;
     }
-    
+
     /**
      * Returns the length of the vector.
-     * 
+     *
      * @return The vector length.
      */
     public double getLength() {
         return Math.sqrt((this.x * this.x) + (this.y * this.y));
     }
- 
+
     /**
      * Returns the angle of the vector.
-     * 
+     *
      * @return The angle of the vector.
      */
     public double getAngle() {
@@ -107,9 +107,9 @@ public class Vector implements Serializable {
 
     /**
      * Tests this vector for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> not permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
@@ -131,7 +131,7 @@ public class Vector implements Serializable {
 
     /**
      * Returns a hash code for this instance.
-     * 
+     *
      * @return A hash code.
      */
     public int hashCode() {
@@ -142,5 +142,5 @@ public class Vector implements Serializable {
         result = 37 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
-    
+
 }
