70d7438ac85cbfc92271063d493d0370416a097e
David Gilbert <jfree@users.noreply.github.com>
2008-07-22 08:13:24 +0000

    Whitespace.

--- source/org/jfree/data/io/CSV.java
+++ source/org/jfree/data/io/CSV.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * --------
  * CSV.java
  * --------
- * (C) Copyright 2003-2007, by Object Refinery Limited.
+ * (C) Copyright 2003-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -49,50 +49,50 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
- * A utility class for reading {@link CategoryDataset} data from a CSV file.  
- * This initial version is very basic, and won't handle errors in the data 
+ * A utility class for reading {@link CategoryDataset} data from a CSV file.
+ * This initial version is very basic, and won't handle errors in the data
  * file very gracefully.
  */
 public class CSV {
 
     /** The field delimiter. */
     private char fieldDelimiter;
-    
+
     /** The text delimiter. */
     private char textDelimiter;
-     
-    /** 
-     * Creates a new CSV reader where the field delimiter is a comma, and the 
+
+    /**
+     * Creates a new CSV reader where the field delimiter is a comma, and the
      * text delimiter is a double-quote.
      */
     public CSV() {
-        this(',', '"');    
+        this(',', '"');
     }
-    
+
     /**
      * Creates a new reader with the specified field and text delimiters.
-     * 
+     *
      * @param fieldDelimiter  the field delimiter (usually a comma, semi-colon,
      *                        colon, tab or space).
-     * @param textDelimiter  the text delimiter (usually a single or double 
+     * @param textDelimiter  the text delimiter (usually a single or double
      *                       quote).
      */
     public CSV(char fieldDelimiter, char textDelimiter) {
         this.fieldDelimiter = fieldDelimiter;
         this.textDelimiter = textDelimiter;
     }
-    
+
     /**
      * Reads a {@link CategoryDataset} from a CSV file or input source.
-     * 
+     *
      * @param in  the input source.
-     * 
+     *
      * @return A category dataset.
-     * 
+     *
      * @throws IOException if there is an I/O problem.
      */
     public CategoryDataset readCategoryDataset(Reader in) throws IOException {
-        
+
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         BufferedReader reader = new BufferedReader(in);
         List columnKeys = null;
@@ -108,15 +108,15 @@ public class CSV {
             line = reader.readLine();
             lineIndex++;
         }
-        return dataset;     
-         
+        return dataset;
+
     }
-    
+
     /**
      * Extracts the column keys from a string.
-     * 
+     *
      * @param line  a line from the input file.
-     * 
+     *
      * @return A list of column keys.
      */
     private List extractColumnKeys(String line) {
@@ -125,7 +125,7 @@ public class CSV {
         int start = 0;
         for (int i = 0; i < line.length(); i++) {
             if (line.charAt(i) == this.fieldDelimiter) {
-                if (fieldIndex > 0) {  // first field is ignored, since 
+                if (fieldIndex > 0) {  // first field is ignored, since
                                        // column 0 is for row keys
                     String key = line.substring(start, i);
                     keys.add(removeStringDelimiters(key));
@@ -136,12 +136,12 @@ public class CSV {
         }
         String key = line.substring(start, line.length());
         keys.add(removeStringDelimiters(key));
-        return keys;        
+        return keys;
     }
-    
+
     /**
      * Extracts the row key and data for a single line from the input source.
-     * 
+     *
      * @param line  the line from the input source.
      * @param dataset  the dataset to be populated.
      * @param columnKeys  the column keys.
@@ -163,7 +163,7 @@ public class CSV {
                         removeStringDelimiters(line.substring(start, i))
                     );
                     dataset.addValue(
-                        value, rowKey, 
+                        value, rowKey,
                         (Comparable) columnKeys.get(fieldIndex - 1)
                     );
                 }
@@ -176,15 +176,15 @@ public class CSV {
         );
         dataset.addValue(
             value, rowKey, (Comparable) columnKeys.get(fieldIndex - 1)
-        ); 
+        );
     }
-    
+
     /**
-     * Removes the string delimiters from a key (as well as any white space 
+     * Removes the string delimiters from a key (as well as any white space
      * outside the delimiters).
-     * 
+     *
      * @param key  the key (including delimiters).
-     * 
+     *
      * @return The key without delimiters.
      */
     private String removeStringDelimiters(String key) {
@@ -197,5 +197,5 @@ public class CSV {
         }
         return k;
     }
-    
+
 }
