6f308acb183ecbabcb51d6b9707a85e3bc1dc902
David Gilbert <jfree@users.noreply.github.com>
2008-07-22 08:15:34 +0000

    Whitespace.

--- source/org/jfree/data/general/AbstractSeriesDataset.java
+++ source/org/jfree/data/general/AbstractSeriesDataset.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * --------------------------
  * AbstractSeriesDataset.java
  * --------------------------
- * (C) Copyright 2001-2007, by Object Refinery Limited.
+ * (C) Copyright 2001-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -47,17 +47,15 @@ package org.jfree.data.general;
 import java.io.Serializable;
 
 /**
- * An abstract implementation of the {@link SeriesDataset} interface, 
+ * An abstract implementation of the {@link SeriesDataset} interface,
  * containing a mechanism for registering change listeners.
  */
 public abstract class AbstractSeriesDataset extends AbstractDataset
-                                            implements SeriesDataset,
-                                                       SeriesChangeListener,
-                                                       Serializable {
+        implements SeriesDataset, SeriesChangeListener, Serializable {
 
     /** For serialization. */
     private static final long serialVersionUID = -6074996219705033171L;
-    
+
     /**
      * Creates a new dataset.
      */
@@ -73,24 +71,24 @@ public abstract class AbstractSeriesDataset extends AbstractDataset
     public abstract int getSeriesCount();
 
     /**
-     * Returns the key for a series.  
+     * Returns the key for a series.
      * <p>
-     * If <code>series</code> is not within the specified range, the 
-     * implementing method should throw an {@link IndexOutOfBoundsException} 
+     * If <code>series</code> is not within the specified range, the
+     * implementing method should throw an {@link IndexOutOfBoundsException}
      * (preferred) or an {@link IllegalArgumentException}.
      *
-     * @param series  the series index (in the range <code>0</code> to 
+     * @param series  the series index (in the range <code>0</code> to
      *     <code>getSeriesCount() - 1</code>).
      *
      * @return The series key.
      */
     public abstract Comparable getSeriesKey(int series);
-    
+
     /**
      * Returns the index of the named series, or -1.
-     * 
+     *
      * @param seriesKey  the series key (<code>null</code> permitted).
-     * 
+     *
      * @return The index.
      */
     public int indexOf(Comparable seriesKey) {
@@ -102,7 +100,7 @@ public abstract class AbstractSeriesDataset extends AbstractDataset
         }
         return -1;
     }
-    
+
     /**
      * Called when a series belonging to the dataset changes.
      *
