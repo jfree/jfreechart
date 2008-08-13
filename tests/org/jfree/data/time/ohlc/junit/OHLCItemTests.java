fb6d00b26dd4e86e0f5c3c8732315c9635963151
David Gilbert <jfree@users.noreply.github.com>
2008-08-13 19:23:53 +0000

    Whitespace.

--- tests/org/jfree/data/time/ohlc/junit/OHLCItemTests.java
+++ tests/org/jfree/data/time/ohlc/junit/OHLCItemTests.java
@@ -2,26 +2,26 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------
@@ -58,7 +58,7 @@ import org.jfree.data.time.ohlc.OHLCItem;
  * Tests for the {@link OHLCItem} class.
  */
 public class OHLCItemTests extends TestCase {
-    
+
     /**
      * Returns the tests as a test suite.
      *
@@ -78,7 +78,7 @@ public class OHLCItemTests extends TestCase {
     }
 
     private static final double EPSILON = 0.00000000001;
-    
+
     /**
      * Some checks for the constructor.
      */
@@ -90,7 +90,7 @@ public class OHLCItemTests extends TestCase {
         assertEquals(1.0, item1.getLowValue(), EPSILON);
         assertEquals(3.0, item1.getCloseValue(), EPSILON);
     }
-    
+
     /**
      * Confirm that the equals method can distinguish all the required fields.
      */
@@ -105,7 +105,7 @@ public class OHLCItemTests extends TestCase {
         assertFalse(item1.equals(item2));
         item2 = new OHLCItem(new Year(2007), 2.0, 4.0, 1.0, 3.0);
         assertTrue(item1.equals(item2));
-        
+
         // open
         item1 = new OHLCItem(new Year(2007), 2.2, 4.0, 1.0, 3.0);
         assertFalse(item1.equals(item2));
