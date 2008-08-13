964b035fad4a53e2e371ab724b3be2a78934a82d
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-08-13 19:23:53 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/data/time/ohlc/junit/OHLCTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/data/time/ohlc/junit/OHLCTests.java
@@ -2,26 +2,26 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * --------------
@@ -83,7 +83,7 @@ public class OHLCTests extends TestCase {
         OHLC i1 = new OHLC(2.0, 4.0, 1.0, 3.0);
         OHLC i2 = new OHLC(2.0, 4.0, 1.0, 3.0);
         assertEquals(i1, i2);
-        
+
         i1 = new OHLC(2.2, 4.0, 1.0, 3.0);
         assertFalse(i1.equals(i2));
         i2 = new OHLC(2.2, 4.0, 1.0, 3.0);
@@ -93,7 +93,7 @@ public class OHLCTests extends TestCase {
         assertFalse(i1.equals(i2));
         i2 = new OHLC(2.2, 4.4, 1.0, 3.0);
         assertTrue(i1.equals(i2));
-    
+
         i1 = new OHLC(2.2, 4.4, 1.1, 3.0);
         assertFalse(i1.equals(i2));
         i2 = new OHLC(2.2, 4.4, 1.1, 3.0);
@@ -119,7 +119,7 @@ public class OHLCTests extends TestCase {
     public void testSerialization() {
         OHLC i1 = new OHLC(2.0, 4.0, 1.0, 3.0);
         OHLC i2 = null;
-        
+
         try {
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
             ObjectOutput out = new ObjectOutputStream(buffer);
@@ -136,5 +136,5 @@ public class OHLCTests extends TestCase {
         }
         assertEquals(i1, i2);
     }
-    
+
 }
