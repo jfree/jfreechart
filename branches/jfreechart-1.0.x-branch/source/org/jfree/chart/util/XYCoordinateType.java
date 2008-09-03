b90f64b53c1de5e41c28c1cc20a63a523b626c52
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-03 09:32:38 +0000

    2008-09-03  David Gilbert  <david.gilbert@object-refinery.com>
        * experimental/org/jfree/experimental/chart/annotations/XYTitleAnnotation.java:
        Moved to main,
        * experimental/org/jfree/experimental/chart/annotations/junit/XYTitleAnnotationTests.java
        Likewise,
        * experimental/org/jfree/experimental/chart/util/XYCoordinateType.java
        Likewise,
        * source/org/jfree/chart/annotation/XYTitleAnnotation.java:
        Transferred from experimental,
        * source/org/jfree/chart/util/XYCoordinateType.java: Likewise,
        * tests/org/jfree/chart/annotations/junit/AnnotationsPackageTests.java
        (suite): Added tests for XYTitleAnnotation,
        * tests/org/jfree/chart/annotations/junit/XYTitleAnnotationTests.java:
        New file.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/util/XYCoordinateType.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/util/XYCoordinateType.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------------
  * XYCoordinateType.java
  * ---------------------
- * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2007, 2008 by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,35 +35,38 @@
  * Changes
  * -------
  * 02-Feb-2007 : Version 1 (DG);
- * 
+ * 03-Sep-2008 : Moved from experimental to main (DG);
+ *
  */
 
-package org.jfree.experimental.chart.util;
+package org.jfree.chart.util;
 
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 /**
  * Represents several possible interpretations for an (x, y) coordinate.
+ *
+ * @since 1.0.11
  */
 public final class XYCoordinateType implements Serializable {
-    
+
     /** The (x, y) coordinates represent a point in the data space. */
-    public static final XYCoordinateType DATA 
+    public static final XYCoordinateType DATA
             = new XYCoordinateType("XYCoordinateType.DATA");
 
-    /** 
-     * The (x, y) coordinates represent a relative position in the data space. 
+    /**
+     * The (x, y) coordinates represent a relative position in the data space.
      * In this case, the values should be in the range (0.0 to 1.0).
      */
-    public static final XYCoordinateType RELATIVE 
+    public static final XYCoordinateType RELATIVE
             = new XYCoordinateType("XYCoordinateType.RELATIVE");
 
-    /** 
-     * The (x, y) coordinates represent indices in a dataset. 
+    /**
+     * The (x, y) coordinates represent indices in a dataset.
      * In this case, the values should be in the range (0.0 to 1.0).
      */
-    public static final XYCoordinateType INDEX 
+    public static final XYCoordinateType INDEX
             = new XYCoordinateType("XYCoordinateType.INDEX");
 
     /** The name. */
@@ -88,7 +91,7 @@ public final class XYCoordinateType implements Serializable {
     }
 
     /**
-     * Returns <code>true</code> if this object is equal to the specified 
+     * Returns <code>true</code> if this object is equal to the specified
      * object, and <code>false</code> otherwise.
      *
      * @param obj  the other object.
@@ -108,7 +111,7 @@ public final class XYCoordinateType implements Serializable {
         }
         return true;
     }
-   
+
     /**
      * Ensures that serialization returns the unique instances.
      *
@@ -122,10 +125,10 @@ public final class XYCoordinateType implements Serializable {
         }
         else if (this.equals(XYCoordinateType.RELATIVE)) {
             return XYCoordinateType.RELATIVE;
-        }     
+        }
         else if (this.equals(XYCoordinateType.INDEX)) {
             return XYCoordinateType.INDEX;
-        }     
+        }
         return null;
     }
 
