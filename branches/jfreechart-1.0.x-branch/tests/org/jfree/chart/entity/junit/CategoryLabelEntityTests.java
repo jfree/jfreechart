53b36cfbed5f10b6213b7c7416c9a19b349c29ca
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-03 16:07:53 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/entity/junit/CategoryLabelEntityTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/entity/junit/CategoryLabelEntityTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------------
  * CategoryLabelEntityTests.java
  * -----------------------------
- * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2007, 2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -81,41 +81,41 @@ public class CategoryLabelEntityTests extends TestCase {
      * Confirm that the equals method can distinguish all the required fields.
      */
     public void testEquals() {
-        CategoryLabelEntity e1 = new CategoryLabelEntity("A", 
-                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL"); 
-        CategoryLabelEntity e2 = new CategoryLabelEntity("A", 
-                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL"); 
-        assertTrue(e1.equals(e2));  
-        
-        e1 = new CategoryLabelEntity("B", new Rectangle2D.Double(1.0, 2.0, 
-                3.0, 4.0), "ToolTip", "URL"); 
+        CategoryLabelEntity e1 = new CategoryLabelEntity("A",
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL");
+        CategoryLabelEntity e2 = new CategoryLabelEntity("A",
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL");
+        assertTrue(e1.equals(e2));
+
+        e1 = new CategoryLabelEntity("B", new Rectangle2D.Double(1.0, 2.0,
+                3.0, 4.0), "ToolTip", "URL");
         assertFalse(e1.equals(e2));
-        e2 = new CategoryLabelEntity("B", new Rectangle2D.Double(1.0, 2.0, 
-                3.0, 4.0), "ToolTip", "URL"); 
-        assertTrue(e1.equals(e2));  
-        
+        e2 = new CategoryLabelEntity("B", new Rectangle2D.Double(1.0, 2.0,
+                3.0, 4.0), "ToolTip", "URL");
+        assertTrue(e1.equals(e2));
+
         e1.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
         assertFalse(e1.equals(e2));
         e2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
 
         e1.setToolTipText("New ToolTip");
         assertFalse(e1.equals(e2));
         e2.setToolTipText("New ToolTip");
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
 
         e1.setURLText("New URL");
         assertFalse(e1.equals(e2));
         e2.setURLText("New URL");
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
     }
 
     /**
      * Confirm that cloning works.
      */
     public void testCloning() {
-        CategoryLabelEntity e1 = new CategoryLabelEntity("A", 
-                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL"); 
+        CategoryLabelEntity e1 = new CategoryLabelEntity("A",
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL");
         CategoryLabelEntity e2 = null;
         try {
             e2 = (CategoryLabelEntity) e1.clone();
@@ -132,8 +132,8 @@ public class CategoryLabelEntityTests extends TestCase {
      * Serialize an instance, restore it, and check for equality.
      */
     public void testSerialization() {
-        CategoryLabelEntity e1 = new CategoryLabelEntity("A", 
-                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL"); 
+        CategoryLabelEntity e1 = new CategoryLabelEntity("A",
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL");
         CategoryLabelEntity e2 = null;
         try {
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/entity/junit/XYItemEntityTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/entity/junit/XYItemEntityTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ----------------------
  * XYItemEntityTests.java
  * ----------------------
- * (C) Copyright 2004, 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2004-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -85,38 +85,38 @@ public class XYItemEntityTests extends TestCase {
         XYItemEntity e1 = new XYItemEntity(
             new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0),
             new TimeSeriesCollection(), 1, 9, "ToolTip", "URL"
-        ); 
+        );
         XYItemEntity e2 = new XYItemEntity(
             new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0),
             new TimeSeriesCollection(), 1, 9, "ToolTip", "URL"
-        ); 
-        assertTrue(e1.equals(e2));  
-        
+        );
+        assertTrue(e1.equals(e2));
+
         e1.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
         assertFalse(e1.equals(e2));
         e2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
 
         e1.setToolTipText("New ToolTip");
         assertFalse(e1.equals(e2));
         e2.setToolTipText("New ToolTip");
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
 
         e1.setURLText("New URL");
         assertFalse(e1.equals(e2));
         e2.setURLText("New URL");
-        assertTrue(e1.equals(e2));  
-        
+        assertTrue(e1.equals(e2));
+
         e1.setSeriesIndex(88);
         assertFalse(e1.equals(e2));
         e2.setSeriesIndex(88);
-        assertTrue(e1.equals(e2)); 
-        
+        assertTrue(e1.equals(e2));
+
         e1.setItem(88);
         assertFalse(e1.equals(e2));
         e2.setItem(88);
-        assertTrue(e1.equals(e2)); 
-        
+        assertTrue(e1.equals(e2));
+
     }
 
     /**
@@ -126,9 +126,9 @@ public class XYItemEntityTests extends TestCase {
         XYItemEntity e1 = new XYItemEntity(
             new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0),
             new TimeSeriesCollection(), 1, 9, "ToolTip", "URL"
-        ); 
+        );
         XYItemEntity e2 = null;
-        
+
         try {
             e2 = (XYItemEntity) e1.clone();
         }
@@ -147,7 +147,7 @@ public class XYItemEntityTests extends TestCase {
         XYItemEntity e1 = new XYItemEntity(
             new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0),
             new TimeSeriesCollection(), 1, 9, "ToolTip", "URL"
-        ); 
+        );
         XYItemEntity e2 = null;
         try {
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
