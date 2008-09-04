3b396c91ff5503d546fa35fef122f82dcdc8c7f6
David Gilbert <jfree@users.noreply.github.com>
2008-09-04 20:07:26 +0000

    Whitespace.

--- tests/org/jfree/chart/junit/BarChartTests.java
+++ tests/org/jfree/chart/junit/BarChartTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------
  * BarChartTests.java
  * ------------------
- * (C) Copyright 2002-2007, by Object Refinery Limited.
+ * (C) Copyright 2002-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -101,7 +101,7 @@ public class BarChartTests extends TestCase {
     }
 
     /**
-     * Draws the chart with a null info object to make sure that no exceptions 
+     * Draws the chart with a null info object to make sure that no exceptions
      * are thrown (a problem that was occurring at one point).
      */
     public void testDrawWithNullInfo() {
@@ -109,10 +109,10 @@ public class BarChartTests extends TestCase {
         boolean success = false;
 
         try {
-            BufferedImage image = new BufferedImage(200 , 100, 
+            BufferedImage image = new BufferedImage(200 , 100,
                     BufferedImage.TYPE_INT_RGB);
             Graphics2D g2 = image.createGraphics();
-            this.chart.draw(g2, new Rectangle2D.Double(0, 0, 200, 100), null, 
+            this.chart.draw(g2, new Rectangle2D.Double(0, 0, 200, 100), null,
                     null);
             g2.dispose();
             success = true;
@@ -136,7 +136,7 @@ public class BarChartTests extends TestCase {
              {new Integer(-10), new Integer(10)},
              {new Integer(20), new Integer(30)}};
 
-        CategoryDataset newData = DatasetUtilities.createCategoryDataset("S", 
+        CategoryDataset newData = DatasetUtilities.createCategoryDataset("S",
                 "C", data);
 
         LocalListener l = new LocalListener();
@@ -166,7 +166,7 @@ public class BarChartTests extends TestCase {
         CategoryToolTipGenerator tt2 = renderer.getToolTipGenerator(0, 0);
         assertTrue(tt2 == tt);
     }
-    
+
     /**
      * Check that setting a URL generator for a series does override the
      * default generator.
@@ -180,7 +180,7 @@ public class BarChartTests extends TestCase {
         CategoryURLGenerator url2 = renderer.getItemURLGenerator(0, 0);
         assertTrue(url2 == url1);
     }
-    
+
     /**
      * Create a bar chart with sample data in the range -3 to +3.
      *
@@ -194,7 +194,7 @@ public class BarChartTests extends TestCase {
              {new Integer(-1), new Integer(1)},
              {new Integer(2), new Integer(3)}};
 
-        CategoryDataset dataset = DatasetUtilities.createCategoryDataset("S", 
+        CategoryDataset dataset = DatasetUtilities.createCategoryDataset("S",
                 "C", data);
 
         // create the chart...
--- tests/org/jfree/chart/junit/ChartRenderingInfoTests.java
+++ tests/org/jfree/chart/junit/ChartRenderingInfoTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ----------------------------
  * ChartRenderingInfoTests.java
  * ----------------------------
- * (C) Copyright 2004-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2004-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -36,7 +36,7 @@
  * -------
  * 19-Mar-2004 : Version 1 (DG);
  * 30-Nov-2005 : Updated for removed field in ChartRenderingInfo (DG);
- * 
+ *
  */
 
 package org.jfree.chart.junit;
@@ -88,24 +88,24 @@ public class ChartRenderingInfoTests extends TestCase {
         ChartRenderingInfo i1 = new ChartRenderingInfo();
         ChartRenderingInfo i2 = new ChartRenderingInfo();
         assertTrue(i1.equals(i2));
-        
+
         i1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
         assertFalse(i1.equals(i2));
         i2.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
         assertTrue(i1.equals(i2));
-        
+
         i1.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
         assertFalse(i1.equals(i2));
         i2.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
         assertTrue(i1.equals(i2));
-        
+
         StandardEntityCollection e1 = new StandardEntityCollection();
         e1.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
         i1.setEntityCollection(e1);
         assertFalse(i1.equals(i2));
         StandardEntityCollection e2 = new StandardEntityCollection();
         e2.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
-        i2.setEntityCollection(e2); 
+        i2.setEntityCollection(e2);
     }
 
     /**
@@ -123,20 +123,20 @@ public class ChartRenderingInfoTests extends TestCase {
         assertTrue(i1 != i2);
         assertTrue(i1.getClass() == i2.getClass());
         assertTrue(i1.equals(i2));
-        
+
         // check independence
         i1.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
         assertFalse(i1.equals(i2));
         i2.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
         assertTrue(i1.equals(i2));
-        
-        i1.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2, 
+
+        i1.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2,
                 1)));
         assertFalse(i1.equals(i2));
-        i2.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2, 
+        i2.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2,
                 1)));
         assertTrue(i1.equals(i2));
-         
+
     }
 
     /**
@@ -171,7 +171,7 @@ public class ChartRenderingInfoTests extends TestCase {
     public void testSerialization2() {
 
         ChartRenderingInfo i1 = new ChartRenderingInfo();
-        i1.getPlotInfo().setDataArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 
+        i1.getPlotInfo().setDataArea(new Rectangle2D.Double(1.0, 2.0, 3.0,
                 4.0));
         ChartRenderingInfo i2 = null;
         try {
