8174bffd852dcd6e30421da909c06807f775455e
David Gilbert <jfree@users.noreply.github.com>
2008-09-04 20:10:12 +0000

    Whitespace.

--- source/org/jfree/chart/labels/StandardCategoryItemLabelGenerator.java
+++ source/org/jfree/chart/labels/StandardCategoryItemLabelGenerator.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
- * 
+ *
  * ---------------------------------------
  * StandardCategoryItemLabelGenerator.java
  * ---------------------------------------
- * (C) Copyright 2004-2007, by Object Refinery Limited.
+ * (C) Copyright 2004-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,7 +35,7 @@
  * Changes
  * -------
  * 11-May-2004 : Version 1 (DG);
- * 20-Apr-2005 : Renamed StandardCategoryLabelGenerator 
+ * 20-Apr-2005 : Renamed StandardCategoryLabelGenerator
  *               --> StandardCategoryItemLabelGenerator (DG);
  * ------------- JFREECHART 1.0.0 ---------------------------------------------
  * 03-May-2005 : Added equals() implementation, to fix bug 1481087 (DG);
@@ -51,20 +51,20 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.util.PublicCloneable;
 
 /**
- * A standard label generator that can be used with a 
+ * A standard label generator that can be used with a
  * {@link org.jfree.chart.renderer.category.CategoryItemRenderer}.
  */
-public class StandardCategoryItemLabelGenerator 
-    extends AbstractCategoryItemLabelGenerator 
+public class StandardCategoryItemLabelGenerator
+    extends AbstractCategoryItemLabelGenerator
     implements CategoryItemLabelGenerator, Cloneable, PublicCloneable,
                Serializable {
 
     /** For serialization. */
     private static final long serialVersionUID = 3499701401211412882L;
-    
+
     /** The default format string. */
     public static final String DEFAULT_LABEL_FORMAT_STRING = "{2}";
-    
+
     /**
      * Creates a new generator with a default number formatter.
      */
@@ -75,46 +75,46 @@ public class StandardCategoryItemLabelGenerator
     /**
      * Creates a new generator with the specified number formatter.
      *
-     * @param labelFormat  the label format string (<code>null</code> not 
+     * @param labelFormat  the label format string (<code>null</code> not
      *                     permitted).
      * @param formatter  the number formatter (<code>null</code> not permitted).
      */
-    public StandardCategoryItemLabelGenerator(String labelFormat, 
+    public StandardCategoryItemLabelGenerator(String labelFormat,
                                               NumberFormat formatter) {
         super(labelFormat, formatter);
     }
-    
+
     /**
      * Creates a new generator with the specified number formatter.
      *
-     * @param labelFormat  the label format string (<code>null</code> not 
+     * @param labelFormat  the label format string (<code>null</code> not
      *                     permitted).
      * @param formatter  the number formatter (<code>null</code> not permitted).
      * @param percentFormatter  the percent formatter (<code>null</code> not
      *     permitted).
-     * 
+     *
      * @since 1.0.2
      */
-    public StandardCategoryItemLabelGenerator(String labelFormat, 
+    public StandardCategoryItemLabelGenerator(String labelFormat,
             NumberFormat formatter, NumberFormat percentFormatter) {
         super(labelFormat, formatter, percentFormatter);
     }
-    
+
     /**
      * Creates a new generator with the specified date formatter.
      *
-     * @param labelFormat  the label format string (<code>null</code> not 
+     * @param labelFormat  the label format string (<code>null</code> not
      *                     permitted).
      * @param formatter  the date formatter (<code>null</code> not permitted).
      */
-    public StandardCategoryItemLabelGenerator(String labelFormat, 
+    public StandardCategoryItemLabelGenerator(String labelFormat,
                                               DateFormat formatter) {
         super(labelFormat, formatter);
     }
-    
+
     /**
-     * Generates the label for an item in a dataset.  Note: in the current 
-     * dataset implementation, each row is a series, and each column contains 
+     * Generates the label for an item in a dataset.  Note: in the current
+     * dataset implementation, each row is a series, and each column contains
      * values for a particular category.
      *
      * @param dataset  the dataset (<code>null</code> not permitted).
@@ -126,13 +126,13 @@ public class StandardCategoryItemLabelGenerator
     public String generateLabel(CategoryDataset dataset, int row, int column) {
         return generateLabelString(dataset, row, column);
     }
-    
+
     /**
      * Tests this generator for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object (<code>null</code> permitted).
-     * 
-     * @return <code>true</code> if this generator is equal to 
+     *
+     * @return <code>true</code> if this generator is equal to
      *     <code>obj</code>, and <code>false</code> otherwise.
      */
     public boolean equals(Object obj) {
--- source/org/jfree/chart/labels/StandardCategorySeriesLabelGenerator.java
+++ source/org/jfree/chart/labels/StandardCategorySeriesLabelGenerator.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------------------------
@@ -51,32 +51,32 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.util.PublicCloneable;
 
 /**
- * A standard series label generator for plots that use data from 
+ * A standard series label generator for plots that use data from
  * a {@link org.jfree.data.category.CategoryDataset}.
  */
-public class StandardCategorySeriesLabelGenerator implements 
+public class StandardCategorySeriesLabelGenerator implements
     CategorySeriesLabelGenerator, Cloneable, PublicCloneable, Serializable {
 
     /** For serialization. */
     private static final long serialVersionUID = 4630760091523940820L;
-    
+
     /** The default item label format. */
     public static final String DEFAULT_LABEL_FORMAT = "{0}";
-    
+
     /** The format pattern. */
     private String formatPattern;
 
     /**
-     * Creates a default series label generator (uses 
+     * Creates a default series label generator (uses
      * {@link #DEFAULT_LABEL_FORMAT}).
      */
     public StandardCategorySeriesLabelGenerator() {
         this(DEFAULT_LABEL_FORMAT);
     }
-    
+
     /**
      * Creates a new series label generator.
-     * 
+     *
      * @param format  the format pattern (<code>null</code> not permitted).
      */
     public StandardCategorySeriesLabelGenerator(String format) {
@@ -88,23 +88,23 @@ public class StandardCategorySeriesLabelGenerator implements
 
     /**
      * Generates a label for the specified series.
-     * 
+     *
      * @param dataset  the dataset (<code>null</code> not permitted).
      * @param series  the series.
-     * 
+     *
      * @return A series label.
      */
     public String generateLabel(CategoryDataset dataset, int series) {
         if (dataset == null) {
             throw new IllegalArgumentException("Null 'dataset' argument.");
         }
-        String label = MessageFormat.format(this.formatPattern, 
+        String label = MessageFormat.format(this.formatPattern,
                 createItemArray(dataset, series));
         return label;
     }
 
     /**
-     * Creates the array of items that can be passed to the 
+     * Creates the array of items that can be passed to the
      * {@link MessageFormat} class for creating labels.
      *
      * @param dataset  the dataset (<code>null</code> not permitted).
@@ -120,15 +120,15 @@ public class StandardCategorySeriesLabelGenerator implements
 
     /**
      * Returns an independent copy of the generator.
-     * 
+     *
      * @return A clone.
-     * 
+     *
      * @throws CloneNotSupportedException if cloning is not supported.
      */
-    public Object clone() throws CloneNotSupportedException { 
+    public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-    
+
     /**
      * Tests this object for equality with an arbitrary object.
      *
@@ -143,17 +143,17 @@ public class StandardCategorySeriesLabelGenerator implements
         if (!(obj instanceof StandardCategorySeriesLabelGenerator)) {
             return false;
         }
-        StandardCategorySeriesLabelGenerator that 
+        StandardCategorySeriesLabelGenerator that
                 = (StandardCategorySeriesLabelGenerator) obj;
         if (!this.formatPattern.equals(that.formatPattern)) {
             return false;
         }
         return true;
     }
-    
+
     /**
      * Returns a hash code for this instance.
-     * 
+     *
      * @return A hash code.
      */
     public int hashCode() {
