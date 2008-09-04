eb1bef63f902a3f7063d6c01b59f81bc7e341da4
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-04 19:46:18 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/DrawableLegendItem.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/DrawableLegendItem.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -----------------------
  * DrawableLegendItem.java
  * -----------------------
- * (C) Copyright 2002-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2002-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   Luke Quinane;
@@ -38,11 +38,11 @@
  * 07-Feb-2002 : Version 1 (DG);
  * 23-Sep-2002 : Renamed LegendItem --> DrawableLegendItem (DG);
  * 02-Oct-2002 : Fixed errors reported by Checkstyle (DG);
- * 08-Oct-2003 : Applied patch for displaying series line style, contributed by 
+ * 08-Oct-2003 : Applied patch for displaying series line style, contributed by
  *               Luke Quinane (DG);
  * 27-Mar-2004 : Added getMaxX() and getMaxY() methods (BN);
  * 27-Jan-2005 : Cleared out code that belongs in the LegendItem class (DG);
- * 
+ *
  */
 
 package org.jfree.chart;
@@ -52,16 +52,16 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 /**
- * This class contains a single legend item along with position details for 
+ * This class contains a single legend item along with position details for
  * drawing the item on a particular chart.
- * 
+ *
  * @deprecated This class is not used by JFreeChart.
  */
 public class DrawableLegendItem {
 
-    /** 
-     * The legend item (encapsulates information about the label, color and 
-     * shape). 
+    /**
+     * The legend item (encapsulates information about the label, color and
+     * shape).
      */
     private LegendItem item;
 
@@ -79,7 +79,7 @@ public class DrawableLegendItem {
 
     /** A shape used to indicate color on the legend. */
     private Shape marker;
-    
+
     /** A line used to indicate the series stroke on the legend */
     private Line2D line;
 
@@ -159,10 +159,10 @@ public class DrawableLegendItem {
     }
 
     /**
-     * Returns the largest X coordinate of the framing rectangle of this legend 
+     * Returns the largest X coordinate of the framing rectangle of this legend
      * item.
-     * 
-     * @return The largest x coordinate of the framing rectangle of this legend 
+     *
+     * @return The largest x coordinate of the framing rectangle of this legend
      *         item.
      */
     public double getMaxX() {
@@ -170,16 +170,16 @@ public class DrawableLegendItem {
     }
 
     /**
-     * Returns the largest Y coordinate of the framing rectangle of this legend 
+     * Returns the largest Y coordinate of the framing rectangle of this legend
      * item.
-     * 
-     * @return The largest Y coordinate of the framing rectangle of this legend 
+     *
+     * @return The largest Y coordinate of the framing rectangle of this legend
      *         item.
      */
     public double getMaxY() {
         return getY() + getHeight();
     }
-    
+
     /**
      * Get the marker.
      *
@@ -192,13 +192,13 @@ public class DrawableLegendItem {
     /**
      * Set the marker.
      *
-     * @param marker  a shape used to indicate color on the legend for this 
+     * @param marker  a shape used to indicate color on the legend for this
      *                item.
      */
     public void setMarker(Shape marker) {
         this.marker = marker;
     }
-    
+
     /**
      * Sets the line used to label this series.
      *
@@ -210,7 +210,7 @@ public class DrawableLegendItem {
 
     /**
      * Returns the list.
-     * 
+     *
      * @return The line.
      */
     public Line2D getLine() {
