506c19ff43f87ba321bfb8e30159a843f2aa0f18
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-04 20:13:31 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/data/jdbc/JDBCCategoryDataset.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/data/jdbc/JDBCCategoryDataset.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ------------------------
  * JDBCCategoryDataset.java
  * ------------------------
- * (C) Copyright 2002-2007, by Bryan Scott and Contributors.
+ * (C) Copyright 2002-2008, by Bryan Scott and Contributors.
  *
  * Original Author:  Bryan Scott; Andy;
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -35,7 +35,7 @@
  *
  * Changes
  * -------
- * 26-Apr-2002 : Creation based on JdbcXYDataSet, using code contributed from 
+ * 26-Apr-2002 : Creation based on JdbcXYDataSet, using code contributed from
  *               Andy;
  * 13-Aug-2002 : Updated Javadocs, import statements and formatting (DG);
  * 03-Sep-2002 : Added fix for bug 591385 (DG);
@@ -44,21 +44,21 @@
  * 03-Feb-2003 : Added Types.DECIMAL (see bug report 677814) (DG);
  * 13-Jun-2003 : Added Types.TIME as suggest by Bryan Scott in the forum (DG);
  * 30-Jun-2003 : CVS Write test (BS);
- * 30-Jul-2003 : Added empty contructor and executeQuery(connection,string) 
+ * 30-Jul-2003 : Added empty contructor and executeQuery(connection,string)
  *               method (BS);
- * 29-Aug-2003 : Added a 'transpose' flag, so that data can be easily 
+ * 29-Aug-2003 : Added a 'transpose' flag, so that data can be easily
  *               transposed if required (DG);
  * 10-Sep-2003 : Added support for additional JDBC types (DG);
  * 24-Sep-2003 : Added clearing results from previous queries to executeQuery
  *               following being highlighted on online forum (BS);
- * 02-Dec-2003 : Throwing exceptions allows to handle errors, removed default 
- *               constructor, as without a connection, a query can never be 
+ * 02-Dec-2003 : Throwing exceptions allows to handle errors, removed default
+ *               constructor, as without a connection, a query can never be
  *               executed (TM);
  * 04-Dec-2003 : Added missing Javadocs (DG);
  * ------------- JFREECHART 1.0.0 ---------------------------------------------
- * 08-Mar-2006 : Fixed bug 1445748 where an exception is thrown if 
+ * 08-Mar-2006 : Fixed bug 1445748 where an exception is thrown if
  *               executeQuery() is called more than once (DG);
- * 
+ *
  */
 
 package org.jfree.data.jdbc;
@@ -77,30 +77,30 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  * A {@link CategoryDataset} implementation over a database JDBC result set.
- * The dataset is populated via a call to {@link #executeQuery(String)} with 
- * the string SQL query.  The SQL query must return at least two columns.  The 
+ * The dataset is populated via a call to {@link #executeQuery(String)} with
+ * the string SQL query.  The SQL query must return at least two columns.  The
  * first column will be the category name and remaining columns values (each
- * column represents a series).  Subsequent calls to 
+ * column represents a series).  Subsequent calls to
  * {@link #executeQuery(String)} will refresh the dataset.
  * <p>
  * The database connection is read-only and no write back facility exists.
  * <p>
- * NOTE: Many people have found this class too restrictive in general use.  
- * For the greatest flexibility, please consider writing your own code to read 
- * data from a <code>ResultSet</code> and populate a 
+ * NOTE: Many people have found this class too restrictive in general use.
+ * For the greatest flexibility, please consider writing your own code to read
+ * data from a <code>ResultSet</code> and populate a
  * {@link DefaultCategoryDataset} directly.
  */
 public class JDBCCategoryDataset extends DefaultCategoryDataset {
 
     /** For serialization. */
     static final long serialVersionUID = -3080395327918844965L;
-    
+
     /** The database connection. */
     private transient Connection connection;
 
     /**
-     * A flag the controls whether or not the table is transposed.  The default 
-     * is 'true' because this provides the behaviour described in the 
+     * A flag the controls whether or not the table is transposed.  The default
+     * is 'true' because this provides the behaviour described in the
      * documentation.
      */
     private boolean transpose = true;
@@ -113,9 +113,9 @@ public class JDBCCategoryDataset extends DefaultCategoryDataset {
      * @param  driverName  the database driver class name.
      * @param  user  the database user.
      * @param  passwd  the database user's password.
-     * 
+     *
      * @throws ClassNotFoundException if the driver cannot be found.
-     * @throws SQLException if there is an error obtaining a connection to the 
+     * @throws SQLException if there is an error obtaining a connection to the
      *                      database.
      */
     public JDBCCategoryDataset(String url,
@@ -141,22 +141,22 @@ public class JDBCCategoryDataset extends DefaultCategoryDataset {
     }
 
     /**
-     * Creates a new dataset with the given database connection, and executes 
+     * Creates a new dataset with the given database connection, and executes
      * the supplied query to populate the dataset.
      *
      * @param connection  the connection.
      * @param query  the query.
-     * 
+     *
      * @throws SQLException if there is a problem executing the query.
      */
-    public JDBCCategoryDataset(Connection connection, String query) 
+    public JDBCCategoryDataset(Connection connection, String query)
         throws SQLException {
         this(connection);
         executeQuery(query);
     }
 
     /**
-     * Returns a flag that controls whether or not the table values are 
+     * Returns a flag that controls whether or not the table values are
      * transposed when added to the dataset.
      *
      * @return A boolean.
@@ -176,15 +176,15 @@ public class JDBCCategoryDataset extends DefaultCategoryDataset {
     }
 
     /**
-     * Populates the dataset by executing the supplied query against the 
-     * existing database connection.  If no connection exists then no action 
+     * Populates the dataset by executing the supplied query against the
+     * existing database connection.  If no connection exists then no action
      * is taken.
      * <p>
-     * The results from the query are extracted and cached locally, thus 
+     * The results from the query are extracted and cached locally, thus
      * applying an upper limit on how many rows can be retrieved successfully.
      *
      * @param query  the query.
-     * 
+     *
      * @throws SQLException if there is a problem executing the query.
      */
     public void executeQuery(String query) throws SQLException {
@@ -192,16 +192,16 @@ public class JDBCCategoryDataset extends DefaultCategoryDataset {
     }
 
     /**
-     * Populates the dataset by executing the supplied query against the 
-     * existing database connection.  If no connection exists then no action 
+     * Populates the dataset by executing the supplied query against the
+     * existing database connection.  If no connection exists then no action
      * is taken.
      * <p>
-     * The results from the query are extracted and cached locally, thus 
+     * The results from the query are extracted and cached locally, thus
      * applying an upper limit on how many rows can be retrieved successfully.
      *
      * @param con  the connection.
      * @param query  the query.
-     * 
+     *
      * @throws SQLException if there is a problem executing the query.
      */
     public void executeQuery(Connection con, String query) throws SQLException {
@@ -270,7 +270,7 @@ public class JDBCCategoryDataset extends DefaultCategoryDataset {
                         case Types.CHAR:
                         case Types.VARCHAR:
                         case Types.LONGVARCHAR: {
-                            String string 
+                            String string
                                 = (String) resultSet.getObject(column);
                             try {
                                 Number value = Double.valueOf(string);
--- branches/jfreechart-1.0.x-branch/source/org/jfree/data/jdbc/JDBCPieDataset.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/data/jdbc/JDBCPieDataset.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -------------------
  * JDBCPieDataset.java
  * -------------------
- * (C) Copyright 2002-2007, by Bryan Scott and Contributors.
+ * (C) Copyright 2002-2008, by Bryan Scott and Contributors.
  *
  * Original Author:  Bryan Scott; Andy
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -35,24 +35,24 @@
  *
  * Changes
  * -------
- * 26-Apr-2002 : Creation based on JdbcXYDataSet, but extending 
+ * 26-Apr-2002 : Creation based on JdbcXYDataSet, but extending
  *               DefaultPieDataset (BS);
  * 24-Jun-2002 : Removed unnecessary import and local variable (DG);
- * 13-Aug-2002 : Updated Javadoc comments and imports, removed default 
+ * 13-Aug-2002 : Updated Javadoc comments and imports, removed default
  *               constructor (DG);
  * 18-Sep-2002 : Updated to support BIGINT (BS);
  * 21-Jan-2003 : Renamed JdbcPieDataset --> JDBCPieDataset (DG);
  * 03-Feb-2003 : Added Types.DECIMAL (see bug report 677814) (DG);
  * 05-Jun-2003 : Updated to support TIME, optimised executeQuery method (BS);
- * 30-Jul-2003 : Added empty contructor and executeQuery(connection,string) 
+ * 30-Jul-2003 : Added empty contructor and executeQuery(connection,string)
  *               method (BS);
- * 02-Dec-2003 : Throwing exceptions allows to handle errors, removed default 
- *               constructor, as without a connection, a query can never be 
+ * 02-Dec-2003 : Throwing exceptions allows to handle errors, removed default
+ *               constructor, as without a connection, a query can never be
  *               executed (TM);
  * 04-Dec-2003 : Added missing Javadocs (DG);
  * ------------- JFREECHART 1.0.x ---------------------------------------------
  * 02-Feb-2007 : Removed author tags all over JFreeChart sources (DG);
- * 
+ *
  */
 
 package org.jfree.data.jdbc;
@@ -72,8 +72,8 @@ import org.jfree.data.general.PieDataset;
 /**
  * A {@link PieDataset} that reads data from a database via JDBC.
  * <P>
- * A query should be supplied that returns data in two columns, the first 
- * containing VARCHAR data, and the second containing numerical data.  The 
+ * A query should be supplied that returns data in two columns, the first
+ * containing VARCHAR data, and the second containing numerical data.  The
  * data is cached in-memory and can be refreshed at any time.
  */
 public class JDBCPieDataset extends DefaultPieDataset {
@@ -91,9 +91,9 @@ public class JDBCPieDataset extends DefaultPieDataset {
      * @param driverName  the database driver class name.
      * @param user  the database user.
      * @param password  the database users password.
-     * 
+     *
      * @throws ClassNotFoundException if the driver cannot be found.
-     * @throws SQLException if there is a problem obtaining a database 
+     * @throws SQLException if there is a problem obtaining a database
      *                      connection.
      */
     public JDBCPieDataset(String url,
@@ -101,7 +101,7 @@ public class JDBCPieDataset extends DefaultPieDataset {
                           String user,
                           String password)
         throws SQLException, ClassNotFoundException {
-        
+
         Class.forName(driverName);
         this.connection = DriverManager.getConnection(url, user, password);
     }
@@ -128,7 +128,7 @@ public class JDBCPieDataset extends DefaultPieDataset {
      *
      * @param con  the database connection.
      * @param query  the database connection.
-     * 
+     *
      * @throws SQLException if there is a problem executing the query.
      */
     public JDBCPieDataset(Connection con, String query) throws SQLException {
@@ -144,7 +144,7 @@ public class JDBCPieDataset extends DefaultPieDataset {
      *  applying an upper limit on how many rows can be retrieved successfully.
      *
      * @param  query  the query to be executed.
-     * 
+     *
      * @throws SQLException if there is a problem executing the query.
      */
     public void executeQuery(String query) throws SQLException {
@@ -160,7 +160,7 @@ public class JDBCPieDataset extends DefaultPieDataset {
      *
      * @param  query  the query to be executed
      * @param  con  the connection the query is to be executed against
-     * 
+     *
      * @throws SQLException if there is a problem executing the query.
      */
     public void executeQuery(Connection con, String query) throws SQLException {
