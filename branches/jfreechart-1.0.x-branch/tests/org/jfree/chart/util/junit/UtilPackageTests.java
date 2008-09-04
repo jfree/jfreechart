a0c921437a537a2aae1e7b3508c4ee9c6f90e1de
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-04 19:54:46 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/util/junit/UtilPackageTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/util/junit/UtilPackageTests.java
@@ -6,22 +6,22 @@
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------------
@@ -72,10 +72,10 @@ public class UtilPackageTests extends TestCase {
     public UtilPackageTests(String name) {
         super(name);
     }
-    
+
     /**
      * Runs the test suite using JUnit's text-based runner.
-     * 
+     *
      * @param args  ignored.
      */
     public static void main(String[] args) {
