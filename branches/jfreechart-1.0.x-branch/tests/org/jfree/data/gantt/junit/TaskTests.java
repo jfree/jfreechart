ed9c49c915e55209bcef1ade08490ff33317ba4a
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-04 19:52:22 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/data/gantt/junit/TaskTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/data/gantt/junit/TaskTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * --------------
  * TaskTests.java
  * --------------
- * (C) Copyright 2004-2007, by Object Refinery Limited.
+ * (C) Copyright 2004-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -82,7 +82,7 @@ public class TaskTests extends TestCase {
      * Confirm that the equals method can distinguish all the required fields.
      */
     public void testEquals() {
-        
+
         Task t1 = new Task("T", new Date(1), new Date(2));
         Task t2 = new Task("T", new Date(1), new Date(2));
         assertTrue(t1.equals(t2));
@@ -92,22 +92,22 @@ public class TaskTests extends TestCase {
         assertFalse(t1.equals(t2));
         t2.setDescription("X");
         assertTrue(t1.equals(t2));
-        
+
         t1.setDuration(new SimpleTimePeriod(new Date(2), new Date(3)));
         assertFalse(t1.equals(t2));
         t2.setDuration(new SimpleTimePeriod(new Date(2), new Date(3)));
         assertTrue(t1.equals(t2));
-        
+
         t1.setPercentComplete(0.5);
         assertFalse(t1.equals(t2));
         t2.setPercentComplete(0.5);
         assertTrue(t1.equals(t2));
-        
+
         t1.addSubtask(new Task("T", new Date(22), new Date(33)));
         assertFalse(t1.equals(t2));
         t2.addSubtask(new Task("T", new Date(22), new Date(33)));
         assertTrue(t1.equals(t2));
-        
+
 
     }
 
@@ -154,7 +154,7 @@ public class TaskTests extends TestCase {
         assertEquals(t1, t2);
 
     }
-    
+
     /**
      * Check the getSubTaskCount() method.
      */
@@ -162,7 +162,7 @@ public class TaskTests extends TestCase {
         Task t1 = new Task("T", new Date(100), new Date(200));
         assertEquals(0, t1.getSubtaskCount());
         t1.addSubtask(new Task("S1", new Date(100), new Date(110)));
-        assertEquals(1, t1.getSubtaskCount());  
+        assertEquals(1, t1.getSubtaskCount());
         Task s2 = new Task("S2", new Date(111), new Date(120));
         t1.addSubtask(s2);
         assertEquals(2, t1.getSubtaskCount());
