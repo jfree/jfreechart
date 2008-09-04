aa3a7ac182bd0af9c53ef5bf479f553fa469f708
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-04 20:12:30 +0000

    Whitespace.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/entity/junit/CategoryItemEntityTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/entity/junit/CategoryItemEntityTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ----------------------------
  * CategoryItemEntityTests.java
  * ----------------------------
- * (C) Copyright 2004-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2004-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -88,41 +88,41 @@ public class CategoryItemEntityTests extends TestCase {
         d.addValue(3.0, "R2", "C1");
         d.addValue(4.0, "R2", "C2");
         CategoryItemEntity e1 = new CategoryItemEntity(new Rectangle2D.Double(
-                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1); 
+                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1);
         CategoryItemEntity e2 = new CategoryItemEntity(new Rectangle2D.Double(
-                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1); 
-        assertTrue(e1.equals(e2));  
-        
+                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1);
+        assertTrue(e1.equals(e2));
+
         e1.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
         assertFalse(e1.equals(e2));
         e2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
 
         e1.setToolTipText("New ToolTip");
         assertFalse(e1.equals(e2));
         e2.setToolTipText("New ToolTip");
-        assertTrue(e1.equals(e2));  
+        assertTrue(e1.equals(e2));
 
         e1.setURLText("New URL");
         assertFalse(e1.equals(e2));
         e2.setURLText("New URL");
-        assertTrue(e1.equals(e2));  
-        
+        assertTrue(e1.equals(e2));
+
         e1.setCategory("C1");
         assertFalse(e1.equals(e2));
         e2.setCategory("C1");
-        assertTrue(e1.equals(e2)); 
-        
+        assertTrue(e1.equals(e2));
+
         e1.setCategoryIndex(0);
         assertFalse(e1.equals(e2));
         e2.setCategoryIndex(0);
-        assertTrue(e1.equals(e2)); 
-        
+        assertTrue(e1.equals(e2));
+
         e1.setSeries(0);
         assertFalse(e1.equals(e2));
         e2.setSeries(0);
-        assertTrue(e1.equals(e2)); 
-        
+        assertTrue(e1.equals(e2));
+
     }
 
     /**
@@ -135,9 +135,9 @@ public class CategoryItemEntityTests extends TestCase {
         d.addValue(3.0, "R2", "C1");
         d.addValue(4.0, "R2", "C2");
         CategoryItemEntity e1 = new CategoryItemEntity(new Rectangle2D.Double(
-                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1); 
+                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1);
         CategoryItemEntity e2 = null;
-        
+
         try {
             e2 = (CategoryItemEntity) e1.clone();
         }
@@ -159,7 +159,7 @@ public class CategoryItemEntityTests extends TestCase {
         d.addValue(3.0, "R2", "C1");
         d.addValue(4.0, "R2", "C2");
         CategoryItemEntity e1 = new CategoryItemEntity(new Rectangle2D.Double(
-                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1); 
+                1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 1, "C2", 1);
         CategoryItemEntity e2 = null;
         try {
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
