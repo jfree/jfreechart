c1e137b372cd2e1507b0fab4ef155a43cf348296
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2008-09-18 13:11:34 +0000

    Modified default.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/renderer/junit/AbstractRendererTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/renderer/junit/AbstractRendererTests.java
@@ -927,7 +927,7 @@ public class AbstractRendererTests extends TestCase {
         assertEquals(true, r.getAutoPopulateSeriesPaint());
         assertEquals(false, r.getAutoPopulateSeriesFillPaint());
         assertEquals(false, r.getAutoPopulateSeriesOutlinePaint());
-        assertEquals(false, r.getAutoPopulateSeriesStroke());
+        assertEquals(true, r.getAutoPopulateSeriesStroke());
         assertEquals(false, r.getAutoPopulateSeriesOutlineStroke());
         assertEquals(true, r.getAutoPopulateSeriesShape());
     }
