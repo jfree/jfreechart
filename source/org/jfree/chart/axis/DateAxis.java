fe9a0c029e064fb75132ea6fd6e4acc15ae3ac83
David Gilbert <jfree@users.noreply.github.com>
2009-01-29 08:31:26 +0000

    Fixed deprecation warnings.

--- source/org/jfree/chart/axis/DateAxis.java
+++ source/org/jfree/chart/axis/DateAxis.java
@@ -185,7 +185,7 @@ public class DateAxis extends ValueAxis implements Cloneable, Serializable {
 
     /** The default date tick unit. */
     public static final DateTickUnit DEFAULT_DATE_TICK_UNIT
-            = new DateTickUnit(DateTickUnit.DAY, 1, new SimpleDateFormat());
+            = new DateTickUnit(DateTickUnitType.DAY, 1, new SimpleDateFormat());
 
     /** The default anchor date. */
     public static final Date DEFAULT_ANCHOR_DATE = new Date();
@@ -1170,97 +1170,97 @@ public class DateAxis extends ValueAxis implements Cloneable, Serializable {
         f7.setTimeZone(zone);
 
         // milliseconds
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 1, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 5,
-                DateTickUnit.MILLISECOND, 1, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 10,
-                DateTickUnit.MILLISECOND, 1, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 25,
-                DateTickUnit.MILLISECOND, 5, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 50,
-                DateTickUnit.MILLISECOND, 10, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 100,
-                DateTickUnit.MILLISECOND, 10, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 250,
-                DateTickUnit.MILLISECOND, 10, f1));
-        units.add(new DateTickUnit(DateTickUnit.MILLISECOND, 500,
-                DateTickUnit.MILLISECOND, 50, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 1, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 5,
+                DateTickUnitType.MILLISECOND, 1, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 10,
+                DateTickUnitType.MILLISECOND, 1, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 25,
+                DateTickUnitType.MILLISECOND, 5, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 50,
+                DateTickUnitType.MILLISECOND, 10, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 100,
+                DateTickUnitType.MILLISECOND, 10, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 250,
+                DateTickUnitType.MILLISECOND, 10, f1));
+        units.add(new DateTickUnit(DateTickUnitType.MILLISECOND, 500,
+                DateTickUnitType.MILLISECOND, 50, f1));
 
         // seconds
-        units.add(new DateTickUnit(DateTickUnit.SECOND, 1,
-                DateTickUnit.MILLISECOND, 50, f2));
-        units.add(new DateTickUnit(DateTickUnit.SECOND, 5,
-                DateTickUnit.SECOND, 1, f2));
-        units.add(new DateTickUnit(DateTickUnit.SECOND, 10,
-                DateTickUnit.SECOND, 1, f2));
-        units.add(new DateTickUnit(DateTickUnit.SECOND, 30,
-                DateTickUnit.SECOND, 5, f2));
+        units.add(new DateTickUnit(DateTickUnitType.SECOND, 1,
+                DateTickUnitType.MILLISECOND, 50, f2));
+        units.add(new DateTickUnit(DateTickUnitType.SECOND, 5,
+                DateTickUnitType.SECOND, 1, f2));
+        units.add(new DateTickUnit(DateTickUnitType.SECOND, 10,
+                DateTickUnitType.SECOND, 1, f2));
+        units.add(new DateTickUnit(DateTickUnitType.SECOND, 30,
+                DateTickUnitType.SECOND, 5, f2));
 
         // minutes
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 1,
-                DateTickUnit.SECOND, 5, f3));
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 2,
-                DateTickUnit.SECOND, 10, f3));
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 5,
-                DateTickUnit.MINUTE, 1, f3));
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 10,
-                DateTickUnit.MINUTE, 1, f3));
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 15,
-                DateTickUnit.MINUTE, 5, f3));
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 20,
-                DateTickUnit.MINUTE, 5, f3));
-        units.add(new DateTickUnit(DateTickUnit.MINUTE, 30,
-                DateTickUnit.MINUTE, 5, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 1,
+                DateTickUnitType.SECOND, 5, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 2,
+                DateTickUnitType.SECOND, 10, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 5,
+                DateTickUnitType.MINUTE, 1, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 10,
+                DateTickUnitType.MINUTE, 1, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 15,
+                DateTickUnitType.MINUTE, 5, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 20,
+                DateTickUnitType.MINUTE, 5, f3));
+        units.add(new DateTickUnit(DateTickUnitType.MINUTE, 30,
+                DateTickUnitType.MINUTE, 5, f3));
 
         // hours
-        units.add(new DateTickUnit(DateTickUnit.HOUR, 1,
-                DateTickUnit.MINUTE, 5, f3));
-        units.add(new DateTickUnit(DateTickUnit.HOUR, 2,
-                DateTickUnit.MINUTE, 10, f3));
-        units.add(new DateTickUnit(DateTickUnit.HOUR, 4,
-                DateTickUnit.MINUTE, 30, f3));
-        units.add(new DateTickUnit(DateTickUnit.HOUR, 6,
-                DateTickUnit.HOUR, 1, f3));
-        units.add(new DateTickUnit(DateTickUnit.HOUR, 12,
-                DateTickUnit.HOUR, 1, f4));
+        units.add(new DateTickUnit(DateTickUnitType.HOUR, 1,
+                DateTickUnitType.MINUTE, 5, f3));
+        units.add(new DateTickUnit(DateTickUnitType.HOUR, 2,
+                DateTickUnitType.MINUTE, 10, f3));
+        units.add(new DateTickUnit(DateTickUnitType.HOUR, 4,
+                DateTickUnitType.MINUTE, 30, f3));
+        units.add(new DateTickUnit(DateTickUnitType.HOUR, 6,
+                DateTickUnitType.HOUR, 1, f3));
+        units.add(new DateTickUnit(DateTickUnitType.HOUR, 12,
+                DateTickUnitType.HOUR, 1, f4));
 
         // days
-        units.add(new DateTickUnit(DateTickUnit.DAY, 1,
-                DateTickUnit.HOUR, 1, f5));
-        units.add(new DateTickUnit(DateTickUnit.DAY, 2,
-                DateTickUnit.HOUR, 1, f5));
-        units.add(new DateTickUnit(DateTickUnit.DAY, 7,
-                DateTickUnit.DAY, 1, f5));
-        units.add(new DateTickUnit(DateTickUnit.DAY, 15,
-                DateTickUnit.DAY, 1, f5));
+        units.add(new DateTickUnit(DateTickUnitType.DAY, 1,
+                DateTickUnitType.HOUR, 1, f5));
+        units.add(new DateTickUnit(DateTickUnitType.DAY, 2,
+                DateTickUnitType.HOUR, 1, f5));
+        units.add(new DateTickUnit(DateTickUnitType.DAY, 7,
+                DateTickUnitType.DAY, 1, f5));
+        units.add(new DateTickUnit(DateTickUnitType.DAY, 15,
+                DateTickUnitType.DAY, 1, f5));
 
         // months
-        units.add(new DateTickUnit(DateTickUnit.MONTH, 1,
-                DateTickUnit.DAY, 1, f6));
-        units.add(new DateTickUnit(DateTickUnit.MONTH, 2,
-                DateTickUnit.DAY, 1, f6));
-        units.add(new DateTickUnit(DateTickUnit.MONTH, 3,
-                DateTickUnit.MONTH, 1, f6));
-        units.add(new DateTickUnit(DateTickUnit.MONTH, 4,
-                DateTickUnit.MONTH, 1, f6));
-        units.add(new DateTickUnit(DateTickUnit.MONTH, 6,
-                DateTickUnit.MONTH, 1, f6));
+        units.add(new DateTickUnit(DateTickUnitType.MONTH, 1,
+                DateTickUnitType.DAY, 1, f6));
+        units.add(new DateTickUnit(DateTickUnitType.MONTH, 2,
+                DateTickUnitType.DAY, 1, f6));
+        units.add(new DateTickUnit(DateTickUnitType.MONTH, 3,
+                DateTickUnitType.MONTH, 1, f6));
+        units.add(new DateTickUnit(DateTickUnitType.MONTH, 4,
+                DateTickUnitType.MONTH, 1, f6));
+        units.add(new DateTickUnit(DateTickUnitType.MONTH, 6,
+                DateTickUnitType.MONTH, 1, f6));
 
         // years
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 1,
-                DateTickUnit.MONTH, 1, f7));
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 2,
-                DateTickUnit.MONTH, 3, f7));
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 5,
-                DateTickUnit.YEAR, 1, f7));
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 10,
-                DateTickUnit.YEAR, 1, f7));
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 25,
-                DateTickUnit.YEAR, 5, f7));
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 50,
-                DateTickUnit.YEAR, 10, f7));
-        units.add(new DateTickUnit(DateTickUnit.YEAR, 100,
-                DateTickUnit.YEAR, 20, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 1,
+                DateTickUnitType.MONTH, 1, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 2,
+                DateTickUnitType.MONTH, 3, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 5,
+                DateTickUnitType.YEAR, 1, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 10,
+                DateTickUnitType.YEAR, 1, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 25,
+                DateTickUnitType.YEAR, 5, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 50,
+                DateTickUnitType.YEAR, 10, f7));
+        units.add(new DateTickUnit(DateTickUnitType.YEAR, 100,
+                DateTickUnitType.YEAR, 20, f7));
 
         return units;
 
