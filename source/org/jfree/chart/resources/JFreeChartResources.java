a79ba8f922d1f8266f65ea74b0c55eaaefe9a914
David Gilbert <jfree@users.noreply.github.com>
2009-02-03 16:09:17 +0000

    Updated version number for next release (hopefully before the end of Feb).

--- source/org/jfree/chart/resources/JFreeChartResources.java
+++ source/org/jfree/chart/resources/JFreeChartResources.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2009, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ------------------------
  * JFreeChartResources.java
  * ------------------------
- * (C) Copyright 2002-2008, by Object Refinery Limited.
+ * (C) Copyright 2002-2009, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -55,10 +55,10 @@ public class JFreeChartResources extends ListResourceBundle {
     /** The resources to be localised. */
     private static final Object[][] CONTENTS = {
         {"project.name",      "JFreeChart"},
-        {"project.version",   "1.0.11"},
+        {"project.version",   "1.0.13"},
         {"project.info",      "http://www.jfree.org/jfreechart/index.html"},
         {"project.copyright",
-         "(C)opyright 2000-2008, by Object Refinery Limited and Contributors"}
+         "(C)opyright 2000-2009, by Object Refinery Limited and Contributors"}
     };
 
 }
