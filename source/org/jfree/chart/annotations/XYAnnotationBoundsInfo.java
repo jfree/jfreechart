26037f388256fb6e371a58eab9fba8b632723381
David Gilbert <jfree@users.noreply.github.com>
2009-03-17 14:05:06 +0000

    Added API docs.

--- source/org/jfree/chart/annotations/XYAnnotationBoundsInfo.java
+++ source/org/jfree/chart/annotations/XYAnnotationBoundsInfo.java
@@ -49,10 +49,29 @@ import org.jfree.data.Range;
  */
 public interface XYAnnotationBoundsInfo {
 
+    /**
+     * Returns a flag that determines whether or not the annotation's
+     * bounds should be taken into account for auto-range calculations on
+     * the axes that the annotation is plotted against.
+     *
+     * @return A boolean.
+     */
     public boolean getIncludeInDataBounds();
-    
+
+    /**
+     * Returns the range of x-values (in data space) that the annotation
+     * uses.
+     *
+     * @return The x-range.
+     */
     public Range getXRange();
-    
+
+    /**
+     * Returns the range of y-values (in data space) that the annotation
+     * uses.
+     *
+     * @return The y-range.
+     */
     public Range getYRange();
 
 }
