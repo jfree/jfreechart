b03c6a31d2679d562fb28fafe92ba937a26260fc
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2009-05-11 19:43:19 +0000

    Fixed Checkstyle warnings.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/axis/Axis.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/axis/Axis.java
@@ -1067,37 +1067,37 @@ public abstract class Axis implements Cloneable, Serializable {
      *
      * @since 1.0.13
      */
-	protected void createAndAddEntity(double cursor, AxisState state,
+    protected void createAndAddEntity(double cursor, AxisState state,
             Rectangle2D dataArea, RectangleEdge edge,
-            PlotRenderingInfo plotState){
+            PlotRenderingInfo plotState) {
 
-		if (plotState == null || plotState.getOwner() == null) {
+        if (plotState == null || plotState.getOwner() == null) {
             return;  // no need to create entity if we can´t save it anyways...
         }
-		Rectangle2D hotspot = null;
-		if (edge.equals(RectangleEdge.TOP)){
-			hotspot = new Rectangle2D.Double(dataArea.getX(),
+        Rectangle2D hotspot = null;
+        if (edge.equals(RectangleEdge.TOP)) {
+            hotspot = new Rectangle2D.Double(dataArea.getX(),
                     state.getCursor(), dataArea.getWidth(),
                     cursor - state.getCursor());
-		}
-		else if(edge.equals(RectangleEdge.BOTTOM)) {
-			hotspot = new Rectangle2D.Double(dataArea.getX(), cursor,
+        }
+        else if (edge.equals(RectangleEdge.BOTTOM)) {
+            hotspot = new Rectangle2D.Double(dataArea.getX(), cursor,
                     dataArea.getWidth(), state.getCursor() - cursor);
-		}
-		else if(edge.equals(RectangleEdge.LEFT)) {
-			hotspot = new Rectangle2D.Double(state.getCursor(),
+        }
+        else if (edge.equals(RectangleEdge.LEFT)) {
+            hotspot = new Rectangle2D.Double(state.getCursor(),
                     dataArea.getY(), cursor - state.getCursor(),
                     dataArea.getHeight());
-		}
-		else if(edge.equals(RectangleEdge.RIGHT)){
-			hotspot = new Rectangle2D.Double(cursor, dataArea.getY(),
+        }
+        else if (edge.equals(RectangleEdge.RIGHT)) {
+            hotspot = new Rectangle2D.Double(cursor, dataArea.getY(),
                     state.getCursor() - cursor, dataArea.getHeight());
-		}
-		EntityCollection e = plotState.getOwner().getEntityCollection();
-		if (e != null) {
+        }
+        EntityCollection e = plotState.getOwner().getEntityCollection();
+        if (e != null) {
             e.add(new AxisEntity(hotspot, this));
         }
-	}
+    }
 
     /**
      * Registers an object for notification of changes to the axis.
