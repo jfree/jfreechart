638e37ad8127114048d3747a1853837ebdb0cbcc
David Gilbert <jfree@users.noreply.github.com>
2009-05-28 04:40:49 +0000

    2009-05-29  David Gilbert  <david.gilbert@object-refinery.com>
        Patch 2795746 by Peter Kolb:
        * source/org/jfree/data/function/PolynomialFunction2D;java: New file,
        * source/org/jfree/data/statistics/Regression.java
        (getPolynomialRegression): New method,
        (calculateSubmatrix): New private method,
        * tests/org/jfree/data/function/junit/DataFunctionPackageTests.java:
        New test file,
        * tests/org/jfree/data/function/junit/PolynomialFunction2DTests.java:
        Likewise.

---
+++ source/org/jfree/data/function/PolynomialFunction2D.java
@@ -0,0 +1,131 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2009, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * -------------------------
+ * PolynomialFunction2D.java
+ * -------------------------
+ * (C) Copyright 2009, by Object Refinery Limited.
+ *
+ * Original Author:  Peter Kolb;
+ * Contributor(s):   David Gilbert (for Object Refinery Limited);
+ *
+ * Changes:
+ * --------
+ * 23-Mar-2009 : Version 1, patch 2795746 (PK);
+ * 28-May-2009 : Integrated in JFreeChart with modifications (DG);
+ *
+ */
+
+package org.jfree.data.function;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import org.jfree.chart.HashUtilities;
+
+/**
+ * A function in the form <code>y = a0 + a1 * x + a2 * x^2 + ... + an *
+ * x^n</code>.  Instances of this class are immutable.
+ *
+ * @since 1.0.14
+ */
+public class PolynomialFunction2D implements Function2D, Serializable {
+
+    /** The coefficients. */
+    private double[] coefficients;
+
+    /**
+     * Constructs a new polynomial function <code>y = a0 + a1 * x + a2 * x^2 +
+     * ... + an * x^n</code>
+     *
+     * @param coefficients  an array with the coefficients [a0, a1, ..., an]
+     *         (<code>null</code> not permitted).
+     */
+    public PolynomialFunction2D(double[] coefficients) {
+        if (coefficients == null) {
+            throw new IllegalArgumentException("Null 'coefficients' argument");
+        }
+        this.coefficients = coefficients.clone();
+    }
+
+    /**
+     * Returns a copy of the coefficients array that was specified in the
+     * constructor.
+     *
+     * @return The coefficients array.
+     */
+    public double[] getCoefficients() {
+        return this.coefficients.clone();
+    }
+
+    /**
+     * Returns the order of the polynomial.
+     *
+     * @return The order.
+     */
+    public int getOrder() {
+        return this.coefficients.length - 1;
+    }
+
+    /**
+     * Returns the function value.
+     *
+     * @param x  the x-value.
+     *
+     * @return The value.
+     */
+    public double getValue(double x) {
+        double y = 0;
+        for(int i = 0; i < coefficients.length; i++){
+            y += coefficients[i] * Math.pow(x, i);
+        }
+        return y;
+    }
+
+    /**
+     * Tests this function for equality with an arbitrary object.
+     *
+     * @param obj  the object (<code>null</code> permitted).
+     *
+     * @return A boolean.
+     */
+    public boolean equals(Object obj) {
+        if (!(obj instanceof PolynomialFunction2D)) {
+            return false;
+        }
+        PolynomialFunction2D that = (PolynomialFunction2D) obj;
+        return Arrays.equals(this.coefficients, that.coefficients);
+    }
+
+    /**
+     * Returns a hash code for this instance.
+     *
+     * @return A hash code.
+     */
+    public int hashCode() {
+        return HashUtilities.hashCodeForDoubleArray(this.coefficients);
+    }
+
+}
