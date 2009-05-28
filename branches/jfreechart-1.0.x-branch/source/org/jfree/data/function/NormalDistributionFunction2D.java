760bf83c441500a99e7c2d4787a1ec84727d1bf6
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2009-05-28 06:18:43 +0000

    2009-05-28  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/data/function/NormalDistributionFunction2D.java:
        Implemented Serializable,
        (equals): New method override,
        (hashCode): Likewise,
        * tests/org/jfree/data/function/junit/NormalDistributionFunction2DTests.java:
        New file.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/data/function/NormalDistributionFunction2D.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/data/function/NormalDistributionFunction2D.java
@@ -38,16 +38,22 @@
  * 21-Nov-2005 : Added getters for the mean and standard deviation (DG);
  * 12-Feb-2009 : Precompute some constants from the function - see bug
  *               2572016 (DG);
+ * 28-May-2009 : Implemented equals() and hashCode(), and added serialization
+ *               support (DG);
  *
  */
 
 package org.jfree.data.function;
 
+import java.io.Serializable;
+
+import org.jfree.chart.HashUtilities;
+
 /**
  * A normal distribution function.  See
  * http://en.wikipedia.org/wiki/Normal_distribution.
  */
-public class NormalDistributionFunction2D implements Function2D {
+public class NormalDistributionFunction2D implements Function2D, Serializable {
 
     /** The mean. */
     private double mean;
@@ -106,6 +112,39 @@ public class NormalDistributionFunction2D implements Function2D {
     public double getValue(double x) {
         double z = x - this.mean;
         return this.factor * Math.exp(-z * z / this.denominator);
+    }
+
+    /**
+     * Tests this function for equality with an arbitrary object.
+     *
+     * @param obj  the object (<code>null</code> permitted).
+     *
+     * @return A boolean.
+     */
+    public boolean equals(Object obj) {
+        if (!(obj instanceof NormalDistributionFunction2D)) {
+            return false;
+        }
+        NormalDistributionFunction2D that = (NormalDistributionFunction2D) obj;
+        if (this.mean != that.mean) {
+            return false;
+        }
+        if (this.std != that.std) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns a hash code for this instance.
+     *
+     * @return A hash code.
+     */
+    public int hashCode() {
+        int result = 29;
+        result = HashUtilities.hashCode(result, this.mean);
+        result = HashUtilities.hashCode(result, this.std);
+        return result;
     }
 
 }
