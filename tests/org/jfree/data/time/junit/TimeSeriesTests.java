6bf077fc9167c0ad5112e49da2c5483fcda10d6a
David Gilbert <jfree@users.noreply.github.com>
2009-06-10 10:01:40 +0000

    2009-06-10  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/data/time/TimeSeries.java
        (addOrUpdate(RegularTimePeriod, Number)): Delegate to new method,
        (addOrUpdate(TimeSeriesDataItem)): New method.

--- tests/org/jfree/data/time/junit/TimeSeriesTests.java
+++ tests/org/jfree/data/time/junit/TimeSeriesTests.java
@@ -672,6 +672,30 @@ public class TimeSeriesTests extends TestCase implements SeriesChangeListener {
     }
 
     /**
+     * Some more checks for the addOrUpdate() method.
+     */
+    public void testAddOrUpdate4() {
+        TimeSeries ts = new TimeSeries("S");
+        TimeSeriesDataItem overwritten = ts.addOrUpdate(new Year(2009), 20.09);
+        assertNull(overwritten);
+        overwritten = ts.addOrUpdate(new Year(2009), 1.0);
+        assertEquals(new Double(20.09), overwritten.getValue());
+        assertEquals(new Double(1.0), ts.getValue(new Year(2009)));
+
+        // changing the overwritten record shouldn't affect the series
+        overwritten.setValue(null);
+        assertEquals(new Double(1.0), ts.getValue(new Year(2009)));
+
+        TimeSeriesDataItem item = new TimeSeriesDataItem(new Year(2010), 20.10);
+        overwritten = ts.addOrUpdate(item);
+        assertNull(overwritten);
+        assertEquals(new Double(20.10), ts.getValue(new Year(2010)));
+        // changing the item that was added should not change the series
+        item.setValue(null);
+        assertEquals(new Double(20.10), ts.getValue(new Year(2010)));
+    }
+
+    /**
      * A test for the bug report 1075255.
      */
     public void testBug1075255() {
