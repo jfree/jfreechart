2462fdebabd98d18086a0d6f3ec14d7093f5d3c6
David Gilbert <jfree@users.noreply.github.com>
2009-09-15 18:38:45 +0000

    Clarifications to API docs.

--- source/org/jfree/data/xy/IntervalXYDataset.java
+++ source/org/jfree/data/xy/IntervalXYDataset.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2009, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ----------------------
  * IntervalXYDataset.java
  * ----------------------
- * (C) Copyright 2001-2008, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2001-2009, by Object Refinery Limited and Contributors.
  *
  * Original Author:  Mark Watson (www.markwatson.com);
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -37,100 +37,118 @@
  * 18-Oct-2001 : Version 1, thanks to Mark Watson (DG);
  * 22-Oct-2001 : Renamed DataSource.java --> Dataset.java etc (DG);
  * 06-May-2004 : Added methods that return double primitives (DG);
+ * 15-Sep-2009 : Added clarifications to API docs (DG);
  *
  */
 
 package org.jfree.data.xy;
 
 /**
- * An extension of the {@link XYDataset} interface that allows a range of data
- * to be defined for the X values, the Y values, or both the X and Y values.
+ * An extension of the {@link XYDataset} interface that allows an x-interval
+ * and a y-interval to be defined.  Note that the x and y values defined
+ * by the parent interface are NOT required to fall within these intervals.
  * This interface is used to support (among other things) bar plots against
  * numerical axes.
  */
 public interface IntervalXYDataset extends XYDataset {
 
     /**
-     * Returns the starting X value for the specified series and item.
+     * Returns the lower bound of the x-interval for the specified series and
+     * item.  If this lower bound is specified, it should be less than or
+     * equal to the upper bound of the interval (if one is specified).
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The value.
+     * @return The lower bound of the x-interval (<code>null</code> permitted).
      */
     public Number getStartX(int series, int item);
 
     /**
-     * Returns the start x-value (as a double primitive) for an item within a
-     * series.
+     * Returns the lower bound of the x-interval (as a double primitive) for
+     * the specified series and item.
      *
      * @param series  the series (zero-based index).
      * @param item  the item (zero-based index).
      *
-     * @return The start x-value.
+     * @return The lower bound of the x-interval.
+     *
+     * @see #getStartX(int, int)
      */
     public double getStartXValue(int series, int item);
 
     /**
-     * Returns the ending X value for the specified series and item.
+     * Returns the upper bound of the x-interval for the specified series and
+     * item.  If this upper bound is specified, it should be greater than or
+     * equal to the lower bound of the interval (if one is specified).
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The value.
+     * @return The upper bound of the x-interval (<code>null</code> permitted).
      */
     public Number getEndX(int series, int item);
 
     /**
-     * Returns the end x-value (as a double primitive) for an item within a
-     * series.
+     * Returns the upper bound of the x-interval (as a double primitive) for
+     * the specified series and item.
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The end x-value.
+     * @return The upper bound of the x-interval.
+     *
+     * @see #getEndX(int, int)
      */
     public double getEndXValue(int series, int item);
 
     /**
-     * Returns the starting Y value for the specified series and item.
+     * Returns the lower bound of the y-interval for the specified series and
+     * item.  If this lower bound is specified, it should be less than or
+     * equal to the upper bound of the interval (if one is specified).
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The value.
+     * @return The lower bound of the y-interval (<code>null</code> permitted).
      */
     public Number getStartY(int series, int item);
 
     /**
-     * Returns the start y-value (as a double primitive) for an item within a
-     * series.
+     * Returns the lower bound of the y-interval (as a double primitive) for
+     * the specified series and item.
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The start y-value.
+     * @return The lower bound of the y-interval.
+     *
+     * @see #getStartY(int, int)
      */
     public double getStartYValue(int series, int item);
 
     /**
-     * Returns the ending Y value for the specified series and item.
+     * Returns the upper bound of the y-interval for the specified series and
+     * item.  If this upper bound is specified, it should be greater than or
+     * equal to the lower bound of the interval (if one is specified).
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The value.
+     * @return The upper bound of the y-interval (<code>null</code> permitted).
      */
     public Number getEndY(int series, int item);
 
     /**
-     * Returns the end y-value (as a double primitive) for an item within a
-     * series.
+     * Returns the upper bound of the y-interval (as a double primitive) for
+     * the specified series and item.
      *
      * @param series  the series index (zero-based).
      * @param item  the item index (zero-based).
      *
-     * @return The end y-value.
+     * @return The upper bound of the y-interval.
+     *
+     * @see #getEndY(int, int)
      */
     public double getEndYValue(int series, int item);
 
