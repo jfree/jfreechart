93926a1a93d133fe1c598024b9a9cb00f9c672c7
David Gilbert <jfree@users.noreply.github.com>
2011-10-30 21:04:50 +0000

    Update headers for Oracle trademarks.

--- tests/org/jfree/chart/labels/junit/IntervalCategoryItemLabelGeneratorTests.java
+++ tests/org/jfree/chart/labels/junit/IntervalCategoryItemLabelGeneratorTests.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2011, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -21,12 +21,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
- * in the United States and other countries.]
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
  *
- * ----------------------------------------
- * IntervalCategoryLabelGeneratorTests.java
- * ----------------------------------------
+ * --------------------------------------------
+ * IntervalCategoryItemLabelGeneratorTests.java
+ * --------------------------------------------
  * (C) Copyright 2003-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
