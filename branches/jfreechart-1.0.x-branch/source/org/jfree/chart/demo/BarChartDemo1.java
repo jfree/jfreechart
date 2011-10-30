6a715f4f53f5437441f1354bf47716e0432ce355
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2011-10-30 10:48:34 +0000

    2011-10-30  David Gilbert <david.gilbert@object-refinery.com>
        * org/jfree/chart/StandardChartTheme.java
        (StandardChartTheme(String, boolean)): New method.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/demo/BarChartDemo1.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/demo/BarChartDemo1.java
@@ -47,6 +47,7 @@ import java.awt.GradientPaint;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.axis.NumberAxis;
@@ -62,6 +63,15 @@ import org.jfree.ui.RefineryUtilities;
  * A simple demonstration application showing how to create a bar chart.
  */
 public class BarChartDemo1 extends ApplicationFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    {
+        // set a theme using the new shadow generator feature available in
+        // 1.0.14 - for backwards compatibility it is not enabled by default
+        ChartFactory.setChartTheme(new StandardChartTheme("JFree/Shadow",
+                true));
+    }
 
     /**
      * Creates a new demo instance.
