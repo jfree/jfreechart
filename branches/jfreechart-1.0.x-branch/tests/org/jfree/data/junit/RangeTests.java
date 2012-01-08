7d8dbb78252e3eb5bbfd6a93678cc2d0f9011a25
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2012-01-08 20:09:17 +0000

    2012-01-08  David Gilbert <david.gilbert@object-refinery.com>
        * source/org/jfree/data/Range.java
        (combineIgnoringNaN): New method.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/data/junit/RangeTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/data/junit/RangeTests.java
@@ -352,4 +352,26 @@ public class RangeTests extends TestCase {
         assertTrue(Double.isNaN(rr.getUpperBound()));
     }
 
+    /**
+     * Some checks for the combineIgnoringNaN() method.
+     */
+    public void testCombineIgnoringNaN() {
+        Range r1 = new Range(1.0, 2.0);
+        Range r2 = new Range(1.5, 2.5);
+
+        assertNull(Range.combineIgnoringNaN(null, null));
+        assertEquals(r1, Range.combineIgnoringNaN(r1, null));
+        assertEquals(r2, Range.combineIgnoringNaN(null, r2));
+        assertEquals(new Range(1.0, 2.5), Range.combineIgnoringNaN(r1, r2));
+
+        Range r3 = new Range(Double.NaN, 1.3);
+        Range rr = Range.combineIgnoringNaN(r1, r3);
+        assertEquals(1.0, rr.getLowerBound(), EPSILON);
+        assertEquals(2.0, rr.getUpperBound(), EPSILON);
+
+        Range r4 = new Range(1.7, Double.NaN);
+        rr = Range.combineIgnoringNaN(r4, r1);
+        assertEquals(1.0, rr.getLowerBound(), EPSILON);
+        assertEquals(2.0, rr.getUpperBound(), EPSILON);
+    }
 }
