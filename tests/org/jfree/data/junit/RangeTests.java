d8de41b640b2a0c846ee5f918ee49cd75f1b22de
David Gilbert <jfree@users.noreply.github.com>
2012-01-08 18:44:46 +0000

    Added testCombine().

--- tests/org/jfree/data/junit/RangeTests.java
+++ tests/org/jfree/data/junit/RangeTests.java
@@ -36,6 +36,7 @@
  * -------
  * 14-Aug-2003 : Version 1 (DG);
  * 18-Dec-2007 : Additional tests from Sergei Ivanov (DG);
+ * 08-Jan-2012 : Added test for combine() method (DG);
  *
  */
 
@@ -326,5 +327,29 @@ public class RangeTests extends TestCase {
 
     }
 
+    private static final double EPSILON = 0.0000000001;
+
+    /**
+     * Some checks for the combine method.
+     */
+    public void testCombine() {
+        Range r1 = new Range(1.0, 2.0);
+        Range r2 = new Range(1.5, 2.5);
+
+        assertNull(Range.combine(null, null));
+        assertEquals(r1, Range.combine(r1, null));
+        assertEquals(r2, Range.combine(null, r2));
+        assertEquals(new Range(1.0, 2.5), Range.combine(r1, r2));
+
+        Range r3 = new Range(Double.NaN, 1.3);
+        Range rr = Range.combine(r1, r3);
+        assertTrue(Double.isNaN(rr.getLowerBound()));
+        assertEquals(2.0, rr.getUpperBound(), EPSILON);
+
+        Range r4 = new Range(1.7, Double.NaN);
+        rr = Range.combine(r4, r1);
+        assertEquals(1.0, rr.getLowerBound(), EPSILON);
+        assertTrue(Double.isNaN(rr.getUpperBound()));
+    }
 
 }
