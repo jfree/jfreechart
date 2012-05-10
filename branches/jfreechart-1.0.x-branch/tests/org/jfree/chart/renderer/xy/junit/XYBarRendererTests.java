12f0a77e207012116c79c0b7ec53ab5beae73fb7
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2012-05-10 04:09:15 +0000

    2012-05-10  David Gilbert <david.gilbert@object-refinery.com>
        * org/jfree/chart/renderer/xy/XYBarRenderer.java
        (findDomainBounds): call new method in super-class to take account of
        non-visible series,
        (findRangeBounds): likewise;
        * tests/org/jfree/chart/renderer/xy/junit/XYBarRendererTests.java
        (testFindDomainBounds2): new test;
        (testFindRangeBounds2): Likewise.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/renderer/xy/junit/XYBarRendererTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/renderer/xy/junit/XYBarRendererTests.java
@@ -69,6 +69,8 @@ import org.jfree.chart.renderer.xy.XYBarRenderer;
 import org.jfree.data.Range;
 import org.jfree.data.xy.DefaultIntervalXYDataset;
 import org.jfree.data.xy.XYBarDataset;
+import org.jfree.data.xy.XYIntervalSeries;
+import org.jfree.data.xy.XYIntervalSeriesCollection;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.GradientPaintTransformType;
@@ -302,6 +304,32 @@ public class XYBarRendererTests extends TestCase {
         assertFalse(bounds.contains(2.8));
     }
 
+    /**
+     * A test for the findDomainBounds method to ensure it correctly accounts 
+     * for the series visibility.
+     */
+    public void testFindDomainBounds2() {
+        XYIntervalSeries s1 = new XYIntervalSeries("S1");
+        s1.add(1.0, 0.5, 1.5, 10.0, 9.5, 10.5);
+        s1.add(2.0, 1.9, 2.1, 20.0, 19.8, 20.3);
+        XYIntervalSeries s2 = new XYIntervalSeries("S2");
+        s2.add(3.0, 2.5, 3.5, 30.0, 29.5, 30.5);
+        s2.add(4.0, 3.9, 4.1, 9.0, 9.0, 9.0);
+        XYIntervalSeriesCollection dataset = new XYIntervalSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+        
+        XYBarRenderer renderer = new XYBarRenderer();
+        Range r = renderer.findDomainBounds(dataset);
+        assertEquals(0.5, r.getLowerBound(), EPSILON);
+        assertEquals(4.1, r.getUpperBound(), EPSILON);
+        
+        renderer.setSeriesVisible(1, Boolean.FALSE);
+        r = renderer.findDomainBounds(dataset);
+        assertEquals(0.5, r.getLowerBound(), EPSILON);
+        assertEquals(2.1, r.getUpperBound(), EPSILON);
+    }
+
     private static final double EPSILON = 0.0000000001;
 
     /**
@@ -327,6 +355,33 @@ public class XYBarRendererTests extends TestCase {
         r = renderer.findRangeBounds(dataset);
         assertEquals(1.0, r.getLowerBound(), EPSILON);
         assertEquals(4.0, r.getUpperBound(), EPSILON);
+    }
+
+    /**
+     * A test for the findRangeBounds method to ensure it correctly accounts 
+     * for the series visibility.
+     */
+    public void testFindRangeBounds2() {
+        XYIntervalSeries s1 = new XYIntervalSeries("S1");
+        s1.add(1.0, 0.5, 1.5, 10.0, 9.5, 10.5);
+        s1.add(2.0, 1.9, 2.1, 20.0, 19.8, 20.3);
+        XYIntervalSeries s2 = new XYIntervalSeries("S2");
+        s2.add(3.0, 2.5, 3.5, 30.0, 29.5, 30.5);
+        s2.add(4.0, 3.9, 4.1, 9.0, 9.0, 9.0);
+        XYIntervalSeriesCollection dataset = new XYIntervalSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+        
+        XYBarRenderer renderer = new XYBarRenderer();
+        renderer.setUseYInterval(false);
+        Range r = renderer.findRangeBounds(dataset);
+        assertEquals(9.0, r.getLowerBound(), EPSILON);
+        assertEquals(30.0, r.getUpperBound(), EPSILON);
+        
+        renderer.setSeriesVisible(1, Boolean.FALSE);
+        r = renderer.findRangeBounds(dataset);
+        assertEquals(10.0, r.getLowerBound(), EPSILON);
+        assertEquals(20.0, r.getUpperBound(), EPSILON);
     }
 
     /**
