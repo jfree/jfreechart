f23878849d0ae7f93db70efbcbea81cdbf33465d
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2012-07-05 19:04:09 +0000

    2012-07-05  David Gilbert <david.gilbert@object-refinery.com>
        * org/jfree/chart/ChartPanel.java
        (setMouseWheelEnabled): Remove reflection only needed for JRE 1.3.1.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/MouseWheelHandler.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/MouseWheelHandler.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2011, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2012, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ----------------------
  * MouseWheelHandler.java
  * ----------------------
- * (C) Copyright 2009, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2009 by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   Ulrich Voigt - patch 2686040;
