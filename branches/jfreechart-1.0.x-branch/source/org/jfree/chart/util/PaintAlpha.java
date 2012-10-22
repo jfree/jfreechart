ab5858ef1dcb341f0a29b20c6ac3ace1ea243904
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2012-10-22 05:32:42 +0000

    Use reflection in PaintAlpha to allow compilation in JDK 1.4 (from Dave Law)

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/util/PaintAlpha.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/util/PaintAlpha.java
@@ -35,6 +35,8 @@
  * Changes
  * -------
  * 09-Mar-2011 : Written (DaveLaw)
+ * 03-Jul-2012 : JDK 1.6 References made reflective for JDK 1.3 compatibility (
+ *               DaveLaw)
  *
  */
 
@@ -42,13 +44,15 @@ package org.jfree.chart.util;
 
 import java.awt.Color;
 import java.awt.GradientPaint;
-import java.awt.LinearGradientPaint;
 import java.awt.Paint;
-import java.awt.RadialGradientPaint;
 import java.awt.TexturePaint;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.IndexColorModel;
 import java.awt.image.WritableRaster;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Hashtable;
 
 /**
@@ -76,6 +80,10 @@ import java.util.Hashtable;
  * @author  DaveLaw
  */
 public class PaintAlpha {
+    // TODO Revert to SVN revision 2469 in JFreeChart 1.0.16
+    //      (MultipleGradientPaint's / JDK issues)
+    // TODO THEN: change visibility of ALL darker(...) Methods EXCEPT
+    //      darker(Paint) to private!
 
     /**
      * Multiplier for the <code>darker</code> Methods.<br>
@@ -86,13 +94,13 @@ public class PaintAlpha {
     private static boolean legacyAlpha = false;
 
     /**
-     * Per default <code>PaintAlpha</code> will try to honour alpha-channel 
+     * Per default <code>PaintAlpha</code> will try to honour alpha-channel
      * information.  In the past this was not the case.
-     * If you wish legacy functionality for your application you can request 
+     * If you wish legacy functionality for your application you can request
      * this here.
-     * 
+     *
      * @param legacyAlpha boolean
-     * 
+     *
      * @return the previous setting
      */
     public static boolean setLegacyAlpha(boolean legacyAlpha) {
@@ -125,11 +133,13 @@ public class PaintAlpha {
         if (paint instanceof GradientPaint) {
             return darker((GradientPaint) paint);
         }
-        if (paint instanceof LinearGradientPaint) {
-            return darker((LinearGradientPaint) paint);
+        if (paint.getClass().getName().equals("java.awt.LinearGradientPaint")) {
+            // TODO -> instanceof
+            return darkerLinearGradientPaint(paint);
         }
-        if (paint instanceof RadialGradientPaint) {
-            return darker((RadialGradientPaint) paint);
+        if (paint.getClass().getName().equals("java.awt.RadialGradientPaint")) {
+            // TODO -> instanceof
+            return darkerRadialGradientPaint(paint);
         }
         if (paint instanceof TexturePaint) {
             return darker((TexturePaint) paint, true);
@@ -142,12 +152,12 @@ public class PaintAlpha {
      * <p>
      * The essential difference is that this method
      * maintains the alpha-channel unchanged<br>
-     * 
+     *
      * @param paint a <code>Color</code>
-     * 
+     *
      * @return a darker version of the <code>Color</code>
      */
-    public static Color darker(Color paint) {
+    private static Color darker(Color paint) {
         return new Color(
                 (int)(paint.getRed  () * FACTOR),
                 (int)(paint.getGreen() * FACTOR),
@@ -156,12 +166,12 @@ public class PaintAlpha {
 
     /**
      * Create a new Gradient with its colours darkened.
-     * 
+     *
      * @param paint a <code>GradientPaint</code>
-     * 
+     *
      * @return a darker version of the <code>GradientPaint</code>
      */
-    public static GradientPaint darker(GradientPaint paint) {
+    private static GradientPaint darker(GradientPaint paint) {
         return new GradientPaint(
                 paint.getPoint1(), darker(paint.getColor1()),
                 paint.getPoint2(), darker(paint.getColor2()),
@@ -170,51 +180,124 @@ public class PaintAlpha {
 
     /**
      * Create a new Gradient with its colours darkened.
-     * 
+     *
      * @param paint a <code>LinearGradientPaint</code>
-     * 
+     *
      * @return a darker version of the <code>LinearGradientPaint</code>
      */
-    public static LinearGradientPaint darker(LinearGradientPaint paint) {
+    private static final Paint darkerLinearGradientPaint(Paint paint) {
+        // TODO Rename->darker & change Paint->LinearGradientPaint
+        try {
+            final Color[] paintColours = (Color[]) invokeZeroArgumentMethod(
+                    paint, "getColors");
 
-        final Color[] paintColours = paint.getColors();
+            for (int i = 0; i < paintColours.length; i++) {
+                paintColours[i] = darker(paintColours[i]);
+            }
 
-        for (int i = 0; i < paintColours.length; i++) {
-            paintColours[i] = darker(paintColours[i]);
+            final Constructor[] constructors
+                    = paint.getClass().getConstructors();
+
+            for (int i = 0; i < constructors.length; i++) {
+
+                final Class[] args = constructors[i].getParameterTypes();
+
+                if (args.length == 7
+                        &&  args[args.length-1].equals(AffineTransform.class)) {
+
+                    return (Paint) constructors[i].newInstance(new Object[] {
+                        (Point2D) invokeZeroArgumentMethod(paint, "getStartPoint"),
+                        (Point2D) invokeZeroArgumentMethod(paint, "getEndPoint"),
+                        (float[]) invokeZeroArgumentMethod(paint, "getFractions"),
+                        paintColours,
+                        (Object) invokeZeroArgumentMethod(paint, "getCycleMethod"),
+                        (Object) invokeZeroArgumentMethod(paint, "getColorSpace"),
+                        (AffineTransform) invokeZeroArgumentMethod(paint,
+                            "getTransform") });
+                    }
+                }
+            } catch (IllegalArgumentException e) {
+            } catch (SecurityException e) {
+            } catch (IllegalAccessException e) {
+            } catch (InvocationTargetException e) {
+            } catch (NoSuchMethodException e) {
+            } catch (InstantiationException e) {
         }
-
-        return new LinearGradientPaint(paint.getStartPoint(),
-                paint.getEndPoint(),
-                paint.getFractions(),
-                paintColours,
-                paint.getCycleMethod(), 
-                paint.getColorSpace(),
-                paint.getTransform());
+        throw new UnsupportedOperationException(
+                "Probably new Constructor signatures in newer JDK");
     }
 
     /**
      * Create a new Gradient with its colours darkened.
-     * 
+     *
      * @param paint a <code>RadialGradientPaint</code>
-     * 
+     *
      * @return a darker version of the <code>RadialGradientPaint</code>
      */
-    public static RadialGradientPaint darker(RadialGradientPaint paint) {
+    private static final Paint darkerRadialGradientPaint(Paint paint) {
+        // TODO Rename->darker & change Paint->RadialGradientPaint
+        try {
+            final Color[] paintColours
+                    = (Color[]) invokeZeroArgumentMethod(paint, "getColors");
 
-        final Color[] paintColours = paint.getColors();
+            for (int i = 0; i < paintColours.length; i++) {
+                paintColours[i] = darker(paintColours[i]);
+            }
 
-        for (int i = 0; i < paintColours.length; i++) {
-            paintColours[i] = darker(paintColours[i]);
+            final Constructor[] constructors
+                    = paint.getClass().getConstructors();
+
+            for (int i = 0; i < constructors.length; i++) {
+
+                final Class[] args = constructors[i].getParameterTypes();
+
+                if (     args.length == 8
+                &&  args[args.length-1].equals(AffineTransform.class)) {
+
+                    return (Paint) constructors[i].newInstance(new Object[] {
+                        (Point2D) invokeZeroArgumentMethod(paint, "getCenterPoint"),
+                        (Float) invokeZeroArgumentMethod(paint, "getRadius"),
+                        (Point2D) invokeZeroArgumentMethod(paint, "getFocusPoint"),
+                        (float[]) invokeZeroArgumentMethod(paint, "getFractions"),
+                        paintColours,
+                        (Object) invokeZeroArgumentMethod(paint, "getCycleMethod"),
+                        (Object) invokeZeroArgumentMethod(paint, "getColorSpace"),
+                        (AffineTransform) invokeZeroArgumentMethod(paint,
+                            "getTransform") });
+                }
+            }
+        } catch (IllegalArgumentException e) {
+        } catch (SecurityException e) {
+        } catch (IllegalAccessException e) {
+        } catch (InvocationTargetException e) {
+        } catch (NoSuchMethodException e) {
+        } catch (InstantiationException e) {
         }
+        throw new UnsupportedOperationException(
+                "Probably new Constructor signatures in newer JDK");
+    }
 
-        return new RadialGradientPaint(paint.getCenterPoint(),
-                paint.getRadius(),
-                paint.getFocusPoint(),
-                paint.getFractions(),
-                paintColours,
-                paint.getCycleMethod(),
-                paint.getColorSpace(),
-                paint.getTransform());
+    /**
+     * Convenience method to invoke the zero argument <code>methodName</code>
+     * method of <code>object</code> via Reflection.
+     *
+     * @param object
+     * @param methodName
+     *
+     * @return the result
+     *
+     * @throws IllegalArgumentException
+     * @throws SecurityException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     */
+    private static final Object invokeZeroArgumentMethod(Object object,
+            String methodName) throws IllegalArgumentException,
+            SecurityException, IllegalAccessException,
+            InvocationTargetException, NoSuchMethodException {
+        return object.getClass().getMethod(methodName,
+                new Class[] {}).invoke(object, new Object[] {});
     }
 
     /**
@@ -222,24 +305,24 @@ public class PaintAlpha {
      * <p>
      * This entails cloning the underlying <code>BufferedImage</code>,
      * then darkening each colour-pixel individually!
-     * 
+     *
      * @param paint a <code>TexturePaint</code>
-     * 
-     * @param ignoreThisDummyArgument which is just to guarantee a unique 
+     *
+     * @param ignoreThisDummyArgument which is just to guarantee a unique
      *        method signature
-     * 
+     *
      * @return a darker version of the <code>TexturePaint</code>
      */
-    private static TexturePaint darker(TexturePaint paint, 
+    private static TexturePaint darker(TexturePaint paint,
             boolean ignoreThisDummyArgument) {
         try {
             return darker(paint);
         }
         catch (Exception e) {
             /*
-             * Lots can go wrong while fiddling with Images, Colour Models 
-             * & such!  If anything at all goes awry, just return the original 
-             * TexturePaint.  (TexturePaint's are immutable anyway, so no harm 
+             * Lots can go wrong while fiddling with Images, Colour Models
+             * & such!  If anything at all goes awry, just return the original
+             * TexturePaint.  (TexturePaint's are immutable anyway, so no harm
              * done)
              */
             return paint;
@@ -251,20 +334,22 @@ public class PaintAlpha {
      * <p>
      * This entails cloning the underlying <code>BufferedImage</code>,
      * then darkening each colour-pixel individually!
-     * 
+     *
      * @param paint a <code>TexturePaint</code>
-     * 
+     *
      * @return a darker version of the <code>TexturePaint</code>
      */
-    public static TexturePaint darker(TexturePaint paint) {
+    private static TexturePaint darker(TexturePaint paint) {
         /**
-         * Colour Models with pre-multiplied Alpha tested OK without any 
+         * Colour Models with pre-multiplied Alpha tested OK without any
          * special logic
-         * 
+         *
          * BufferedImage.TYPE_INT_ARGB_PRE:    // Type 03: tested OK 2011.02.27
          * BufferedImage.TYPE_4BYTE_ABGR_PRE:  // Type 07: tested OK 2011.02.27
          */
-        if (paint.getImage().getColorModel().isAlphaPremultiplied()) {/* Placeholder */}
+        if (paint.getImage().getColorModel().isAlphaPremultiplied()) {
+            /* Placeholder */
+        }
 
         BufferedImage img = cloneImage(paint.getImage());
 
@@ -280,26 +365,26 @@ public class PaintAlpha {
         /* (pix-buffer is large enough for all pixels of one row) */
 
         /**
-         * Indexed Colour Models (sort of a Palette) CANNOT be simply 
+         * Indexed Colour Models (sort of a Palette) CANNOT be simply
          * multiplied (the pixel-value is just an index into the Palette).
-         * 
+         *
          * Fortunately, IndexColorModel.getComponents(..) resolves the colours.
          * The resolved colours can then be multiplied by our FACTOR.
          * IndexColorModel.getDataElement(..) then tries to map the computed
          * colour to the "nearest" in the Palette.
-         * 
-         * It is quite possible that the "nearest" colour is the ORIGINAL 
+         *
+         * It is quite possible that the "nearest" colour is the ORIGINAL
          * colour!  In the worst case, the returned Image will be identical to
          * the original.
-         * 
+         *
          * Applies to following Image Types:
-         * 
+         *
          * BufferedImage.TYPE_BYTE_BINARY:    // Type 12: tested OK 2011.02.27
          * BufferedImage.TYPE_BYTE_INDEXED:   // Type 13: tested OK 2011.02.27
          */
         if (img.getColorModel() instanceof IndexColorModel) {
 
-            int[] nco = new int[4]; // RGB (+ optional Alpha which we leave 
+            int[] nco = new int[4]; // RGB (+ optional Alpha which we leave
                                     // unchanged)
 
             for (int y = miY; y < maY; y++)  {
@@ -310,7 +395,7 @@ public class PaintAlpha {
                     nco    =  img.getColorModel().getComponents(pix[p], nco, 0);
                     nco[0] *= FACTOR; // Red
                     nco[1] *= FACTOR; // Green
-                    nco[2] *= FACTOR; // Blue. Now map computed colour to 
+                    nco[2] *= FACTOR; // Blue. Now map computed colour to
                                       // nearest in Palette...
                     pix[p] = img.getColorModel().getDataElement(nco, 0);
                 }
@@ -328,7 +413,7 @@ public class PaintAlpha {
          */
         if (img.getSampleModel().getNumBands() == 4) {
             /**
-             * The following Image Types have an Alpha-channel which we will 
+             * The following Image Types have an Alpha-channel which we will
              * leave unchanged:
              *
              * BufferedImage.TYPE_INT_ARGB:   // Type 02: tested OK 2011.02.27
@@ -363,7 +448,7 @@ public class PaintAlpha {
             /**
              * Above, we multiplied every pixel by our FACTOR because the
              * applicable Image Types consist only of colour or grey channels:
-             * 
+             *
              * BufferedImage.TYPE_INT_RGB:        // Type 01: tested OK 2011.02.27
              * BufferedImage.TYPE_INT_BGR:        // Type 04: tested OK 2011.02.27
              * BufferedImage.TYPE_3BYTE_BGR:      // Type 05: tested OK 2011.02.27
@@ -371,7 +456,7 @@ public class PaintAlpha {
              * BufferedImage.TYPE_USHORT_GRAY:    // Type 11: tested OK 2011.02.27
              * BufferedImage.TYPE_USHORT_565_RGB: // Type 08: tested OK 2011.02.27
              * BufferedImage.TYPE_USHORT_555_RGB: // Type 09: tested OK 2011.02.27
-             * 
+             *
              * Note: as ras.getPixels(..) returned colours in the order R, G, B, A (optional)
              * for both TYPE_4BYTE_ABGR & TYPE_3BYTE_BGR,
              * it is assumed that TYPE_INT_BGR will behave similarly.
@@ -382,17 +467,17 @@ public class PaintAlpha {
     /**
      * Clone a {@link BufferedImage}.
      * <p>
-     * Note: when constructing the clone, the original Colour Model Object is 
-     * reused.<br>  That keeps things simple & should not be a problem, as all 
+     * Note: when constructing the clone, the original Colour Model Object is
+     * reused.<br>  That keeps things simple & should not be a problem, as all
      * known Colour Models<br>
      * ({@link java.awt.image.IndexColorModel     IndexColorModel},
      *  {@link java.awt.image.DirectColorModel    DirectColorModel},
-     *  {@link java.awt.image.ComponentColorModel ComponentColorModel}) are 
+     *  {@link java.awt.image.ComponentColorModel ComponentColorModel}) are
      * immutable.
-     * 
+     *
      * @param image original BufferedImage to clone
-     * 
-     * @return a new BufferedImage reusing the original's Colour Model & 
+     *
+     * @return a new BufferedImage reusing the original's Colour Model &
      *         containing a clone of its pixels
      */
     public static BufferedImage cloneImage(BufferedImage image) {
@@ -403,8 +488,9 @@ public class PaintAlpha {
 
         /*
          * Buffered Images may have properties, but NEVER disclose them!
-         * Nevertheless, just in case someone implements getPropertyNames() one day...
-         */        
+         * Nevertheless, just in case someone implements getPropertyNames()
+         * one day...
+         */
         Hashtable props = null;
         String[] propNames = image.getPropertyNames();
         if (propNames != null) { // ALWAYS null
@@ -412,8 +498,8 @@ public class PaintAlpha {
             for (int i = 0; i < propNames.length; i++) {
                 props.put(propNames[i], image.getProperty(propNames[i]));
             }
-        } 
-        return new BufferedImage(image.getColorModel(), ras, 
+        }
+        return new BufferedImage(image.getColorModel(), ras,
                 image.isAlphaPremultiplied(), props);
     }
 }
