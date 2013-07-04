b53b526a22a17d229d7071835ebb2d45ba6e785b
David Gilbert <jfree@users.noreply.github.com>
2013-07-04 12:18:03 +0000

    Update copyright notice.

--- tests/org/jfree/data/xy/junit/YWithXIntervalTests.java
+++ tests/org/jfree/data/xy/junit/YWithXIntervalTests.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2011, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2013, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
