39f2449785cf8abde1a184f08c93bb401251fee4
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2013-08-02 06:21:11 +0000

    Use fail().

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/plot/junit/SpiderWebPlotTests.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/chart/plot/junit/SpiderWebPlotTests.java
@@ -358,19 +358,16 @@ public class SpiderWebPlotTests extends TestCase {
         dataset.addValue(25.0, "S1", "C5");
         SpiderWebPlot plot = new SpiderWebPlot(dataset);
         JFreeChart chart = new JFreeChart(plot);
-        boolean success = false;
         try {
             BufferedImage image = new BufferedImage(200 , 100,
                     BufferedImage.TYPE_INT_RGB);
             Graphics2D g2 = image.createGraphics();
             chart.draw(g2, new Rectangle2D.Double(0, 0, 200, 100), null, null);
             g2.dispose();
-            success = true;
         }
         catch (Exception e) {
-            success = false;
+            fail("There should be no exception.");
         }
-        assertTrue(success);
     }
 
     /**
