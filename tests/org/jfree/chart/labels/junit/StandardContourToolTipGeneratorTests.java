28d0dc6aae74076f2dfda68637ae69f78086db0f
David Gilbert <jfree@users.noreply.github.com>
2013-08-02 04:46:44 +0000

    Remove System.err.println.

--- tests/org/jfree/chart/labels/junit/StandardContourToolTipGeneratorTests.java
+++ tests/org/jfree/chart/labels/junit/StandardContourToolTipGeneratorTests.java
@@ -42,6 +42,7 @@ package org.jfree.chart.labels.junit;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
@@ -79,29 +80,21 @@ public class StandardContourToolTipGeneratorTests extends TestCase {
     /**
      * Serialize an instance, restore it, and check for equality.
      */
-    public void testSerialization() {
-
+    public void testSerialization() throws IOException, ClassNotFoundException {
         StandardContourToolTipGenerator g1
             = new StandardContourToolTipGenerator();
         StandardContourToolTipGenerator g2 = null;
 
-        try {
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-            ObjectOutput out = new ObjectOutputStream(buffer);
-            out.writeObject(g1);
-            out.close();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ObjectOutput out = new ObjectOutputStream(buffer);
+        out.writeObject(g1);
+        out.close();
 
-            ObjectInput in = new ObjectInputStream(
-                new ByteArrayInputStream(buffer.toByteArray())
-            );
-            g2 = (StandardContourToolTipGenerator) in.readObject();
-            in.close();
-        }
-        catch (Exception e) {
-            System.out.println(e.toString());
-        }
+        ObjectInput in = new ObjectInputStream(new ByteArrayInputStream(
+                buffer.toByteArray()));
+        g2 = (StandardContourToolTipGenerator) in.readObject();
+        in.close();
         assertEquals(g1, g2);
-
     }
 
 }
