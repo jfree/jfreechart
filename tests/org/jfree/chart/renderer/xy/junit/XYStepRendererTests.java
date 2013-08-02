20a10938c573c674e87bd9e8fc964b49baa6eb05
David Gilbert <jfree@users.noreply.github.com>
2013-08-02 05:19:34 +0000

    Remove System.err.println.

--- tests/org/jfree/chart/renderer/xy/junit/XYStepRendererTests.java
+++ tests/org/jfree/chart/renderer/xy/junit/XYStepRendererTests.java
@@ -121,15 +121,9 @@ public class XYStepRendererTests extends TestCase {
     /**
      * Confirm that cloning works.
      */
-    public void testCloning() {
+    public void testCloning() throws CloneNotSupportedException {
         XYStepRenderer r1 = new XYStepRenderer();
-        XYStepRenderer r2 = null;
-        try {
-            r2 = (XYStepRenderer) r1.clone();
-        }
-        catch (CloneNotSupportedException e) {
-            System.err.println("Failed to clone.");
-        }
+        XYStepRenderer r2 = (XYStepRenderer) r1.clone();
         assertTrue(r1 != r2);
         assertTrue(r1.getClass() == r2.getClass());
         assertTrue(r1.equals(r2));
