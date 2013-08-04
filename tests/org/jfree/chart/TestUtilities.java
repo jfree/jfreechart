e9a84f72c840264a6893590518b4326b6a23a509
David Gilbert <jfree@users.noreply.github.com>
2013-08-04 05:17:20 +0000

    Added serialised() method to serialise and deserialise an object.

--- tests/org/jfree/chart/TestUtilities.java
+++ tests/org/jfree/chart/TestUtilities.java
@@ -40,6 +40,13 @@
 
 package org.jfree.chart;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -68,4 +75,24 @@ public class TestUtilities {
         return false;
     }
 
+    public static Object serialised(Object original) {
+        Object result = null;
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ObjectOutput out;
+        try {
+            out = new ObjectOutputStream(buffer);
+            out.writeObject(original);
+            out.close();
+            ObjectInput in = new ObjectInputStream(
+                    new ByteArrayInputStream(buffer.toByteArray()));
+            result = in.readObject();
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+    
 }
