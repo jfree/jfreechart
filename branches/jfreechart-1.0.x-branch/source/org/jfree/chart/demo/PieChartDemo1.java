f60b8feedde49d7117eb048890b75dfdad5ac4c4
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2013-09-12 15:16:57 +0000

    Update to use real data and a new style.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/demo/PieChartDemo1.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/demo/PieChartDemo1.java
@@ -27,7 +27,7 @@
  * ------------------
  * PieChartDemo1.java
  * ------------------
- * (C) Copyright 2003-2011, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2003-2013, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   ;
@@ -41,6 +41,14 @@
 
 package org.jfree.chart.demo;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Point;
+import java.awt.RadialGradientPaint;
+import java.awt.geom.Point2D;
 import javax.swing.JPanel;
 
 import org.jfree.chart.ChartFactory;
@@ -48,9 +56,13 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.HorizontalAlignment;
+import org.jfree.ui.RectangleEdge;
+import org.jfree.ui.RectangleInsets;
 import org.jfree.ui.RefineryUtilities;
 
 /**
@@ -80,17 +92,17 @@ public class PieChartDemo1 extends ApplicationFrame {
 
     /**
      * Creates a sample dataset.
+     * 
+     * Source: http://www.bbc.co.uk/news/business-15489523
      *
      * @return A sample dataset.
      */
     private static PieDataset createDataset() {
         DefaultPieDataset dataset = new DefaultPieDataset();
-        dataset.setValue("One", new Double(43.2));
-        dataset.setValue("Two", new Double(10.0));
-        dataset.setValue("Three", new Double(27.5));
-        dataset.setValue("Four", new Double(17.5));
-        dataset.setValue("Five", new Double(11.0));
-        dataset.setValue("Six", new Double(19.4));
+        dataset.setValue("Samsung", new Double(27.8));
+        dataset.setValue("Others", new Double(55.3));
+        dataset.setValue("Nokia", new Double(16.8));
+        dataset.setValue("Apple", new Double(17.1));
         return dataset;
     }
 
@@ -104,18 +116,70 @@ public class PieChartDemo1 extends ApplicationFrame {
     private static JFreeChart createChart(PieDataset dataset) {
 
         JFreeChart chart = ChartFactory.createPieChart(
-            "Pie Chart Demo 1",  // chart title
-            dataset,             // data
-            true,                // include legend
-            true,
-            false
+            "Smart Phones Manufactured / Q3 2011",  // chart title
+            dataset,            // data
+            false,              // no legend
+            true,               // tooltips
+            false               // no URL generation
         );
 
+        // set a custom background for the chart
+        chart.setBackgroundPaint(new GradientPaint(new Point(0, 0), 
+                new Color(20, 20, 20), new Point(400, 200), Color.DARK_GRAY));
+
+        // customise the title position and font
+        TextTitle t = chart.getTitle();
+        t.setHorizontalAlignment(HorizontalAlignment.LEFT);
+        t.setPaint(new Color(240, 240, 240));
+        t.setFont(new Font("Arial", Font.BOLD, 26));
+
         PiePlot plot = (PiePlot) chart.getPlot();
-        plot.setSectionOutlinesVisible(false);
-        plot.setNoDataMessage("No data available");
+        plot.setBackgroundPaint(null);
+        plot.setInteriorGap(0.04);
+        plot.setOutlineVisible(false);
+
+        // use gradients and white borders for the section colours
+        plot.setSectionPaint("Others", createGradientPaint(new Color(200, 200, 255), Color.BLUE));
+        plot.setSectionPaint("Samsung", createGradientPaint(new Color(255, 200, 200), Color.RED));
+        plot.setSectionPaint("Apple", createGradientPaint(new Color(200, 255, 200), Color.GREEN));
+        plot.setSectionPaint("Nokia", createGradientPaint(new Color(200, 255, 200), Color.YELLOW));
+        plot.setBaseSectionOutlinePaint(Color.WHITE);
+        plot.setSectionOutlinesVisible(true);
+        plot.setBaseSectionOutlineStroke(new BasicStroke(2.0f));
+
+        // customise the section label appearance
+        plot.setLabelFont(new Font("Courier New", Font.BOLD, 20));
+        plot.setLabelLinkPaint(Color.WHITE);
+        plot.setLabelLinkStroke(new BasicStroke(2.0f));
+        plot.setLabelOutlineStroke(null);
+        plot.setLabelPaint(Color.WHITE);
+        plot.setLabelBackgroundPaint(null);
+        
+        // add a subtitle giving the data source
+        TextTitle source = new TextTitle("Source: http://www.bbc.co.uk/news/business-15489523", 
+                new Font("Courier New", Font.PLAIN, 12));
+        source.setPaint(Color.WHITE);
+        source.setPosition(RectangleEdge.BOTTOM);
+        source.setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        chart.addSubtitle(source);
         return chart;
 
+    }
+
+    /**
+     * A utility method for creating gradient paints.
+     * 
+     * @param c1  color 1.
+     * @param c2  color 2.
+     * 
+     * @return A radial gradient paint.
+     */
+    private static RadialGradientPaint createGradientPaint(Color c1, Color c2) {
+        Point2D center = new Point2D.Float(0, 0);
+        float radius = 200;
+        float[] dist = {0.0f, 1.0f};
+        return new RadialGradientPaint(center, radius, dist,
+                new Color[] {c1, c2});
     }
 
     /**
@@ -125,8 +189,10 @@ public class PieChartDemo1 extends ApplicationFrame {
      */
     public static JPanel createDemoPanel() {
         JFreeChart chart = createChart(createDataset());
+        chart.setPadding(new RectangleInsets(4, 8, 2, 2));
         ChartPanel panel = new ChartPanel(chart);
         panel.setMouseWheelEnabled(true);
+        panel.setPreferredSize(new Dimension(600, 300));
         return panel;
     }
 
@@ -145,10 +211,11 @@ public class PieChartDemo1 extends ApplicationFrame {
         //
         // ******************************************************************
 
-        PieChartDemo1 demo = new PieChartDemo1("Pie Chart Demo 1");
+        PieChartDemo1 demo = new PieChartDemo1("JFreeChart: Pie Chart Demo 1");
         demo.pack();
         RefineryUtilities.centerFrameOnScreen(demo);
         demo.setVisible(true);
     }
 
 }
+
