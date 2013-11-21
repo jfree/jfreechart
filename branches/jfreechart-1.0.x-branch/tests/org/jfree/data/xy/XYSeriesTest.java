912a57ae870f22656260285c0df1a7d74765ebd7
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2013-11-21 08:41:51 +0000

    New test.

--- branches/jfreechart-1.0.x-branch/tests/org/jfree/data/xy/XYSeriesTest.java
+++ branches/jfreechart-1.0.x-branch/tests/org/jfree/data/xy/XYSeriesTest.java
@@ -48,11 +48,12 @@ package org.jfree.data.xy;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
 
 import org.jfree.chart.TestUtilities;
 
 import org.jfree.data.general.SeriesException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
@@ -772,4 +773,31 @@ public class XYSeriesTest {
         assertEquals(2.0, s1.getMaxY(), EPSILON);
     }
 
+    @Test
+    public void testSetKey() {
+        XYSeries s1 = new XYSeries("S");
+        s1.setKey("S1");
+        assertEquals("S1", s1.getKey());
+        
+        XYSeriesCollection c = new XYSeriesCollection();
+        c.addSeries(s1);
+        XYSeries s2 = new XYSeries("S2");
+        c.addSeries(s2);
+        
+        // now we should be allowed to change s1's key to anything but "S2"
+        s1.setKey("OK");
+        assertEquals("OK", s1.getKey());
+        
+        try {
+            s1.setKey("S2");
+            fail("Expect an exception here.");
+        } catch (IllegalArgumentException e) {
+            // OK
+        }
+        
+        // after s1 is removed from the collection, we should be able to set
+        // the key to anything we want...
+        c.removeSeries(s1);
+        s1.setKey("S2");
+    }
 }
