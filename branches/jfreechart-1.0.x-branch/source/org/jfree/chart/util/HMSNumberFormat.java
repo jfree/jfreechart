951c2c34c1b05f819c1710e3ae4ec8df63145d9b
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2013-11-21 06:31:12 +0000

    2013-11-21  David Gilbert  <david.gilbert@object-refinery.com>
        * org/jfree/chart/utils/HMSNumberFormat.java: New class.

---
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/util/HMSNumberFormat.java
@@ -0,0 +1,120 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2013, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * --------------------
+ * HMSNumberFormat.java
+ * --------------------
+ * (C) Copyright 2013, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes:
+ * --------
+ * 28-Sep-2013 : Version 1 (RW);
+ *
+ */
+
+package org.jfree.chart.util;
+
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+
+/**
+ * A custom number formatter that formats numbers (in seconds) as HH:MM:SS.
+ * Created in response to:
+ * 
+ * http://stackoverflow.com/questions/19028908/jfreechart-need-to-customize-y-axis-just-for-printing
+ * 
+ * @since 1.0.17
+ */
+public class HMSNumberFormat extends NumberFormat {
+
+    private NumberFormat formatter = new DecimalFormat("00");
+    
+    /**
+     * Creates a new instance.
+     */
+    public HMSNumberFormat() {
+        // nothing to do
+    }
+
+    /**
+     * Formats the specified number as a string of the form HH:MM:SS.  The 
+     * decimal fraction is ignored.
+     *
+     * @param number  the number to format.
+     * @param toAppendTo  the buffer to append to (ignored here).
+     * @param pos  the field position (ignored here).
+     *
+     * @return The string buffer.
+     */
+    @Override
+    public StringBuffer format(double number, StringBuffer toAppendTo,
+            FieldPosition pos) {
+        return format((long) number, toAppendTo, pos);
+    }
+
+    /**
+     * Formats the specified number as a string of the form HH:MM:SS.
+     *
+     * @param number  the number to format.
+     * @param toAppendTo  the buffer to append to (ignored here).
+     * @param pos  the field position (ignored here).
+     *
+     * @return The string buffer.
+     */
+    @Override
+    public StringBuffer format(long number, StringBuffer toAppendTo,
+            FieldPosition pos) {
+        StringBuffer sb = new StringBuffer();
+        long hours = number / 3600;
+        sb.append(this.formatter.format(hours)).append(":");
+        long remaining = number - (hours * 3600);
+        long minutes = remaining / 60;
+        sb.append(this.formatter.format(minutes)).append(":");
+        long seconds = remaining - (minutes * 60);
+        sb.append(this.formatter.format(seconds));
+        return sb;
+    }
+
+    /**
+     * Parsing is not implemented, so this method always returns
+     * <code>null</code>.
+     *
+     * @param source  ignored.
+     * @param parsePosition  ignored.
+     *
+     * @return Always <code>null</code>.
+     */
+    @Override
+    public Number parse (String source, ParsePosition parsePosition) {
+        return null; // don't bother with parsing
+    }
+
+}
