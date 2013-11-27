21a7b0d0b365ff4fd34a9b9d54daac09cbe14f47
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2013-11-27 03:15:11 +0000

    Update version number so I don't forget for the next release.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/resources/JFreeChartResources.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/resources/JFreeChartResources.java
@@ -56,7 +56,7 @@ public class JFreeChartResources extends ListResourceBundle {
     /** The resources to be localised. */
     private static final Object[][] CONTENTS = {
         {"project.name",      "JFreeChart"},
-        {"project.version",   "1.0.17"},
+        {"project.version",   "1.0.18"},
         {"project.info",      "http://www.jfree.org/jfreechart/index.html"},
         {"project.copyright",
          "(C)opyright 2000-2013, by Object Refinery Limited and Contributors"}
