a3b42733b4353ff75c5aefe4b9c1d02ec18126c4
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2014-02-23 08:22:56 +0000

    2014-02-23  David Gilbert  <david.gilbert@object-refinery.com>
        * org/jfree/data/Range.java
        (isNaNRange): New method.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/data/Range.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/data/Range.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2013, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ----------
  * Range.java
  * ----------
- * (C) Copyright 2002-2013, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2002-2014, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   Chuanhao Chiu;
@@ -57,7 +57,8 @@
  * 18-Dec-2007 : New methods intersects(Range) and scale(...) thanks to Sergei
  *               Ivanov (DG);
  * 08-Jan-2012 : New method combineIgnoringNaN() (DG);
- *
+ * 23-Feb-2014 : Added isNaNRange() method (DG);
+ * 
  */
 
 package org.jfree.data;
@@ -225,7 +226,9 @@ public strictfp class Range implements Serializable {
     }
 
     /**
-     * Combines two ranges.  This method has a special handling for Double.NaN.
+     * Returns a new range that spans both <code>range1</code> and 
+     * <code>range2</code>.  This method has a special handling to ignore
+     * Double.NaN values.
      *
      * @param range1  the first range (<code>null</code> permitted).
      * @param range2  the second range (<code>null</code> permitted).
@@ -245,7 +248,7 @@ public strictfp class Range implements Serializable {
         double u = max(range1.getUpperBound(), range2.getUpperBound());
         return new Range(l, u);
     }
-
+    
     private static double min(double d1, double d2) {
         if (Double.isNaN(d1)) {
             return d2;
@@ -415,6 +418,18 @@ public strictfp class Range implements Serializable {
         return true;
     }
 
+    /**
+     * Returns <code>true</code> if both the lower and upper bounds are 
+     * <code>Double.NaN</code>, and <code>false</code> otherwise.
+     * 
+     * @return A boolean.
+     * 
+     * @since 1.0.18
+     */
+    public boolean isNaNRange() {
+        return Double.isNaN(this.lower) && Double.isNaN(this.upper);
+    }
+    
     /**
      * Returns a hash code.
      *
