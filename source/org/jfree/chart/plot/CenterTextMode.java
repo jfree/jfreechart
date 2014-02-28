5de61f81d4329d1a8cc06eece16e4fc6f3fc7dff
David Gilbert <jfree@users.noreply.github.com>
2014-02-28 11:50:27 +0000

    Added CenterTextMode enum.

---
+++ source/org/jfree/chart/plot/CenterTextMode.java
@@ -0,0 +1,59 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.]
+ *
+ * -------------------
+ * CenterTextMode.java
+ * -------------------
+ * (C) Copyright 2014, by Object Refinery Limited.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 28-Feb-2014 : Version 1 (DG);
+ * 
+ */
+
+package org.jfree.chart.plot;
+
+/**
+ * The mode for the center text on a {@link RingPlot}.
+ * 
+ * @since 1.0.18
+ */
+public enum CenterTextMode {
+    
+    /** A fixed text item */
+    FIXED,
+    
+    /** A value item (taken from the first item in the dataset). */
+    VALUE,
+    
+    /** No center text. */
+    NONE
+    
+}
