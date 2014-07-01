d7bada4f237472b9f55b21592dafcc2c7bc739a0
mungady <mungady@4df5dd95-b682-48b0-b31b-748e37b65e73>
2014-07-01 10:21:57 +0000

    Fix Javadoc warnings.

--- branches/jfreechart-1.0.x-branch/source/org/jfree/chart/plot/Crosshair.java
+++ branches/jfreechart-1.0.x-branch/source/org/jfree/chart/plot/Crosshair.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2013, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * --------------
  * Crosshair.java
  * --------------
- * (C) Copyright 2009-2013, by Object Refinery Limited.
+ * (C) Copyright 2009-2014, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -657,7 +657,8 @@ public class Crosshair implements Cloneable, PublicCloneable, Serializable {
      *
      * @return An independent copy of this instance.
      *
-     * @throws java.lang.CloneNotSupportedException
+     * @throws java.lang.CloneNotSupportedException if there is a problem with
+     *         cloning.
      */
     @Override
     public Object clone() throws CloneNotSupportedException {
