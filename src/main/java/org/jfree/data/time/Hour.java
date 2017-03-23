6e4344fc60345d66d780c59046e9fdd68bfd2c0a
David Gilbert <dave@jfree.org>
2017-03-23 07:30:59 +0100

    Fix Javadoc warnings.

--- src/main/java/org/jfree/data/time/Hour.java
+++ src/main/java/org/jfree/data/time/Hour.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2016, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2017, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -138,7 +138,7 @@ public class Hour extends RegularTimePeriod implements Serializable {
      *
      * @param time  the date-time ({@code null} not permitted).
      *
-     * @see #Hour(Date, TimeZone)
+     * @see #Hour(Date, TimeZone, Locale)
      */
     public Hour(Date time) {
         // defer argument checking...
