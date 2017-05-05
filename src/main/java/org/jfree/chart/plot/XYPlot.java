afff21e5cab96c35c0cad31b9256edbc30ae6aa1
David Gilbert <dave@jfree.org>
2017-05-05 19:40:52 +0200

    Update constants.

--- src/main/java/org/jfree/chart/plot/XYPlot.java
+++ src/main/java/org/jfree/chart/plot/XYPlot.java
@@ -328,7 +328,7 @@ public class XYPlot extends Plot implements ValueAxisPlot, Pannable, Zoomable,
             new float[] {2.0f, 2.0f}, 0.0f);
 
     /** The default grid line paint. */
-    public static final Paint DEFAULT_GRIDLINE_PAINT = Color.lightGray;
+    public static final Paint DEFAULT_GRIDLINE_PAINT = Color.LIGHT_GRAY;
 
     /** The default crosshair visibility. */
     public static final boolean DEFAULT_CROSSHAIR_VISIBLE = false;
@@ -338,7 +338,7 @@ public class XYPlot extends Plot implements ValueAxisPlot, Pannable, Zoomable,
             = DEFAULT_GRIDLINE_STROKE;
 
     /** The default crosshair paint. */
-    public static final Paint DEFAULT_CROSSHAIR_PAINT = Color.blue;
+    public static final Paint DEFAULT_CROSSHAIR_PAINT = Color.BLUE;
 
     /** The resourceBundle for the localization. */
     protected static ResourceBundle localizationResources
@@ -687,10 +687,10 @@ public class XYPlot extends Plot implements ValueAxisPlot, Pannable, Zoomable,
 
         this.domainMinorGridlinesVisible = false;
         this.domainMinorGridlineStroke = DEFAULT_GRIDLINE_STROKE;
-        this.domainMinorGridlinePaint = Color.white;
+        this.domainMinorGridlinePaint = Color.WHITE;
 
         this.domainZeroBaselineVisible = false;
-        this.domainZeroBaselinePaint = Color.black;
+        this.domainZeroBaselinePaint = Color.BLACK;
         this.domainZeroBaselineStroke = new BasicStroke(0.5f);
 
         this.rangeGridlinesVisible = true;
@@ -699,10 +699,10 @@ public class XYPlot extends Plot implements ValueAxisPlot, Pannable, Zoomable,
 
         this.rangeMinorGridlinesVisible = false;
         this.rangeMinorGridlineStroke = DEFAULT_GRIDLINE_STROKE;
-        this.rangeMinorGridlinePaint = Color.white;
+        this.rangeMinorGridlinePaint = Color.WHITE;
 
         this.rangeZeroBaselineVisible = false;
-        this.rangeZeroBaselinePaint = Color.black;
+        this.rangeZeroBaselinePaint = Color.BLACK;
         this.rangeZeroBaselineStroke = new BasicStroke(0.5f);
 
         this.domainCrosshairVisible = false;
