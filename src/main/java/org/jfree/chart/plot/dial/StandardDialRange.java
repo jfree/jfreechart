f0ed1864a3f79994c9418c89f60835d799d1de72
David Gilbert <dave@jfree.org>
2017-05-05 20:05:59 +0200

    Update constants.

--- src/main/java/org/jfree/chart/plot/dial/StandardDialRange.java
+++ src/main/java/org/jfree/chart/plot/dial/StandardDialRange.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2016, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2017, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ----------------------
  * StandardDialRange.java
  * ----------------------
- * (C) Copyright 2006-2016, by Object Refinery Limited.
+ * (C) Copyright 2006-2017, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -103,7 +103,7 @@ public class StandardDialRange extends AbstractDialLayer implements DialLayer,
      * Creates a new {@code StandardDialRange} instance.
      */
     public StandardDialRange() {
-        this(0.0, 100.0, Color.white);
+        this(0.0, 100.0, Color.WHITE);
     }
 
     /**
