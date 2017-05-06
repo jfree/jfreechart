3c26b680c5c9a83cc495e5d1ec8a2d5688f2a1dc
David Gilbert <dave@jfree.org>
2017-05-06 10:22:36 +0200

    Rename test file.

--- src/test/java/org/jfree/chart/HashUtilsTest.java
+++ src/test/java/org/jfree/chart/HashUtilsTest.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2016, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2017, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -24,10 +24,10 @@
  * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
  * Other names may be trademarks of their respective owners.]
  *
- * ----------------------
- * HashUtilitiesTest.java
- * ----------------------
- * (C) Copyright 2004-2016, by Object Refinery Limited and Contributors.
+ * ------------------
+ * HashUtilsTest.java
+ * ------------------
+ * (C) Copyright 2004-2017, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -48,7 +48,7 @@ import static org.junit.Assert.assertFalse;
 /**
  * Tests for the {@link HashUtils} class.
  */
-public class HashUtilitiesTest  {
+public class HashUtilsTest  {
 
     /**
      * Some sanity checks for the hashCodeForDoubleArray() method.
