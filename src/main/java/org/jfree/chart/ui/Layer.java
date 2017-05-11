e8f3e13e5698ede31d0a17105e400bc1ab9cb918
David Gilbert <dave@jfree.org>
2017-05-11 22:21:59 +0200

    Remove final keywords.

--- src/main/java/org/jfree/chart/ui/Layer.java
+++ src/main/java/org/jfree/chart/ui/Layer.java
@@ -1,3 +1,31 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2017, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ */
+
 package org.jfree.chart.ui;
 
 import java.io.ObjectStreamException;
@@ -25,7 +53,7 @@ public final class Layer implements Serializable {
      *
      * @param name  the name.
      */
-    private Layer(final String name) {
+    private Layer(String name) {
         this.name = name;
     }
 
@@ -34,19 +62,20 @@ public final class Layer implements Serializable {
      *
      * @return The string.
      */
+    @Override
     public String toString() {
         return this.name;
     }
 
     /**
-     * Returns <code>true</code> if this object is equal to the specified 
-     * object, and <code>false</code> otherwise.
+     * Returns {@code true} if this object is equal to the specified 
+     * object, and {@code false} otherwise.
      *
      * @param o  the other object.
      *
      * @return A boolean.
      */
-    public boolean equals(final Object o) {
+    public boolean equals(Object o) {
 
         if (this == o) {
             return true;
@@ -55,7 +84,7 @@ public final class Layer implements Serializable {
             return false;
         }
 
-        final Layer layer = (Layer) o;
+        Layer layer = (Layer) o;
         if (!this.name.equals(layer.name)) {
             return false;
         }
@@ -69,6 +98,7 @@ public final class Layer implements Serializable {
      *
      * @return the hashcode
      */
+    @Override
     public int hashCode() {
         return this.name.hashCode();
     }
