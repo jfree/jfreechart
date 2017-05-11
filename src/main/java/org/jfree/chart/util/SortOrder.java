4544ff3aceea4d094516607e1b06f54e0eeae86f
David Gilbert <dave@jfree.org>
2017-05-11 23:02:49 +0200

    Remove final keywords.

--- src/main/java/org/jfree/chart/util/SortOrder.java
+++ src/main/java/org/jfree/chart/util/SortOrder.java
@@ -1,3 +1,31 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2017, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ */
+
 package org.jfree.chart.util;
 
 import java.io.ObjectStreamException;
@@ -27,7 +55,7 @@ public final class SortOrder implements Serializable {
      *
      * @param name  the name.
      */
-    private SortOrder(final String name) {
+    private SortOrder(String name) {
         this.name = name;
     }
 
@@ -36,19 +64,21 @@ public final class SortOrder implements Serializable {
      *
      * @return The string.
      */
+    @Override
     public String toString() {
         return this.name;
     }
 
     /**
-     * Returns <code>true</code> if this object is equal to the specified 
-     * object, and <code>false</code> otherwise.
+     * Returns {@code true} if this object is equal to the specified 
+     * object, and {@code false} otherwise.
      *
      * @param obj  the other object.
      *
      * @return A boolean.
      */
-    public boolean equals(final Object obj) {
+    @Override
+    public boolean equals(Object obj) {
 
         if (this == obj) {
             return true;
@@ -57,7 +87,7 @@ public final class SortOrder implements Serializable {
             return false;
         }
 
-        final SortOrder that = (SortOrder) obj;
+        SortOrder that = (SortOrder) obj;
         if (!this.name.equals(that.toString())) {
             return false;
         }
@@ -70,6 +100,7 @@ public final class SortOrder implements Serializable {
      *
      * @return The hashcode
      */
+    @Override
     public int hashCode() {
         return this.name.hashCode();
     }
