41bdd4ff7c567f1d044439b44be8f8c35dde3da0
David Gilbert <dave@jfree.org>
2017-05-11 22:19:52 +0200

    Javadoc tags.

--- src/main/java/org/jfree/chart/ui/LengthAdjustmentType.java
+++ src/main/java/org/jfree/chart/ui/LengthAdjustmentType.java
@@ -1,3 +1,31 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2017, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ */
+
 package org.jfree.chart.ui;
 
 import java.io.ObjectStreamException;
@@ -32,7 +60,7 @@ public final class LengthAdjustmentType implements Serializable {
      *
      * @param name  the name.
      */
-    private LengthAdjustmentType(final String name) {
+    private LengthAdjustmentType(String name) {
         this.name = name;
     }
 
@@ -41,6 +69,7 @@ public final class LengthAdjustmentType implements Serializable {
      *
      * @return The string.
      */
+    @Override
     public String toString() {
         return this.name;
     }
@@ -53,7 +82,8 @@ public final class LengthAdjustmentType implements Serializable {
      *
      * @return A boolean.
      */
-    public boolean equals(final Object obj) {
+    @Override
+    public boolean equals(Object obj) {
         if (obj == this) {
             return true;
         }
@@ -72,6 +102,7 @@ public final class LengthAdjustmentType implements Serializable {
      *
      * @return The hashcode
      */
+    @Override
     public int hashCode() {
         return this.name.hashCode();
     }
