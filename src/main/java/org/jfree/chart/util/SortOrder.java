a0238d176ac452dcf2e3eaf432011519f2f01a8a
David Gilbert <dave@jfree.org>
2017-05-11 23:15:53 +0200

    Convert to enum

--- src/main/java/org/jfree/chart/util/SortOrder.java
+++ src/main/java/org/jfree/chart/util/SortOrder.java
@@ -28,27 +28,19 @@
 
 package org.jfree.chart.util;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-
 /**
  * Defines tokens used to indicate sorting order (ascending or descending).
  */
-public final class SortOrder implements Serializable {
+public enum SortOrder {
 
-    /** For serialization. */
-    private static final long serialVersionUID = -2124469847758108312L;
-    
     /** Ascending order. */
-    public static final SortOrder ASCENDING 
-        = new SortOrder("SortOrder.ASCENDING");
+    ASCENDING("SortOrder.ASCENDING"),
 
     /** Descending order. */
-    public static final SortOrder DESCENDING 
-        = new SortOrder("SortOrder.DESCENDING");
+    DESCENDING("SortOrder.DESCENDING");
 
     /** The name. */
-    private String name;
+    private final String name;
 
     /**
      * Private constructor.
@@ -69,57 +61,4 @@ public final class SortOrder implements Serializable {
         return this.name;
     }
 
-    /**
-     * Returns {@code true} if this object is equal to the specified 
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the other object.
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof SortOrder)) {
-            return false;
-        }
-
-        SortOrder that = (SortOrder) obj;
-        if (!this.name.equals(that.toString())) {
-            return false;
-        }
-
-        return true;
-    }
-    
-    /**
-     * Returns a hash code value for the object.
-     *
-     * @return The hashcode
-     */
-    @Override
-    public int hashCode() {
-        return this.name.hashCode();
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     * 
-     * @return The object.
-     * 
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(SortOrder.ASCENDING)) {
-            return SortOrder.ASCENDING;
-        }
-        else if (this.equals(SortOrder.DESCENDING)) {
-            return SortOrder.DESCENDING;
-        }
-        return null;
-    }
 }
-
