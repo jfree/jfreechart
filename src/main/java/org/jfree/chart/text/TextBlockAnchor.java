c222169478e3676c81be6af15811ff5e263a7892
David Gilbert <dave@jfree.org>
2017-05-11 21:04:10 +0200

    Javadoc tags.

--- src/main/java/org/jfree/chart/text/TextBlockAnchor.java
+++ src/main/java/org/jfree/chart/text/TextBlockAnchor.java
@@ -101,8 +101,8 @@ public final class TextBlockAnchor implements Serializable {
     }
 
     /**
-     * Returns <code>true</code> if this object is equal to the specified 
-     * object, and <code>false</code> otherwise.
+     * Returns {@code true} if this object is equal to the specified 
+     * object, and {@code false} otherwise.
      *
      * @param o  the other object.
      *
