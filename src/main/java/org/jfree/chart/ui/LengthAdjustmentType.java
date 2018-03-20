aef90f405931edd39dcba06ddd5af9a6b83b7a44
Simon Legner <Simon.Legner@gmail.com>
2018-03-20 12:04:31 +0100

    LengthAdjustmentType as enum

--- src/main/java/org/jfree/chart/ui/LengthAdjustmentType.java
+++ src/main/java/org/jfree/chart/ui/LengthAdjustmentType.java
@@ -28,104 +28,20 @@
 
 package org.jfree.chart.ui;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-
 /**
  * Represents the three options for adjusting a length:  expand, contract, and
  * no change.
  */
-public final class LengthAdjustmentType implements Serializable {
+public enum LengthAdjustmentType {
 
-    /** For serialization. */
-    private static final long serialVersionUID = -6097408511380545010L;
-    
     /** NO_CHANGE. */
-    public static final LengthAdjustmentType NO_CHANGE 
-        = new LengthAdjustmentType("NO_CHANGE");
+    NO_CHANGE,
 
     /** EXPAND. */
-    public static final LengthAdjustmentType EXPAND 
-        = new LengthAdjustmentType("EXPAND");
+    EXPAND,
 
     /** CONTRACT. */
-    public static final LengthAdjustmentType CONTRACT 
-        = new LengthAdjustmentType("CONTRACT");
-
-    /** The name. */
-    private String name;
-
-    /**
-     * Private constructor.
-     *
-     * @param name  the name.
-     */
-    private LengthAdjustmentType(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Returns a string representing the object.
-     *
-     * @return The string.
-     */
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    /**
-     * Returns {@code true} if this object is equal to the specified 
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the other object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof LengthAdjustmentType)) {
-            return false;
-        }
-        final LengthAdjustmentType that = (LengthAdjustmentType) obj;
-        if (!this.name.equals(that.name)) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Returns a hash code value for the object.
-     *
-     * @return The hashcode
-     */
-    @Override
-    public int hashCode() {
-        return this.name.hashCode();
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     * 
-     * @return The object.
-     * 
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(LengthAdjustmentType.NO_CHANGE)) {
-            return LengthAdjustmentType.NO_CHANGE;
-        }
-        else if (this.equals(LengthAdjustmentType.EXPAND)) {
-            return LengthAdjustmentType.EXPAND;
-        }
-        else if (this.equals(LengthAdjustmentType.CONTRACT)) {
-            return LengthAdjustmentType.CONTRACT;
-        }
-        return null;
-    }
+    CONTRACT
     
 }
 
