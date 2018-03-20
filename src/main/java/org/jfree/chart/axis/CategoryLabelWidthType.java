13526fe5797f51cd8e42e5ae652fd6ef713f3dcd
Simon Legner <Simon.Legner@gmail.com>
2018-03-20 11:25:06 +0100

    CategoryLabelWidthType as enum

--- src/main/java/org/jfree/chart/axis/CategoryLabelWidthType.java
+++ src/main/java/org/jfree/chart/axis/CategoryLabelWidthType.java
@@ -41,87 +41,15 @@
 
 package org.jfree.chart.axis;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-import org.jfree.chart.util.Args;
-
 /**
  * Represents the width types for a category label.
  */
-public final class CategoryLabelWidthType implements Serializable {
-
-    /** For serialization. */
-    private static final long serialVersionUID = -6976024792582949656L;
+public enum CategoryLabelWidthType {
 
     /** Percentage of category. */
-    public static final CategoryLabelWidthType CATEGORY 
-            = new CategoryLabelWidthType("CategoryLabelWidthType.CATEGORY");
+    CATEGORY,
 
     /** Percentage of range. */
-    public static final CategoryLabelWidthType RANGE 
-            = new CategoryLabelWidthType("CategoryLabelWidthType.RANGE");
-
-    /** The name. */
-    private String name;
-
-    /**
-     * Private constructor.
-     *
-     * @param name  the name ({@code null} not permitted).
-     */
-    private CategoryLabelWidthType(String name) {
-        Args.nullNotPermitted(name, "name");
-        this.name = name;
-    }
-
-    /**
-     * Returns a string representing the object.
-     *
-     * @return The string (never {@code null}).
-     */
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    /**
-     * Returns {@code true} if this object is equal to the specified
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the other object.
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof CategoryLabelWidthType)) {
-            return false;
-        }
-        CategoryLabelWidthType t = (CategoryLabelWidthType) obj;
-        if (!this.name.equals(t.toString())) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     *
-     * @return The object.
-     *
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(CategoryLabelWidthType.CATEGORY)) {
-            return CategoryLabelWidthType.CATEGORY;
-        }
-        else if (this.equals(CategoryLabelWidthType.RANGE)) {
-            return CategoryLabelWidthType.RANGE;
-        }
-        return null;
-    }
+    RANGE
 
 }
