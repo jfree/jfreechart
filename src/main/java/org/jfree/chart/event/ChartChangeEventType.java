aab850c8934c407a38d47814b08a9ca32d10ac57
Simon Legner <Simon.Legner@gmail.com>
2018-03-20 11:25:37 +0100

    ChartChangeEventType as enum

--- src/main/java/org/jfree/chart/event/ChartChangeEventType.java
+++ src/main/java/org/jfree/chart/event/ChartChangeEventType.java
@@ -40,102 +40,18 @@
 
 package org.jfree.chart.event;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-
 /**
  * Defines tokens used to indicate an event type.
  */
-public final class ChartChangeEventType implements Serializable {
-
-    /** For serialization. */
-    private static final long serialVersionUID = 5481917022435735602L;
+public enum ChartChangeEventType {
 
     /** GENERAL. */
-    public static final ChartChangeEventType GENERAL
-            = new ChartChangeEventType("ChartChangeEventType.GENERAL");
+    GENERAL,
 
     /** NEW_DATASET. */
-    public static final ChartChangeEventType NEW_DATASET
-            = new ChartChangeEventType("ChartChangeEventType.NEW_DATASET");
+    NEW_DATASET,
 
     /** DATASET_UPDATED. */
-    public static final ChartChangeEventType DATASET_UPDATED
-            = new ChartChangeEventType("ChartChangeEventType.DATASET_UPDATED");
-
-    /** The name. */
-    private String name;
-
-    /**
-     * Private constructor.
-     *
-     * @param name  the name.
-     */
-    private ChartChangeEventType(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Returns a string representing the object.
-     *
-     * @return The string.
-     */
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    /**
-     * Returns {@code true} if this object is equal to the specified
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof ChartChangeEventType)) {
-            return false;
-        }
-        ChartChangeEventType that = (ChartChangeEventType) obj;
-        if (!this.name.equals(that.toString())) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Returns a hash code value for the object.
-     *
-     * @return The hashcode
-     */
-    @Override
-    public int hashCode() {
-        return this.name.hashCode();
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     *
-     * @return The object.
-     *
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(ChartChangeEventType.GENERAL)) {
-            return ChartChangeEventType.GENERAL;
-        }
-        else if (this.equals(ChartChangeEventType.NEW_DATASET)) {
-            return ChartChangeEventType.NEW_DATASET;
-        }
-        else if (this.equals(ChartChangeEventType.DATASET_UPDATED)) {
-            return ChartChangeEventType.DATASET_UPDATED;
-        }
-        return null;
-    }
+    DATASET_UPDATED
 
 }
