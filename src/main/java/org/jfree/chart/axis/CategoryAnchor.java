ab91008dc51ac5c69cabf44735f709bfc1099024
Simon Legner <Simon.Legner@gmail.com>
2018-03-20 11:24:40 +0100

    CategoryAnchor as enum

--- src/main/java/org/jfree/chart/axis/CategoryAnchor.java
+++ src/main/java/org/jfree/chart/axis/CategoryAnchor.java
@@ -40,93 +40,19 @@
 
 package org.jfree.chart.axis;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-
 /**
  * Used to indicate one of three positions within a category:
  * {@code START}, {@code MIDDLE} and {@code END}.
  */
-public final class CategoryAnchor implements Serializable {
-
-    /** For serialization. */
-    private static final long serialVersionUID = -2604142742210173810L;
+public enum CategoryAnchor {
 
     /** Start of period. */
-    public static final CategoryAnchor START
-        = new CategoryAnchor("CategoryAnchor.START");
+    START,
 
     /** Middle of period. */
-    public static final CategoryAnchor MIDDLE
-        = new CategoryAnchor("CategoryAnchor.MIDDLE");
+    MIDDLE,
 
     /** End of period. */
-    public static final CategoryAnchor END
-        = new CategoryAnchor("CategoryAnchor.END");
-
-    /** The name. */
-    private String name;
-
-    /**
-     * Private constructor.
-     *
-     * @param name  the name.
-     */
-    private CategoryAnchor(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Returns a string representing the object.
-     *
-     * @return The string.
-     */
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    /**
-     * Returns {@code true} if this object is equal to the specified
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the other object.
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof CategoryAnchor)) {
-            return false;
-        }
-        CategoryAnchor position = (CategoryAnchor) obj;
-        if (!this.name.equals(position.toString())) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     *
-     * @return The object.
-     *
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(CategoryAnchor.START)) {
-            return CategoryAnchor.START;
-        }
-        else if (this.equals(CategoryAnchor.MIDDLE)) {
-            return CategoryAnchor.MIDDLE;
-        }
-        else if (this.equals(CategoryAnchor.END)) {
-            return CategoryAnchor.END;
-        }
-        return null;
-    }
+    END
 
 }
