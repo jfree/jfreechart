9025e9887a7056130520e861d57d31aa8fbeb408
Tracy Hiltbrand <tracy.hiltbrand@gmail.com>
2018-11-26 19:26:59 -0600

    Converted several more classes to enums

--- src/main/java/org/jfree/chart/axis/AxisLocation.java
+++ src/main/java/org/jfree/chart/axis/AxisLocation.java
@@ -41,51 +41,31 @@
  * ------------- JFREECHART 1.0.x ---------------------------------------------
  * 22-Mar-2007 : Added getOpposite() method, suggested by Nick Guenther (DG);
  * 02-Jul-2013 : Use ParamChecks (DG);
+ * 26-Nov-2018 : Made AxisLocation an enum (TH);
  *
  */
 
 package org.jfree.chart.axis;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
 import org.jfree.chart.util.Args;
 
 /**
  * Used to indicate the location of an axis on a 2D plot, prior to knowing the
  * orientation of the plot.
  */
-public final class AxisLocation implements Serializable {
-
-    /** For serialization. */
-    private static final long serialVersionUID = -3276922179323563410L;
+public enum AxisLocation {
 
     /** Axis at the top or left. */
-    public static final AxisLocation TOP_OR_LEFT = new AxisLocation(
-            "AxisLocation.TOP_OR_LEFT");
+    TOP_OR_LEFT,
 
     /** Axis at the top or right. */
-    public static final AxisLocation TOP_OR_RIGHT = new AxisLocation(
-            "AxisLocation.TOP_OR_RIGHT");
+    TOP_OR_RIGHT,
 
     /** Axis at the bottom or left. */
-    public static final AxisLocation BOTTOM_OR_LEFT = new AxisLocation(
-            "AxisLocation.BOTTOM_OR_LEFT");
+    BOTTOM_OR_LEFT,
 
     /** Axis at the bottom or right. */
-    public static final AxisLocation BOTTOM_OR_RIGHT = new AxisLocation(
-            "AxisLocation.BOTTOM_OR_RIGHT");
-
-    /** The name. */
-    private String name;
-
-    /**
-     * Private constructor.
-     *
-     * @param name  the name.
-     */
-    private AxisLocation(String name) {
-        this.name = name;
-    }
+    BOTTOM_OR_RIGHT;
 
     /**
      * Returns the location that is opposite to this location.
@@ -96,51 +76,6 @@ public final class AxisLocation implements Serializable {
      */
     public AxisLocation getOpposite() {
         return getOpposite(this);
-    }
-
-    /**
-     * Returns a string representing the object.
-     *
-     * @return The string.
-     */
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    /**
-     * Returns {@code true} if this object is equal to the specified
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the other object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof AxisLocation)) {
-            return false;
-        }
-        AxisLocation location = (AxisLocation) obj;
-        if (!this.name.equals(location.toString())) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Returns a hash code for this instance.
-     * 
-     * @return A hash code.
-     */
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 83 * hash + this.name.hashCode();
-        return hash;
     }
 
     /**
@@ -169,29 +104,6 @@ public final class AxisLocation implements Serializable {
             throw new IllegalStateException("AxisLocation not recognised.");
         }
         return result;
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     *
-     * @return The object.
-     *
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(AxisLocation.TOP_OR_RIGHT)) {
-            return AxisLocation.TOP_OR_RIGHT;
-        }
-        else if (this.equals(AxisLocation.BOTTOM_OR_RIGHT)) {
-            return AxisLocation.BOTTOM_OR_RIGHT;
-        }
-        else if (this.equals(AxisLocation.TOP_OR_LEFT)) {
-            return AxisLocation.TOP_OR_LEFT;
-        }
-        else if (this.equals(AxisLocation.BOTTOM_OR_LEFT)) {
-            return AxisLocation.BOTTOM_OR_LEFT;
-        }
-        return null;
     }
 
 }
