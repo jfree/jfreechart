52daf08cf3a01ad6c9fea47f1f04ee41d503d21e
Tracy Hiltbrand <tracy.hiltbrand@gmail.com>
2019-01-27 15:08:32 -0600

    Converted to enum

--- src/main/java/org/jfree/chart/LegendRenderingOrder.java
+++ src/main/java/org/jfree/chart/LegendRenderingOrder.java
@@ -35,90 +35,20 @@
  * Changes
  * -------
  * 26-Mar-2004 : Version 1 (DG);
+ * 20-Jan-2019 : Made into an enum (TH);
  *
  */
 
 package org.jfree.chart;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-
 /**
  * Represents the order for rendering legend items.
  */
-public final class LegendRenderingOrder implements Serializable {
-
-    /** For serialization. */
-    private static final long serialVersionUID = -3832486612685808616L;
+public enum LegendRenderingOrder {
 
     /** In order. */
-    public static final LegendRenderingOrder STANDARD
-            = new LegendRenderingOrder("LegendRenderingOrder.STANDARD");
+    STANDARD,
 
     /** In reverse order. */
-    public static final LegendRenderingOrder REVERSE
-            = new LegendRenderingOrder("LegendRenderingOrder.REVERSE");
-
-    /** The name. */
-    private String name;
-
-    /**
-     * Private constructor.
-     *
-     * @param name  the name.
-     */
-    private LegendRenderingOrder(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Returns a string representing the object.
-     *
-     * @return The string.
-     */
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    /**
-     * Returns {@code true} if this object is equal to the specified
-     * object, and {@code false} otherwise.
-     *
-     * @param obj  the other object.
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof LegendRenderingOrder)) {
-            return false;
-        }
-        LegendRenderingOrder order = (LegendRenderingOrder) obj;
-        if (!this.name.equals(order.toString())) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Ensures that serialization returns the unique instances.
-     *
-     * @return The object.
-     *
-     * @throws ObjectStreamException if there is a problem.
-     */
-    private Object readResolve() throws ObjectStreamException {
-        if (this.equals(LegendRenderingOrder.STANDARD)) {
-            return LegendRenderingOrder.STANDARD;
-        }
-        else if (this.equals(LegendRenderingOrder.REVERSE)) {
-            return LegendRenderingOrder.REVERSE;
-        }
-        return null;
-    }
-
+    REVERSE
 }
