f8aa988985e7e1dbc32eeccc5ed38ad6dda755a9
David Gilbert <dave@jfree.org>
2020-03-10 22:25:13 +0100

    Fix warnings.

--- src/main/java/org/jfree/chart/panel/CrosshairOverlay.java
+++ src/main/java/org/jfree/chart/panel/CrosshairOverlay.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2018, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2020, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ---------------------
  * CrosshairOverlay.java
  * ---------------------
- * (C) Copyright 2011-2018, by Object Refinery Limited.
+ * (C) Copyright 2011-2020, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   John Matthews;
@@ -86,8 +86,8 @@ public class CrosshairOverlay extends AbstractOverlay implements Overlay,
      */
     public CrosshairOverlay() {
         super();
-        this.xCrosshairs = new java.util.ArrayList<Crosshair>();
-        this.yCrosshairs = new java.util.ArrayList<Crosshair>();
+        this.xCrosshairs = new ArrayList<>();
+        this.yCrosshairs = new ArrayList<>();
     }
 
     /**
