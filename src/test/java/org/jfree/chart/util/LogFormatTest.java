8453a43d2afc29ce40bd6d43f4985f928ae3e2c8
David Gilbert <dave@jfree.org>
2020-03-20 05:49:49 +0100

    Update headers.

--- src/test/java/org/jfree/chart/util/LogFormatTest.java
+++ src/test/java/org/jfree/chart/util/LogFormatTest.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2016, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2020, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,15 +27,10 @@
  * ------------------
  * LogFormatTest.java
  * ------------------
- * (C) Copyright 2008-2016, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2008-2020, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
- *
- * Changes
- * -------
- * 08-Feb-2008 : Version 1 (DG);
- * 14-Jan-2009 : Updated testEquals() for new field (DG);
  *
  */
 
