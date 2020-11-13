8d53dc5ad06cd68752b6598df193ac64488500c9
David Gilbert <dave@jfree.org>
2020-11-13 05:07:39 +0100

    Fix XML vulnerability - see #138

--- src/main/java/org/jfree/data/xml/DatasetReader.java
+++ src/main/java/org/jfree/data/xml/DatasetReader.java
@@ -27,14 +27,10 @@
  * ------------------
  * DatasetReader.java
  * ------------------
- * (C) Copyright 2002-2008, by Object Refinery Limited.
+ * (C) Copyright 2002-2020, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
- *
- * Changes
- * -------
- * 20-Nov-2002 : Version 1 (DG);
  *
  */
 
@@ -45,23 +41,62 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.jfree.chart.util.Args;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.general.PieDataset;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 /**
  * A utility class for reading datasets from XML.
  */
 public class DatasetReader {
 
+    /** A factory for creating new parser instances. */
+    static SAXParserFactory factory;
+
+    /**
+     * Returns the {@link SAXParserFactory} used to create {@link SAXParser} instances.
+     * 
+     * @return The {@link SAXParserFactory} (never {@code null}).
+     */
+    public static SAXParserFactory getSAXParserFactory() {
+    	if (factory == null) {
+            SAXParserFactory f = SAXParserFactory.newInstance();
+            try {
+                f.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+                f.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                f.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                factory = f;
+            } catch (SAXNotRecognizedException | SAXNotSupportedException | ParserConfigurationException e) {
+                throw new RuntimeException(e);
+            }
+    	}
+        return factory;
+    }
+    
+    /**
+     * Sets the SAXParserFactory that will be used to create SAXParser instances.  
+     * You would only call this method if you wish to configure a new factory because
+     * the default does not meet requirements.
+     * 
+     * @param f  the new factory ({@code null} not permitted).
+     */
+    public static void setSAXParserFactory(SAXParserFactory f) {
+    	Args.nullNotPermitted(f, "f");
+        factory = f;
+    }
+
     /**
      * Reads a {@link PieDataset} from an XML file.
      *
-     * @param file  the file.
+     * @param file  the file ({@code null} not permitted).
      *
      * @return A dataset.
      *
@@ -83,24 +118,18 @@ public class DatasetReader {
      * @throws IOException if there is an I/O error.
      */
     public static PieDataset readPieDatasetFromXML(InputStream in)
-        throws IOException {
-
+            throws IOException {
         PieDataset result = null;
-        SAXParserFactory factory = SAXParserFactory.newInstance();
         try {
-            SAXParser parser = factory.newSAXParser();
+            SAXParser parser = getSAXParserFactory().newSAXParser();
             PieDatasetHandler handler = new PieDatasetHandler();
             parser.parse(in, handler);
             result = handler.getDataset();
         }
-        catch (SAXException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (ParserConfigurationException e2) {
-            System.out.println(e2.getMessage());
+        catch (SAXException | ParserConfigurationException e) {
+            throw new RuntimeException(e);
         }
         return result;
-
     }
 
     /**
@@ -113,7 +142,7 @@ public class DatasetReader {
      * @throws IOException if there is a problem reading the file.
      */
     public static CategoryDataset readCategoryDatasetFromXML(File file)
-        throws IOException {
+            throws IOException {
         InputStream in = new FileInputStream(file);
         return readCategoryDatasetFromXML(in);
     }
@@ -128,25 +157,18 @@ public class DatasetReader {
      * @throws IOException if there is a problem reading the file.
      */
     public static CategoryDataset readCategoryDatasetFromXML(InputStream in)
-        throws IOException {
-
+            throws IOException {
         CategoryDataset result = null;
-
-        SAXParserFactory factory = SAXParserFactory.newInstance();
         try {
-            SAXParser parser = factory.newSAXParser();
+            SAXParser parser = getSAXParserFactory().newSAXParser();
             CategoryDatasetHandler handler = new CategoryDatasetHandler();
             parser.parse(in, handler);
             result = handler.getDataset();
         }
-        catch (SAXException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (ParserConfigurationException e2) {
-            System.out.println(e2.getMessage());
+        catch (SAXException | ParserConfigurationException e) {
+            throw new RuntimeException(e);
         }
         return result;
-
     }
 
 }
