440fd21971a3a2329d0187bb7ffdae2223eeac55
David Gilbert <dave@jfree.org>
2021-01-10 23:05:09 +0100

    Create module.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,38 @@
+/** 
+ * JFreeChart module.
+ */
+module org.jfree.chart {
+    requires java.desktop;
+    exports org.jfree.chart;
+    exports org.jfree.chart.annotations;
+    exports org.jfree.chart.axis;
+    exports org.jfree.chart.date;
+    exports org.jfree.chart.editor;
+    exports org.jfree.chart.entity;
+    exports org.jfree.chart.event;
+    exports org.jfree.chart.imagemap;
+    exports org.jfree.chart.needle;
+    exports org.jfree.chart.panel;
+    exports org.jfree.chart.plot;
+    exports org.jfree.chart.plot.dial;
+    exports org.jfree.chart.renderer;
+    exports org.jfree.chart.renderer.category;
+    exports org.jfree.chart.renderer.xy;
+    exports org.jfree.chart.text;
+    exports org.jfree.chart.title;
+    exports org.jfree.chart.ui;
+    exports org.jfree.chart.urls;
+    exports org.jfree.chart.util;
+    exports org.jfree.data;
+    exports org.jfree.data.category;
+    exports org.jfree.data.function;    
+    exports org.jfree.data.gantt;
+    exports org.jfree.data.general;
+    exports org.jfree.data.io;
+    exports org.jfree.data.json;
+    exports org.jfree.data.statistics;
+    exports org.jfree.data.time;
+    exports org.jfree.data.time.ohlc;
+    exports org.jfree.data.xml;
+    exports org.jfree.data.xy;
+}
