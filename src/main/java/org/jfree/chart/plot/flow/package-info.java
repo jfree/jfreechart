50e570249d22ccef416e33b5c64073598be42b23
David Gilbert <dave@jfree.org>
2021-02-20 05:26:26 +0100

    Add package descriptions for Javadocs.

---
+++ src/main/java/org/jfree/chart/plot/flow/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes for creating flow plots (a type of Sankey chart).
+ */
+package org.jfree.chart.plot.flow;
---
+++ src/main/java/org/jfree/data/flow/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data interfaces and classes for flow plots (a type of Sankey chart).
+ */
+package org.jfree.data.flow;
