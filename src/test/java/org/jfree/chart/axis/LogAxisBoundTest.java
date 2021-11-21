55c40d92bec6bfe52010aa54cae01f24f29e7da1
LunaXia <43047886+LunaXia@users.noreply.github.com>
2021-11-21 12:41:44 -0800

    test for new method for issue 215
    add test for new method for issue 215 (min value missing in log plot)

---
+++ src/test/java/org/jfree/chart/axis/LogAxisBoundTest.java
@@ -0,0 +1,192 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2020, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ----------------
+ * LogAxisBoundTest.java
+ * ----------------
+ * (C) Copyright 2007-2020, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   Zhiyue Xia created this test;
+ */
+
+package org.jfree.chart.axis;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.TestUtils;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for the {@link LogAxis} class.
+ */
+public class LogAxisBoundTest {
+
+    /**
+     * Confirm that the equals method can distinguish all the required fields.
+     */
+    @Test
+    public void testEquals() {
+        LogAxis a1 = new LogAxis("Test");
+        LogAxis a2 = new LogAxis("Test");
+        assertTrue(a1.equals(a2));
+
+        a1.setBase(2.0);
+        assertFalse(a1.equals(a2));
+        a2.setBase(2.0);
+        assertTrue(a1.equals(a2));
+
+        a1.setSmallestValue(0.1);
+        assertFalse(a1.equals(a2));
+        a2.setSmallestValue(0.1);
+        assertTrue(a1.equals(a2));
+
+        a1.setMinorTickCount(8);
+        assertFalse(a1.equals(a2));
+        a2.setMinorTickCount(8);
+        assertTrue(a1.equals(a2));
+    }
+
+    private static final double EPSILON = 0.0000001;
+
+    /**
+     * Test the translation of Java2D values to data values.
+     */
+    @Test
+    public void testTranslateJava2DToValue() {
+        LogAxis axis = new LogAxis();
+        axis.setRange(50.0, 100.0);
+        Rectangle2D dataArea = new Rectangle2D.Double(10.0, 50.0, 400.0, 300.0);
+        double y1 = axis.java2DToValue(75.0, dataArea, RectangleEdge.LEFT);
+        assertEquals(94.3874312681693, y1, EPSILON);
+        double y2 = axis.java2DToValue(75.0, dataArea, RectangleEdge.RIGHT);
+        assertEquals(94.3874312681693, y2, EPSILON);
+        double x1 = axis.java2DToValue(75.0, dataArea, RectangleEdge.TOP);
+        assertEquals(55.961246381405, x1, EPSILON);
+        double x2 = axis.java2DToValue(75.0, dataArea, RectangleEdge.BOTTOM);
+        assertEquals(55.961246381405, x2, EPSILON);
+        axis.setInverted(true);
+        double y3 = axis.java2DToValue(75.0, dataArea, RectangleEdge.LEFT);
+        assertEquals(52.9731547179647, y3, EPSILON);
+        double y4 = axis.java2DToValue(75.0, dataArea, RectangleEdge.RIGHT);
+        assertEquals(52.9731547179647, y4, EPSILON);
+        double x3 = axis.java2DToValue(75.0, dataArea, RectangleEdge.TOP);
+        assertEquals(89.3475453695651, x3, EPSILON);
+        double x4 = axis.java2DToValue(75.0, dataArea, RectangleEdge.BOTTOM);
+        assertEquals(89.3475453695651, x4, EPSILON);
+    }
+
+
+    /**
+     * A simple test for the auto-range calculation looking at a
+     * LogAxis used as the range axis for a CategoryPlot.
+     */
+    @Test
+    public void testAutoRange1() {
+        DefaultCategoryDataset<String, String> dataset = new DefaultCategoryDataset<>();
+        dataset.setValue(100.0, "Row 1", "Column 1");
+        dataset.setValue(200.0, "Row 1", "Column 2");
+        JFreeChart chart = ChartFactory.createBarChart("Test", "Categories",
+                "Value", dataset);
+        CategoryPlot<?, ?> plot = (CategoryPlot) chart.getPlot();
+        LogAxis axis = new LogAxis("Log(Y)");
+        plot.setRangeAxis(axis);
+        assertEquals(0.0, axis.getLowerBound(), EPSILON);
+        assertEquals(2.6066426411261268E7, axis.getUpperBound(), EPSILON);
+    }
+
+    /**
+     * A simple test for the auto-range calculation looking at a
+     * NumberAxis used as the range axis for a CategoryPlot.  In this
+     * case, the original dataset is replaced with a new dataset.
+     */
+    @Test
+    public void testAutoRange3() {
+        DefaultCategoryDataset<String, String> dataset = new DefaultCategoryDataset<>();
+        dataset.setValue(100.0, "Row 1", "Column 1");
+        dataset.setValue(200.0, "Row 1", "Column 2");
+        JFreeChart chart = ChartFactory.createLineChart("Test", "Categories",
+                "Value", dataset, PlotOrientation.VERTICAL, false, false,
+                false);
+        @SuppressWarnings("unchecked")
+        CategoryPlot<String, String> plot = (CategoryPlot) chart.getPlot();
+        LogAxis axis = new LogAxis("Log(Y)");
+        plot.setRangeAxis(axis);
+        assertEquals(96.59363289248458, axis.getLowerBound(), EPSILON);
+        assertEquals(207.0529847682752, axis.getUpperBound(), EPSILON);
+
+        // now replacing the dataset should update the axis range...
+        DefaultCategoryDataset<String, String> dataset2 = new DefaultCategoryDataset<>();
+        dataset2.setValue(900.0, "Row 1", "Column 1");
+        dataset2.setValue(1000.0, "Row 1", "Column 2");
+        plot.setDataset(dataset2);
+        assertEquals(895.2712433374774, axis.getLowerBound(), EPSILON);
+        assertEquals(1005.2819262292991, axis.getUpperBound(), EPSILON);
+    }
+
+
+    /**
+     * Some checks for the setLowerBound() method.
+     */
+    @Test
+    public void testSetLowerBound() {
+        LogAxis axis = new LogAxis("X");
+        axis.setRange(0.0, 10.0);
+        axis.setLowerBound(0.0);
+        // set lower bound to a minimum number
+        assertEquals(1e-100, axis.getLowerBound(), EPSILON);
+        assertEquals(10.0, axis.getUpperBound(), EPSILON);
+        assertEquals(1.0E-100, axis.getSmallestValue(), EPSILON);
+
+    }
+    
+    /**
+     * Checks that a TickUnit with a size of 0 doesn't crash.
+     */
+    @Test
+    public void testRefreshTicksWithZeroTickUnit() {
+        LogAxis axis = new LogAxis();
+        AxisState state = new AxisState();
+        BufferedImage image = new BufferedImage(200, 100,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = image.createGraphics();
+        Rectangle2D area = new Rectangle2D.Double(0.0, 0.0, 200, 100);
+        axis.refreshTicks(g2, state, area, RectangleEdge.TOP);
+    }
+}
