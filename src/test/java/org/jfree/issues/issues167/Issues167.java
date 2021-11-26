b26c5214c6006abfbce814b4927266f34d4fe87c
Ivy987 <awinwin5@gmail.com>
2021-11-26 00:58:08 -0600

    Update Issues167.java

--- src/test/java/org/jfree/issues/issues167/Issues167.java
+++ src/test/java/org/jfree/issues/issues167/Issues167.java
@@ -8,7 +8,14 @@ import org.jfree.data.general.DefaultPieDataset;
 import org.junit.jupiter.api.Test;
 
 /**
-
+ * Project: Issues167
+ * Description: This file fixed the issue #167 for the jfree library.
+ *
+ * <p>
+ * author:wm
+ * Date:2021/11/18
+ * Time:17:17
+ * Version:
  */
 public class Issues167 {
 
@@ -23,13 +30,13 @@ public class Issues167 {
         ds.setValue("test05", 20);
         ds.setValue("test06", 20);
 
-        //
+        //set the parameters, including title, dataset, legend, tooltips, urls.
         JFreeChart chart = ChartFactory.createPieChart("", ds, true, true, false);
-        //
+        //Pie plot information
         PiePlot pieplot = (PiePlot) chart.getPlot();
         pieplot.setSimpleLabels(true);
 
-        //
+        // set the title of the frame
         ChartFrame chartFrame = new ChartFrame("", chart);
         chartFrame.pack();
         chartFrame.setVisible(true);
@@ -46,13 +53,13 @@ public class Issues167 {
         ds.setValue("test04", 15);
 
 
-        //
+        //set the parameters, including title, dataset, legend, tooltips, urls.
         JFreeChart chart = ChartFactory.createPieChart("", ds, true, true, false);
-        //
+        //Pie plot information
         PiePlot pieplot = (PiePlot) chart.getPlot();
         pieplot.setSimpleLabels(true);
 
-        //
+        //set the title of the frame
         ChartFrame chartFrame = new ChartFrame("", chart);
         chartFrame.pack();
         chartFrame.setVisible(true);
