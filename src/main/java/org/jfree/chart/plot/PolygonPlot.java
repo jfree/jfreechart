49c72808e0ea26cc95aba9f78c44fbd7191c1a45
richardxs <richard041123@gmail.com>
2021-12-07 15:08:38 -0600

    Update javadoc

--- src/main/java/org/jfree/chart/plot/PolygonPlot.java
+++ src/main/java/org/jfree/chart/plot/PolygonPlot.java
@@ -14,7 +14,7 @@ import org.jfree.chart.swing.ApplicationFrame;
 import org.jfree.chart.axis.NumberAxis;
 
 /**
- * This demo illustrates the solution to issue #234.
+ * CS427： This demo illustrates the solution to issue #234.
  */
 public class PolygonPlot extends ApplicationFrame{
     private static XYDataset dataset = null;
@@ -25,10 +25,10 @@ public class PolygonPlot extends ApplicationFrame{
      *
      * @param title  the frame title.
      */
-    public PolygonPlot(String title) {
+    public PolygonPlot(final String title) {
         super(title);
-        JPanel chartPanel = createDemoPanel(title);
-        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 500));
+        final JPanel chartPanel = createDemoPanel(title);
+        chartPanel.setPreferredSize(new Dimension(1000, 500));
         setContentPane(chartPanel);
     }
 
@@ -39,8 +39,8 @@ public class PolygonPlot extends ApplicationFrame{
      * @return The chart.
      */
 
-    private static JFreeChart createChart(String title) {
-        JFreeChart chart = ChartFactory.createScatterPlot(
+    private static JFreeChart createChart(final String title) {
+        final JFreeChart chart = ChartFactory.createScatterPlot(
                 title, "Distance(m)", "Depth(m)", dataset,
                 PlotOrientation.VERTICAL, true, true, false);
 
@@ -50,46 +50,46 @@ public class PolygonPlot extends ApplicationFrame{
         xyplot.setRangePannable(true);
         xyplot.setDomainAxisLocation(0, AxisLocation.TOP_OR_LEFT);
 
-        NumberAxis domain = (NumberAxis) xyplot.getDomainAxis();
+        final NumberAxis domain = (NumberAxis) xyplot.getDomainAxis();
         domain.setRange(60.0, 800.0);
 
 
-        NumberAxis range = (NumberAxis) xyplot.getRangeAxis();
+        final NumberAxis range = (NumberAxis) xyplot.getRangeAxis();
         range.setRange(0.0, 150.0);
         range.setInverted(true);
         return  chart;
     }
 
-    public void paint(Graphics g){
+    public void paint(final Graphics graphics){
         //Graphics2D g2d = (Graphics2D)g;
-        XYLineAndShapeRenderer renderer
+        final XYLineAndShapeRenderer renderer
                 = (XYLineAndShapeRenderer) xyplot.getRenderer();
         xyplot.setBackgroundPaint(Color.white);
 
-        Color color1 = new Color(138, 43, 226);
-        Color color2 = new Color(18, 10, 255);
-        Color color3 = new Color(18, 255, 0);
-        Color color4 = new Color(255, 255, 0);
+        final Color color1 = new Color(138, 43, 226);
+        final Color color2 = new Color(18, 10, 255);
+        final Color color3 = new Color(18, 255, 0);
+        final Color color4 = new Color(255, 255, 0);
 
 
-        XYPolygonAnnotation polygon1 = new XYPolygonAnnotation(new double[] {70.0,
+        final XYPolygonAnnotation polygon1 = new XYPolygonAnnotation(new double[] {70.0,
                 10.0,80.0, 21.0, 570.0, 21.0,560.0, 10.0}, null, null,color1);
         renderer.addAnnotation(polygon1);
 
-        XYPolygonAnnotation polygon2 = new XYPolygonAnnotation(new double[] {80.0,
+        final XYPolygonAnnotation polygon2 = new XYPolygonAnnotation(new double[] {80.0,
                 20.0, 110.0, 50.0, 600.0, 50.0, 570.0, 20.0}, null, null,color2);
         renderer.addAnnotation(polygon2);
 
-        XYPolygonAnnotation polygon3 = new XYPolygonAnnotation(new double[] {110.0,
+        final XYPolygonAnnotation polygon3 = new XYPolygonAnnotation(new double[] {110.0,
                 50.0, 130.0, 70.0, 620.0, 70.0, 600.0, 50.0}, null, null,color3);
         renderer.addAnnotation(polygon3);
 
 
-        XYPolygonAnnotation polygon4 = new XYPolygonAnnotation(new double[] {130.0,
+        final XYPolygonAnnotation polygon4 = new XYPolygonAnnotation(new double[] {130.0,
                 70.0, 150.0, 90.0, 640.0, 90.0,620.0, 70.0}, null, null,color4);
         renderer.addAnnotation(polygon4);
 
-        XYPolygonAnnotation polygon5 = new XYPolygonAnnotation(new double[] {150.0,
+        final XYPolygonAnnotation polygon5 = new XYPolygonAnnotation(new double[] {150.0,
                 90.0, 180.0, 120.0, 670.0, 120.0, 640.0, 90.0}, null, null,new Color(255,165,0));
         renderer.addAnnotation(polygon5);
     }
@@ -99,11 +99,9 @@ public class PolygonPlot extends ApplicationFrame{
      *
      * @return A panel.
      */
-    public static JPanel createDemoPanel(String title) {
-        JFreeChart chart = createChart(title);
-        ChartPanel panel = new ChartPanel(chart);
-        // panel.setMouseWheelEnabled(true);
-        return panel;
+    public static JPanel createDemoPanel(final String title) {
+        final JFreeChart chart = createChart(title);
+        return new ChartPanel(chart);
     }
 
     /**
@@ -114,22 +112,22 @@ public class PolygonPlot extends ApplicationFrame{
      * @return {@code true} or {@code false}.
      */
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(final Object obj) {
         if (obj == this) {
             return true;
         }
         if (!(obj instanceof PolygonPlot)) {
             return false;
         }
-        PolygonPlot that = (PolygonPlot) obj;
+        final PolygonPlot that = (PolygonPlot) obj;
         if (this.getTitle().equals(that.getTitle())) {
             return true;
         }
         return super.equals(obj);
     }
 
-    public static void main(String[] args) {
-        PolygonPlot demo = new PolygonPlot("PolygonPlot Demo");
+    public static void main(final String[] args) {
+        final PolygonPlot demo = new PolygonPlot("PolygonPlot Demo");
         demo.pack();
         UIUtils.centerFrameOnScreen(demo);
         demo.setVisible(true);
