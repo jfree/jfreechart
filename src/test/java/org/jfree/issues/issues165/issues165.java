ebda8176a8ca41b0a4567463b4c0483e293a0411
Ivy987 <awinwin5@gmail.com>
2021-12-08 13:53:55 -0600

    Update issues165.java

--- src/test/java/org/jfree/issues/issues165/issues165.java
+++ src/test/java/org/jfree/issues/issues165/issues165.java
@@ -4,18 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
- * 类名: issues165
- * 描述:
+ * issues165
  *
- * <p>
- * author:
- * Date:2021/11/26
- * Time:9:21
  * Version:
  */
 public class issues165 {
 
     @Test
+    //create a test for the year before 1990
     public void testOldCode() {
         try {
             org.jfree.data.time.Day day = new org.jfree.data.time.Day(1, 1, 1890);
@@ -25,6 +21,7 @@ public class issues165 {
     }
 
     @Test
+    //test the year of 1890
     public void testSucessCode() {
         Day day = new Day(1, 1, 1890);
     }
