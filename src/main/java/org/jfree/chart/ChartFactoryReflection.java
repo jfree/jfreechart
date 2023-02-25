a58da16350aae168420daf08452faa0a1701f648
Rallefar1103 <rasmus@turftank.com>
2023-02-24 19:35:44 -0800

    reflection barChart works with a hack...

--- src/main/java/org/jfree/chart/ChartFactoryReflection.java
+++ src/main/java/org/jfree/chart/ChartFactoryReflection.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.jfree.data.category.DefaultCategoryDataset;
+
 public class ChartFactoryReflection extends ChartFactory {
     ChartFactoryReflection() {
 
@@ -25,6 +27,7 @@ public class ChartFactoryReflection extends ChartFactory {
         }
 
         Object chartObj = chartConstructor.newInstance();
+
         return getChartObject(chart, classObj, chartObj, params.get(chart));
 
     }
@@ -32,6 +35,8 @@ public class ChartFactoryReflection extends ChartFactory {
     public JFreeChart getChartObject(String simpleClassName, Class<?> classObj, Object chartObj, List<Object> params)
             throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
             InvocationTargetException {
+        System.out.println("Called getChartObject with parameters: \n" + simpleClassName + "\n" + classObj.getName()
+                + "\n" + chartObj.getClass().getName() + "\n" + params);
         Method createMethod;
         Class<?>[] parameterTypes = new Class<?>[params.size()];
 
@@ -40,9 +45,18 @@ public class ChartFactoryReflection extends ChartFactory {
             parameterTypes[i] = objectType;
         }
 
+        System.out.println("Parameters in parameterTypes are: \n");
+
+        for (var param : parameterTypes) {
+            System.out.println(param);
+        }
+
         createMethod = classObj.getMethod("createChart", parameterTypes);
 
-        return (JFreeChart) createMethod.invoke(chartObj, params);
+        System.out.println("Create method found is: " + createMethod.getName() + " with parammeters: "
+                + createMethod.getParameterCount());
+
+        return (JFreeChart) createMethod.invoke(chartObj, params.get(0), params.get(1), params.get(2), params.get(3));
 
     }
 }
