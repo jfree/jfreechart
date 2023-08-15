321f7f5cbf8ffe618f5f17589c79b8f92d5bc235
daniele <daniele.russo.vs@gmail.com>
2023-08-15 17:15:36 +0200

    Add default values in tests

--- src/test/java/org/jfree/chart/encoders/EncoderUtilTest.java
+++ src/test/java/org/jfree/chart/encoders/EncoderUtilTest.java
@@ -12,58 +12,78 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EncoderUtilTest {
     private BufferedImage bufferedImage;
-    private String defaultFormat = "jpeg";
+    private ByteArrayOutputStream baos;
+    private final String defaultBufferValue = "test";
+    private final String defaultFormat = "jpeg";
+    private final String defaultInvalidFormat = "test";
+    private final float defaultQualityLevel = 0.4f;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         bufferedImage = new BufferedImage(1, 1, TYPE_INT_RGB);
+        baos = new ByteArrayOutputStream();
+        baos.write(defaultBufferValue.getBytes());
     }
 
-    @Test()
-    void throwExceptionWithNullInputs() {
+    @Test
+    void encodeThrowExceptionWhenRequiredInputsAreNull(){
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.encode(null, null)
         );
-    }
 
-    @Test()
-    void throwExceptionWhenImageFormatIsNotSpecified() {
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.encode(bufferedImage, null)
         );
-    }
 
-    @Test
-    void throwExceptionWithInvalidImageFormat() throws IOException {
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.encode(bufferedImage, "pippo")
         );
-    }
 
-    @Test
-    void throwExceptionWithAllNulls() throws IOException {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> EncoderUtil.encode(null, null, true));
-    }
-
-    @Test
-    void throwExceptionWhenFormatIsNull() throws IOException {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> EncoderUtil.encode(bufferedImage, null, true)
-        );
-    }
-
-    @Test
-    void throwExceptionWhenAllNulls(){
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.encode(null, null, 0f)
         );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(null, null, true));
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(bufferedImage, null, true)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(null, null, defaultQualityLevel, true)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(null, null, defaultQualityLevel, false)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(bufferedImage, null, defaultQualityLevel, true)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(bufferedImage, null, defaultQualityLevel, false)
+        );
+    }
+
+    @Test
+    void throwsExceptionIfFormatIsInvalid(){
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.encode(bufferedImage, defaultInvalidFormat, defaultQualityLevel, false)
+        );
+
     }
 
     @Test
@@ -80,50 +100,128 @@ class EncoderUtilTest {
 
     @Test
     void canEncodeWithAlphaTransparency() throws IOException {
-        byte[] encoded = EncoderUtil.encode(bufferedImage, "jpg", true);
+        byte[] encoded = EncoderUtil.encode(bufferedImage, defaultFormat, true);
         assertNotNull(encoded);
     }
 
     @Test
     void canEncodeWithoutAlphaTransparency() throws IOException {
-        byte[] encoded = EncoderUtil.encode(bufferedImage, "jpg", false);
+        byte[] encoded = EncoderUtil.encode(bufferedImage, defaultFormat, false);
         assertNotNull(encoded);
     }
 
     @Test
     void canEncodeWithQualityLevel() throws IOException {
-        byte[] encoded = EncoderUtil.encode(bufferedImage, "jpg", 0.4f);
+        byte[] encoded = EncoderUtil.encode(bufferedImage, defaultFormat, defaultQualityLevel);
         assertNotNull(encoded);
     }
 
     @Test
-    void writeBufferedImage_throwExceptionWithAllNulls() throws IOException {
+    void canEncodeWithQualityAndTransparency() throws IOException {
+        byte[] encoded = EncoderUtil.encode(bufferedImage, defaultFormat, defaultQualityLevel, true);
+        assertNotNull(encoded);
+    }
+
+    @Test
+    void canEncodeWithQualityButWithoutTransparency() throws IOException {
+        byte[] encoded = EncoderUtil.encode(bufferedImage, defaultFormat, defaultQualityLevel, false);
+        assertNotNull(encoded);
+    }
+
+    @Test
+    void writeBufferedImage_throwsExceptionWithAllNulls(){
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.writeBufferedImage(null, null, null));
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(null, null, null, defaultQualityLevel)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(null, null, null, true)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(null, null, null, true)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(null, null, null,defaultQualityLevel, true)
+        );
     }
 
     @Test
-    void writeBufferedImage_throwExceptionWithNullFormat(){
+    void writeBufferedImage_throwsExceptionWhenFormatIsNull(){
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.writeBufferedImage(bufferedImage, null, null));
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(bufferedImage, null, null, defaultQualityLevel)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(bufferedImage, null, null,defaultQualityLevel, true)
+        );
     }
 
     @Test
-    void writeBufferedImage_throwExceptionWhenNullOutputStream() throws IOException {
+    void writeBufferedImage_throwsExceptionWhenOutputStreamIsNull(){
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, null, defaultQualityLevel)
+        );
+
         assertThrows(
                 IllegalArgumentException.class,
                 () -> EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, null));
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, null,defaultQualityLevel, true)
+        );
     }
 
     @Test
     void testWriteBufferedImage() throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        baos.write("ciao".getBytes());
         EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, baos);
-        assertTrue(baos.toString().contains("ciao"));
+        assertTrue(baos.toString().contains(defaultBufferValue));
     }
 
+    @Test
+    void canWriteBufferedImageWithQuality() throws IOException {
+        EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, baos, defaultQualityLevel);
+        assertTrue(baos.toString().contains(defaultBufferValue));
+    }
 
+    @Test
+    void canWriteBufferedImageWithTransparency() throws IOException {
+        EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, baos, true);
+        assertTrue(baos.toString().contains(defaultBufferValue));
+    }
+
+    @Test
+    void canWriteBufferedImageWithoutTransparency() throws IOException {
+        EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, baos, false);
+        assertTrue(baos.toString().contains(defaultBufferValue));
+    }
+
+    @Test
+    void canWriteBufferedImageWithQualityAndTransparency() throws IOException {
+        EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, baos, defaultQualityLevel, true);
+        assertTrue(baos.toString().contains(defaultBufferValue));
+    }
+
+    @Test
+    void canWriteBufferedImageWithQualityButWithoutTransparency() throws IOException {
+        EncoderUtil.writeBufferedImage(bufferedImage, defaultFormat, baos, defaultQualityLevel, false);
+        assertTrue(baos.toString().contains(defaultBufferValue));
+    }
 }
