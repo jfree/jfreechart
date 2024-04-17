6e7f7d2269499a8369d68d70cc227209d5bbef69
trashgod <trashgod@gmail.com>
2024-04-17 19:40:40 -0400

    CVE-2024-23077.

--- src/main/java/org/jfree/chart/plot/CompassPlot.java
+++ src/main/java/org/jfree/chart/plot/CompassPlot.java
@@ -464,7 +464,7 @@ public class CompassPlot extends Plot implements Cloneable, Serializable {
      * @param needle  the needle.
      */
     public void setSeriesNeedle(int index, MeterNeedle needle) {
-        if ((needle != null) && (index < this.seriesNeedle.length)) {
+        if ((needle != null) && (index >= 0) && (index < this.seriesNeedle.length)) {
             this.seriesNeedle[index] = needle;
         }
         fireChangeEvent();
